1524a7c75ce7a0631747ec5f4679477b25b00e9b
Dustin Tran <trandustin@google.com>
2018-04-13 17:48:59 -0700

    Set default initializer for variational distribution's stddev to be close to 0.
    The defaults in passing around arguments were incorrect. In particular, originally a `None` initializer is passed to `untransformed_scale` in all mean-field variational distributions; Keras defaults this to Glorot uniform. This is bad for initializing scale parameters: converging to a non-trivial solution typically requires an initialization <-3. This is because its transform implies a stddev initialization close to 0, which allows the parameter update trajectory to begin roughly like non-VI-based gradient descent.
    
    PiperOrigin-RevId: 192850120

--- tensorflow_probability/python/layers/util.py
+++ tensorflow_probability/python/layers/util.py
@@ -66,8 +66,9 @@ def default_loc_scale_fn(
       The default is `tf.random_normal_initializer(mean=0., stddev=0.1)`.
     untransformed_scale_initializer: Initializer function for the `scale`
       parameters. Default value: `tf.random_normal_initializer(mean=-3.,
-      stddev=0.1)`. This implies the softplus transformed result has mean
-      approximately `0.05` and std. deviation approximately `0.005`.
+      stddev=0.1)`. This implies the softplus transformed result is initialized
+      near `0`. It allows a `Normal` distribution with `scale` parameter set to
+      this value to approximately act like a point mass.
     loc_regularizer: Regularizer function for the `loc` parameters.
       The default (`None`) is to use the `tf.get_variable` default.
     untransformed_scale_regularizer: Regularizer function for the `scale`
@@ -118,8 +119,9 @@ def default_loc_scale_fn(
 
 def default_mean_field_normal_fn(
     is_singular=False,
-    loc_initializer=None,
-    untransformed_scale_initializer=None,
+    loc_initializer=tf.random_normal_initializer(stddev=0.1),
+    untransformed_scale_initializer=tf.random_normal_initializer(
+        mean=-3., stddev=0.1),
     loc_regularizer=None,
     untransformed_scale_regularizer=None,
     loc_constraint=None,
@@ -143,11 +145,12 @@ def default_mean_field_normal_fn(
     is_singular: Python `bool` if `True`, forces the special case limit of
       `scale->0`, i.e., a `Deterministic` distribution.
     loc_initializer: Initializer function for the `loc` parameters.
-      If `None` (default), values are initialized using the default
-      initializer used by `tf.get_variable`.
+      The default is `tf.random_normal_initializer(mean=0., stddev=0.1)`.
     untransformed_scale_initializer: Initializer function for the `scale`
-      parameters. If `None` (default), values are initialized using the default
-      initializer used by `tf.get_variable`.
+      parameters. Default value: `tf.random_normal_initializer(mean=-3.,
+      stddev=0.1)`. This implies the softplus transformed result is initialized
+      near `0`. It allows a `Normal` distribution with `scale` parameter set to
+      this value to approximately act like a point mass.
     loc_regularizer: Regularizer function for the `loc` parameters.
     untransformed_scale_regularizer: Regularizer function for the `scale`
       parameters.
@@ -168,13 +171,13 @@ def default_mean_field_normal_fn(
       using from args: `dtype, shape, name, trainable, add_variable_fn`.
   """
   loc_scale_fn_ = default_loc_scale_fn(
-      is_singular,
-      loc_initializer,
-      untransformed_scale_initializer,
-      loc_regularizer,
-      untransformed_scale_regularizer,
-      loc_constraint,
-      untransformed_scale_constraint)
+      is_singular=is_singular,
+      loc_initializer=loc_initializer,
+      untransformed_scale_initializer=untransformed_scale_initializer,
+      loc_regularizer=loc_regularizer,
+      untransformed_scale_regularizer=untransformed_scale_regularizer,
+      loc_constraint=loc_constraint,
+      untransformed_scale_constraint=untransformed_scale_constraint)
   def _fn(dtype, shape, name, trainable, add_variable_fn):
     """Creates multivariate `Deterministic` or `Normal` distribution."""
     loc, scale = loc_scale_fn_(dtype, shape, name, trainable, add_variable_fn)
