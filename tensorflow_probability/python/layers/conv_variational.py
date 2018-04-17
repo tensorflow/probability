BUGFIX: Make tfp.layers output scalar KL by making prior multivariate, instead of broadcasting against a scalar distribution
A previous commit incorrectly used a `tfd.Normal` prior with a vector batch rather than scalar batch. This is because `tfd.Independent` defaults to turning all but the first batch dimension into events. `reinterpreted_batch_ndims` must be set explicitly.

In addition, distribution regularizers are recommended to be obtained with `layer.losses` instead of `layer.get_losses_for(inputs=None)`.

PiperOrigin-RevId: 193124951

tensorflow_probability/python/layers/conv_variational.py
@@ -130,9 +130,7 @@ class _ConvVariational(tf.keras.layers.Layer):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -251,28 +249,16 @@ class _ConvVariational(tf.keras.layers.Layer):
     if self.activation is not None:
       outputs = self.activation(outputs)
     if not self._built_kernel_divergence:
-      kernel_posterior = self.kernel_posterior
-      kernel_prior = self.kernel_prior
-      if isinstance(self.kernel_posterior, tfd.Independent):
-        kernel_posterior = kernel_posterior.distribution
-      if isinstance(self.kernel_prior, tfd.Independent):
-        kernel_prior = kernel_prior.distribution
       self._apply_divergence(self.kernel_divergence_fn,
-                             kernel_posterior,
-                             kernel_prior,
+                             self.kernel_posterior,
+                             self.kernel_prior,
                              self.kernel_posterior_tensor,
                              name='divergence_kernel')
       self._built_kernel_divergence = True
     if not self._built_bias_divergence:
-      bias_posterior = self.bias_posterior
-      bias_prior = self.bias_prior
-      if isinstance(self.bias_posterior, tfd.Independent):
-        bias_posterior = bias_posterior.distribution
-      if isinstance(self.bias_prior, tfd.Independent):
-        bias_prior = bias_prior.distribution
       self._apply_divergence(self.bias_divergence_fn,
-                             bias_posterior,
-                             bias_prior,
+                             self.bias_posterior,
+                             self.bias_prior,
                              self.bias_posterior_tensor,
                              name='divergence_bias')
       self._built_bias_divergence = True
@@ -505,9 +491,7 @@ class _ConvReparameterization(_ConvVariational):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -624,14 +608,14 @@ class Conv1DReparameterization(_ConvReparameterization):
       tf.keras.layers.Reshape([128, 1]),
       tfp.layers.Convolution1DReparameterization(
           64, kernel_size=5, padding='SAME', activation=tf.nn.relu),
-      tf.keras.layers.Reshape([128 * 64]),
-      tfp.layers.DenseReparameterization(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseReparameterization(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -663,9 +647,7 @@ class Conv1DReparameterization(_ConvReparameterization):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -775,14 +757,14 @@ class Conv2DReparameterization(_ConvReparameterization):
       tf.keras.layers.MaxPooling2D(pool_size=[2, 2],
                                    strides=[2, 2],
                                    padding='SAME'),
-      tf.keras.layers.Reshape([16 * 16 * 64]),
-      tfp.layers.DenseReparameterization(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseReparameterization(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -814,9 +796,7 @@ class Conv2DReparameterization(_ConvReparameterization):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -932,14 +912,14 @@ class Conv3DReparameterization(_ConvReparameterization):
       tf.keras.layers.MaxPooling3D(pool_size=[2, 2, 2],
                                    strides=[2, 2, 2],
                                    padding='SAME'),
-      tf.keras.layers.Reshape([128 * 16 * 16 * 64]),
-      tfp.layers.DenseReparameterization(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseReparameterization(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -971,9 +951,7 @@ class Conv3DReparameterization(_ConvReparameterization):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -1099,9 +1077,7 @@ class _ConvFlipout(_ConvVariational):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -1287,14 +1263,14 @@ class Conv1DFlipout(_ConvFlipout):
       tf.keras.layers.Reshape([128, 1]),
       tfp.layers.Convolution1DFlipout(
           64, kernel_size=5, padding='SAME', activation=tf.nn.relu),
-      tf.keras.layers.Reshape([128 * 64]),
-      tfp.layers.DenseFlipout(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseFlipout(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -1327,9 +1303,7 @@ class Conv1DFlipout(_ConvFlipout):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -1446,14 +1420,14 @@ class Conv2DFlipout(_ConvFlipout):
       tf.keras.layers.MaxPooling2D(pool_size=[2, 2],
                                    strides=[2, 2],
                                    padding='SAME'),
-      tf.keras.layers.Reshape([16 * 16 * 64]),
-      tfp.layers.DenseFlipout(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseFlipout(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -1486,9 +1460,7 @@ class Conv2DFlipout(_ConvFlipout):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -1611,14 +1583,14 @@ class Conv3DFlipout(_ConvFlipout):
       tf.keras.layers.MaxPooling3D(pool_size=[2, 2, 2],
                                    strides=[2, 2, 2],
                                    padding='SAME'),
-      tf.keras.layers.Reshape([128 * 16 * 16 * 64]),
-      tfp.layers.DenseFlipout(10)
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseFlipout(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -1651,9 +1623,7 @@ class Conv3DFlipout(_ConvFlipout):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),

tensorflow_probability/python/layers/conv_variational_test.py
@@ -112,9 +112,10 @@ class ConvVariational(tf.test.TestCase):
       import tensorflow as tf  # pylint: disable=g-import-not-at-top,redefined-outer-name
       tfd = tf.contrib.distributions  # pylint: disable=redefined-outer-name
 
-      loc = tf.zeros(shape, dtype=dtype)
-      scale = tf.ones(shape, dtype=dtype)
-      return tfd.Independent(tfd.Normal(loc=loc, scale=scale))
+      dist = tfd.Normal(loc=tf.zeros(shape, dtype),
+                        scale=dtype.as_numpy_dtype(1))
+      batch_ndims = tf.size(dist.batch_shape_tensor())
+      return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
 
     if layer_class in (tfp.layers.Convolution1DReparameterization,
                        tfp.layers.Convolution1DFlipout):
@@ -152,27 +153,25 @@ class ConvVariational(tf.test.TestCase):
         inputs = tf.random_uniform([2, 3, 3, 3, 1], seed=1)
 
       # No keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 0)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 0)
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
       _ = layer(inputs)
 
       # Yes keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 1)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 1)
+      self.assertEqual(layer.losses[0].shape, ())
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
   def _testKLPenaltyBoth(self, layer_class):
-    def _make_normal(dtype, shape, *dummy_args):
-      return tfd.Independent(tfd.Normal(
-          loc=tf.zeros(shape, dtype), scale=dtype.as_numpy_dtype(1.)))
     with self.test_session():
       layer = layer_class(
           filters=2,
           kernel_size=3,
           bias_posterior_fn=tfp.layers.default_mean_field_normal_fn(),
-          bias_prior_fn=_make_normal)
+          bias_prior_fn=tfp.layers.default_multivariate_normal_fn)
       if layer_class in (tfp.layers.Convolution1DReparameterization,
                          tfp.layers.Convolution1DFlipout):
         inputs = tf.random_uniform([2, 3, 1], seed=1)
@@ -184,16 +183,18 @@ class ConvVariational(tf.test.TestCase):
         inputs = tf.random_uniform([2, 3, 3, 3, 1], seed=1)
 
       # No keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 0)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 0)
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
       _ = layer(inputs)
 
       # Yes keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 2)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 2)
+      self.assertEqual(layer.losses[0].shape, ())
+      self.assertEqual(layer.losses[1].shape, ())
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
   def _testConvSetUp(self, layer_class, batch_size, depth=None,
                      height=None, width=None, channels=None, filters=None,
@@ -225,7 +226,7 @@ class ConvVariational(tf.test.TestCase):
         result_log_prob=tf.random_uniform(kernel_shape, seed=seed()),
         result_sample=tf.random_uniform(kernel_shape, seed=seed()))
     kernel_divergence = MockKLDivergence(
-        result=tf.random_uniform(kernel_shape, seed=seed()))
+        result=tf.random_uniform([], seed=seed()))
 
     bias_size = (filters,)
     bias_posterior = MockDistribution(
@@ -235,7 +236,7 @@ class ConvVariational(tf.test.TestCase):
         result_log_prob=tf.random_uniform(bias_size, seed=seed()),
         result_sample=tf.random_uniform(bias_size, seed=seed()))
     bias_divergence = MockKLDivergence(
-        result=tf.random_uniform(bias_size, seed=seed()))
+        result=tf.random_uniform([], seed=seed()))
 
     layer = layer_class(
         filters=filters,
@@ -308,15 +309,11 @@ class ConvVariational(tf.test.TestCase):
           rtol=1e-6, atol=0.)
 
       self.assertAllEqual(
-          [[kernel_posterior.distribution,
-            kernel_prior.distribution,
-            kernel_posterior.result_sample]],
+          [[kernel_posterior, kernel_prior, kernel_posterior.result_sample]],
           kernel_divergence.args)
 
       self.assertAllEqual(
-          [[bias_posterior.distribution,
-            bias_prior.distribution,
-            bias_posterior.result_sample]],
+          [[bias_posterior, bias_prior, bias_posterior.result_sample]],
           bias_divergence.args)
 
   def _testConvFlipout(self, layer_class):
@@ -412,13 +409,11 @@ class ConvVariational(tf.test.TestCase):
           rtol=1e-6, atol=0.)
 
       self.assertAllEqual(
-          [[kernel_posterior.distribution, kernel_prior.distribution, None]],
+          [[kernel_posterior, kernel_prior, None]],
           kernel_divergence.args)
 
       self.assertAllEqual(
-          [[bias_posterior.distribution,
-            bias_prior.distribution,
-            bias_posterior.result_sample]],
+          [[bias_posterior, bias_prior, bias_posterior.result_sample]],
           bias_divergence.args)
 
   def _testRandomConvFlipout(self, layer_class):
@@ -468,8 +463,10 @@ class ConvVariational(tf.test.TestCase):
           padding='SAME',
           kernel_posterior_fn=lambda *args: kernel_posterior,
           kernel_posterior_tensor_fn=lambda d: d.sample(seed=42),
+          kernel_divergence_fn=None,
           bias_posterior_fn=lambda *args: bias_posterior,
           bias_posterior_tensor_fn=lambda d: d.sample(seed=43),
+          bias_divergence_fn=None,
           seed=44)
       layer_two = layer_class(
           filters=filters,
@@ -477,8 +474,10 @@ class ConvVariational(tf.test.TestCase):
           padding='SAME',
           kernel_posterior_fn=lambda *args: kernel_posterior,
           kernel_posterior_tensor_fn=lambda d: d.sample(seed=42),
+          kernel_divergence_fn=None,
           bias_posterior_fn=lambda *args: bias_posterior,
           bias_posterior_tensor_fn=lambda d: d.sample(seed=43),
+          bias_divergence_fn=None,
           seed=45)
 
       outputs_one = layer_one(inputs)

tensorflow_probability/python/layers/dense_variational.py
@@ -111,9 +111,7 @@ class _DenseVariational(tf.keras.layers.Layer):
       activity_regularizer=None,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(is_singular=True),  # pylint: disable=line-too-long
       bias_posterior_tensor_fn=lambda d: d.sample(),
@@ -189,28 +187,16 @@ class _DenseVariational(tf.keras.layers.Layer):
     if self.activation is not None:
       outputs = self.activation(outputs)  # pylint: disable=not-callable
     if not self._built_kernel_divergence:
-      kernel_posterior = self.kernel_posterior
-      kernel_prior = self.kernel_prior
-      if isinstance(self.kernel_posterior, tfd.Independent):
-        kernel_posterior = kernel_posterior.distribution
-      if isinstance(self.kernel_prior, tfd.Independent):
-        kernel_prior = kernel_prior.distribution
       self._apply_divergence(self.kernel_divergence_fn,
-                             kernel_posterior,
-                             kernel_prior,
+                             self.kernel_posterior,
+                             self.kernel_prior,
                              self.kernel_posterior_tensor,
                              name='divergence_kernel')
       self._built_kernel_divergence = True
     if not self._built_bias_divergence:
-      bias_posterior = self.bias_posterior
-      bias_prior = self.bias_prior
-      if isinstance(self.bias_posterior, tfd.Independent):
-        bias_posterior = bias_posterior.distribution
-      if isinstance(self.bias_prior, tfd.Independent):
-        bias_prior = bias_prior.distribution
       self._apply_divergence(self.bias_divergence_fn,
-                             bias_posterior,
-                             bias_prior,
+                             self.bias_posterior,
+                             self.bias_prior,
                              self.bias_posterior_tensor,
                              name='divergence_bias')
       self._built_bias_divergence = True
@@ -386,13 +372,13 @@ class DenseReparameterization(_DenseVariational):
 
   model = tf.keras.Sequential([
       tfp.layers.DenseReparameterization(512, activation=tf.nn.relu),
-      tfp.layers.DenseReparameterization(10)
+      tfp.layers.DenseReparameterization(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -420,9 +406,7 @@ class DenseReparameterization(_DenseVariational):
       trainable=True,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(
           is_singular=True),
@@ -505,13 +489,13 @@ class DenseLocalReparameterization(_DenseVariational):
 
   model = tf.keras.Sequential([
       tfp.layers.DenseReparameterization(512, activation=tf.nn.relu),
-      tfp.layers.DenseReparameterization(10)
+      tfp.layers.DenseReparameterization(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -539,9 +523,7 @@ class DenseLocalReparameterization(_DenseVariational):
       trainable=True,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(
           is_singular=True),
@@ -638,13 +620,13 @@ class DenseFlipout(_DenseVariational):
 
   model = tf.keras.Sequential([
       tfp.layers.DenseFlipout(512, activation=tf.nn.relu),
-      tfp.layers.DenseFlipout(10)
+      tfp.layers.DenseFlipout(10),
   ])
 
   logits = model(features)
   neg_log_likelihood = tf.nn.softmax_cross_entropy_with_logits(
       labels=labels, logits=logits)
-  kl = sum(model.get_losses_for(inputs=None))
+  kl = sum(model.losses)
   loss = neg_log_likelihood + kl
   train_op = tf.train.AdamOptimizer().minimize(loss)
   ```
@@ -673,9 +655,7 @@ class DenseFlipout(_DenseVariational):
       trainable=True,
       kernel_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(),
       kernel_posterior_tensor_fn=lambda d: d.sample(),
-      kernel_prior_fn=lambda dtype, shape, *dummy_args: tfd.Independent(  # pylint: disable=g-long-lambda
-          tfd.Normal(loc=tf.zeros(shape, dtype),
-                     scale=dtype.as_numpy_dtype(1.))),
+      kernel_prior_fn=tfp_layers_util.default_multivariate_normal_fn,
       kernel_divergence_fn=lambda q, p, ignore: tfd.kl_divergence(q, p),
       bias_posterior_fn=tfp_layers_util.default_mean_field_normal_fn(
           is_singular=True),

tensorflow_probability/python/layers/dense_variational_test.py
@@ -110,9 +110,9 @@ class DenseVariational(tf.test.TestCase):
       import tensorflow as tf  # pylint: disable=g-import-not-at-top,redefined-outer-name
       tfd = tf.contrib.distributions  # pylint: disable=redefined-outer-name
 
-      loc = tf.zeros(shape, dtype=dtype)
-      scale = tf.ones(shape, dtype=dtype)
-      return tfd.Independent(tfd.Normal(loc=loc, scale=scale))
+      dist = tfd.Normal(loc=tf.zeros(shape, dtype), scale=tf.ones(shape, dtype))
+      batch_ndims = tf.size(dist.batch_shape_tensor())
+      return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
 
     kwargs = {'units': 3,
               'kernel_posterior_fn': kernel_posterior_fn,
@@ -136,39 +136,39 @@ class DenseVariational(tf.test.TestCase):
       inputs = tf.random_uniform([2, 3], seed=1)
 
       # No keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 0)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 0)
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
       _ = layer(inputs)
 
       # Yes keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 1)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 1)
+      self.assertEqual(layer.losses[0].shape, ())
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
   def _testKLPenaltyBoth(self, layer_class):
-    def _make_normal(dtype, shape, *dummy_args):
-      return tfd.Independent(tfd.Normal(
-          loc=tf.zeros(shape, dtype), scale=dtype.as_numpy_dtype(1.)))
     with self.test_session():
       layer = layer_class(
           units=2,
           bias_posterior_fn=tfp.layers.default_mean_field_normal_fn(),
-          bias_prior_fn=_make_normal)
+          bias_prior_fn=tfp.layers.default_multivariate_normal_fn)
       inputs = tf.random_uniform([2, 3], seed=1)
 
       # No keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 0)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 0)
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
       _ = layer(inputs)
 
       # Yes keys.
-      losses = layer.get_losses_for(inputs=None)
-      self.assertEqual(len(losses), 2)
-      self.assertListEqual(layer.losses, losses)
+      input_dependent_losses = layer.get_losses_for(inputs=None)
+      self.assertEqual(len(layer.losses), 2)
+      self.assertEqual(layer.losses[0].shape, ())
+      self.assertEqual(layer.losses[1].shape, ())
+      self.assertListEqual(layer.losses, input_dependent_losses)
 
   def _testDenseSetUp(self, layer_class, batch_size, in_size, out_size,
                       **kwargs):
@@ -185,7 +185,7 @@ class DenseVariational(tf.test.TestCase):
         result_log_prob=tf.random_uniform(kernel_size, seed=seed()),
         result_sample=tf.random_uniform(kernel_size, seed=seed()))
     kernel_divergence = MockKLDivergence(
-        result=tf.random_uniform(kernel_size, seed=seed()))
+        result=tf.random_uniform([], seed=seed()))
 
     bias_size = [out_size]
     bias_posterior = MockDistribution(
@@ -195,7 +195,7 @@ class DenseVariational(tf.test.TestCase):
         result_log_prob=tf.random_uniform(bias_size, seed=seed()),
         result_sample=tf.random_uniform(bias_size, seed=seed()))
     bias_divergence = MockKLDivergence(
-        result=tf.random_uniform(bias_size, seed=seed()))
+        result=tf.random_uniform([], seed=seed()))
 
     layer = layer_class(
         units=out_size,
@@ -287,15 +287,11 @@ class DenseVariational(tf.test.TestCase):
           rtol=1e-6, atol=0.)
 
       self.assertAllEqual(
-          [[kernel_posterior.distribution,
-            kernel_prior.distribution,
-            kernel_posterior.result_sample]],
+          [[kernel_posterior, kernel_prior, kernel_posterior.result_sample]],
           kernel_divergence.args)
 
       self.assertAllEqual(
-          [[bias_posterior.distribution,
-            bias_prior.distribution,
-            bias_posterior.result_sample]],
+          [[bias_posterior, bias_prior, bias_posterior.result_sample]],
           bias_divergence.args)
 
   def testDenseLocalReparameterization(self):
@@ -342,15 +338,11 @@ class DenseVariational(tf.test.TestCase):
           rtol=1e-6, atol=0.)
 
       self.assertAllEqual(
-          [[kernel_posterior.distribution,
-            kernel_prior.distribution,
-            None]],
+          [[kernel_posterior, kernel_prior, None]],
           kernel_divergence.args)
 
       self.assertAllEqual(
-          [[bias_posterior.distribution,
-            bias_prior.distribution,
-            bias_posterior.result_sample]],
+          [[bias_posterior, bias_prior, bias_posterior.result_sample]],
           bias_divergence.args)
 
   def testDenseFlipout(self):
@@ -417,13 +409,11 @@ class DenseVariational(tf.test.TestCase):
           rtol=1e-6, atol=0.)
 
       self.assertAllEqual(
-          [[kernel_posterior.distribution, kernel_prior.distribution, None]],
+          [[kernel_posterior, kernel_prior, None]],
           kernel_divergence.args)
 
       self.assertAllEqual(
-          [[bias_posterior.distribution,
-            bias_prior.distribution,
-            bias_posterior.result_sample]],
+          [[bias_posterior, bias_prior, bias_posterior.result_sample]],
           bias_divergence.args)
 
   def testRandomDenseFlipout(self):
@@ -454,15 +444,19 @@ class DenseVariational(tf.test.TestCase):
           units=out_size,
           kernel_posterior_fn=lambda *args: kernel_posterior,
           kernel_posterior_tensor_fn=lambda d: d.sample(seed=42),
+          kernel_divergence_fn=None,
           bias_posterior_fn=lambda *args: bias_posterior,
           bias_posterior_tensor_fn=lambda d: d.sample(seed=43),
+          bias_divergence_fn=None,
           seed=44)
       layer_two = tfp.layers.DenseFlipout(
           units=out_size,
           kernel_posterior_fn=lambda *args: kernel_posterior,
           kernel_posterior_tensor_fn=lambda d: d.sample(seed=42),
+          kernel_divergence_fn=None,
           bias_posterior_fn=lambda *args: bias_posterior,
           bias_posterior_tensor_fn=lambda d: d.sample(seed=43),
+          bias_divergence_fn=None,
           seed=45)
 
       outputs_one = layer_one(inputs)

tensorflow_probability/python/layers/util.py
@@ -31,6 +31,7 @@ tfd = tf.contrib.distributions
 __all__ = [
     'default_loc_scale_fn',
     'default_mean_field_normal_fn',
+    'default_multivariate_normal_fn',
     'deserialize_function',
     'random_sign',
     'serialize_function',
@@ -129,17 +130,7 @@ def default_mean_field_normal_fn(
   """Creates a function to build Normal distributions with trainable params.
 
   This function produces a closure which produces `tf.distributions.Normal`
-  parameterized by a loc` and `scale` each created using `tf.get_variable`. The
-  produced closure accepts the following arguments:
-
-    name: Python `str` name prepended to any created (or existing)
-      `tf.Variable`s.
-    shape: Python `list`-like representing the parameter's event shape.
-    dtype: Type of parameter's event.
-    trainable: Python `bool` indicating all created `tf.Variable`s should be
-      added to the graph collection `GraphKeys.TRAINABLE_VARIABLES`.
-    add_variable_fn: `tf.get_variable`-like `callable` used to create (or
-      access existing) `tf.Variable`s.
+  parameterized by a loc` and `scale` each created using `tf.get_variable`.
 
   Args:
     is_singular: Python `bool` if `True`, forces the special case limit of
@@ -170,7 +161,7 @@ def default_mean_field_normal_fn(
     make_normal_fn: Python `callable` which creates a `tf.distributions.Normal`
       using from args: `dtype, shape, name, trainable, add_variable_fn`.
   """
-  loc_scale_fn_ = default_loc_scale_fn(
+  loc_scale_fn = default_loc_scale_fn(
       is_singular=is_singular,
       loc_initializer=loc_initializer,
       untransformed_scale_initializer=untransformed_scale_initializer,
@@ -179,16 +170,52 @@ def default_mean_field_normal_fn(
       loc_constraint=loc_constraint,
       untransformed_scale_constraint=untransformed_scale_constraint)
   def _fn(dtype, shape, name, trainable, add_variable_fn):
-    """Creates multivariate `Deterministic` or `Normal` distribution."""
-    loc, scale = loc_scale_fn_(dtype, shape, name, trainable, add_variable_fn)
+    """Creates multivariate `Deterministic` or `Normal` distribution.
+
+    Args:
+      dtype: Type of parameter's event.
+      shape: Python `list`-like representing the parameter's event shape.
+      name: Python `str` name prepended to any created (or existing)
+        `tf.Variable`s.
+      trainable: Python `bool` indicating all created `tf.Variable`s should be
+        added to the graph collection `GraphKeys.TRAINABLE_VARIABLES`.
+      add_variable_fn: `tf.get_variable`-like `callable` used to create (or
+        access existing) `tf.Variable`s.
+
+    Returns:
+      Multivariate `Deterministic` or `Normal` distribution.
+    """
+    loc, scale = loc_scale_fn(dtype, shape, name, trainable, add_variable_fn)
     if scale is None:
       dist = tfd.Deterministic(loc=loc)
     else:
       dist = tfd.Normal(loc=loc, scale=scale)
-    reinterpreted_batch_ndims = tf.shape(dist.batch_shape_tensor())[0]
-    return tfd.Independent(
-        dist, reinterpreted_batch_ndims=reinterpreted_batch_ndims)
+    batch_ndims = tf.size(dist.batch_shape_tensor())
+    return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
   return _fn
+
+
+def default_multivariate_normal_fn(dtype, shape, name, trainable,
+                                   add_variable_fn):
+  """Creates multivariate standard `Normal` distribution.
+
+  Args:
+    dtype: Type of parameter's event.
+    shape: Python `list`-like representing the parameter's event shape.
+    name: Python `str` name prepended to any created (or existing)
+      `tf.Variable`s.
+    trainable: Python `bool` indicating all created `tf.Variable`s should be
+      added to the graph collection `GraphKeys.TRAINABLE_VARIABLES`.
+    add_variable_fn: `tf.get_variable`-like `callable` used to create (or
+      access existing) `tf.Variable`s.
+
+  Returns:
+    Multivariate standard `Normal` distribution.
+  """
+  del name, trainable, add_variable_fn   # unused
+  dist = tfd.Normal(loc=tf.zeros(shape, dtype), scale=dtype.as_numpy_dtype(1))
+  batch_ndims = tf.size(dist.batch_shape_tensor())
+  return tfd.Independent(dist, reinterpreted_batch_ndims=batch_ndims)
 
 
 def deserialize_function(serial, function_type):