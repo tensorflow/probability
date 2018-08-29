0d10c979eeb730ceb8b4dcd2511ec5978f073344
Googler <no-reply@google.com>
2018-08-29 13:58:48 -0700

    Add TestCase class to provide TFP specific test features
    PiperOrigin-RevId: 210784890

---
+++ tensorflow_probability/python/internal/test_case_test.py
@@ -0,0 +1,93 @@
+# Copyright 2018 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests for TensorFlow Probability TestCase class."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Dependency imports
+import numpy as np
+import tensorflow as tf
+
+from tensorflow_probability.python.internal import test_case
+from tensorflow.python.framework import test_util
+
+
+class _TestCaseTest(object):
+
+  def test_assert_all_finite_input_finite(self):
+    minval = tf.constant(self.dtype.min, dtype=self.dtype)
+    maxval = tf.constant(self.dtype.max, dtype=self.dtype)
+
+    # This tests if the minimum value for the dtype is detected as finite.
+    self.assertAllFinite(minval)
+
+    # This tests if the maximum value for the dtype is detected as finite.
+    self.assertAllFinite(maxval)
+
+    # This tests if a rank 3 `Tensor` with entries in the range
+    # [0.4*minval, 0.4*maxval] is detected as finite.
+    # The choice of range helps to avoid overflows or underflows
+    # in tf.linspace calculations.
+    num_elem = 1000
+    shape = (10, 10, 10)
+    a = tf.reshape(tf.linspace(0.4*minval, 0.4*maxval, num_elem), shape)
+    self.assertAllFinite(a)
+
+  def test_assert_all_finite_input_nan(self):
+    # This tests if np.nan is detected as non-finite.
+    num_elem = 1000
+    shape = (10, 10, 10)
+    a = np.linspace(0., 1., num_elem)
+    a[50] = np.nan
+    a = tf.reshape(tf.convert_to_tensor(a, dtype=self.dtype), shape)
+    with self.assertRaisesRegexp(AssertionError, "Arrays are not equal"):
+      self.assertAllFinite(a)
+
+  def test_assert_all_finite_input_inf(self):
+    # This tests if np.inf is detected as non-finite.
+    num_elem = 1000
+    shape = (10, 10, 10)
+    a = np.linspace(0., 1., num_elem)
+    a[100] = np.inf
+    a = tf.reshape(tf.convert_to_tensor(a, dtype=self.dtype), shape)
+    with self.assertRaisesRegexp(AssertionError, "Arrays are not equal"):
+      self.assertAllFinite(a)
+
+  def test_assert_all_finite_input_py_literal(self):
+    # This tests if finite Python literals are detected as finite.
+    for a in [1, 3., -7.1e-12]:
+      self.assertAllFinite(a)
+
+    b = [0, 1.45e17, 0x2a]
+    self.assertAllFinite(b)
+
+    c = (1, 2., 3, 4)
+    self.assertAllFinite(c)
+
+
+@test_util.run_all_in_graph_and_eager_modes
+class TestCaseTestFloat32(test_case.TestCase, _TestCaseTest):
+  dtype = tf.float32
+
+
+@test_util.run_all_in_graph_and_eager_modes
+class TestCaseTestFloat64(test_case.TestCase, _TestCaseTest):
+  dtype = tf.float64
+
+
+if __name__ == "__main__":
+  tf.test.main()
