e00ab7df4b3865054a39f8af64e85e31123aba5c
Dustin Tran <trandustin@google.com>
2018-09-06 09:51:17 -0700

    Make style changes to classifier examples to be consistent with others.
    Motivated by https://github.com/tensorflow/probability/pull/155#discussion_r215524715.
    
    PiperOrigin-RevId: 211822890

--- tensorflow_probability/examples/logistic_regression.py
+++ tensorflow_probability/examples/logistic_regression.py
@@ -72,7 +72,7 @@ def toy_logistic_data(num_examples, input_size=2, weights_prior_stddev=5.0):
        1]^{input_size}`, as a Numpy `array` of shape `(num_examples,
        input_size)`.
     labels: Labels sampled from the logistic model `p(label=1) =
-      logistic(dot(inputs, random_weights) + random_bias)`, as a Numpy
+      logistic(dot(features, random_weights) + random_bias)`, as a Numpy
       `int32` `array` of shape `(num_examples, 1)`.
   """
   random_weights = weights_prior_stddev * np.random.randn(input_size)
@@ -86,16 +86,16 @@ def toy_logistic_data(num_examples, input_size=2, weights_prior_stddev=5.0):
   return random_weights, random_bias, np.float32(design_matrix), labels
 
 
-def visualize_decision(inputs, labels, true_w_b, candidate_w_bs, fname):
+def visualize_decision(features, labels, true_w_b, candidate_w_bs, fname):
   """Utility method to visualize decision boundaries in R^2.
 
   Args:
-    inputs: Input points, as a Numpy `array` of shape `[num_examples, 2]`.
+    features: Input points, as a Numpy `array` of shape `[num_examples, 2]`.
     labels: Numpy `float`-like array of shape `[num_examples, 1]` giving a
       label for each point.
     true_w_b: A `tuple` `(w, b)` where `w` is a Numpy array of
        shape `[2]` and `b` is a scalar `float`, interpreted as a
-       decision rule of the form `dot(inputs, w) + b > 0`.
+       decision rule of the form `dot(features, w) + b > 0`.
     candidate_w_bs: Python `iterable` containing tuples of the same form as
        true_w_b.
     fname: The filename to save the plot as a PNG image (Python `str`).
@@ -103,7 +103,7 @@ def visualize_decision(inputs, labels, true_w_b, candidate_w_bs, fname):
   fig = figure.Figure(figsize=(6, 6))
   canvas = backend_agg.FigureCanvasAgg(fig)
   ax = fig.add_subplot(1, 1, 1)
-  ax.scatter(inputs[:, 0], inputs[:, 1],
+  ax.scatter(features[:, 0], features[:, 1],
              c=np.float32(labels[:, 0]),
              cmap=cm.get_cmap("binary"))
 
@@ -134,12 +134,12 @@ def build_input_pipeline(x, y, batch_size):
   """Build a Dataset iterator for supervised classification.
 
   Args:
-    x: Numpy `array` of inputs, indexed by the first dimension.
+    x: Numpy `array` of features, indexed by the first dimension.
     y: Numpy `array` of labels, with the same first dimension as `x`.
     batch_size: Number of elements in each training batch.
 
   Returns:
-    batch_data: `Tensor` feed  inputs, of shape
+    batch_features: `Tensor` feed  features, of shape
       `[batch_size] + x.shape[1:]`.
     batch_labels: `Tensor` feed of labels, of shape
       `[batch_size] + y.shape[1:]`.
@@ -147,8 +147,8 @@ def build_input_pipeline(x, y, batch_size):
   training_dataset = tf.data.Dataset.from_tensor_slices((x, y))
   training_batches = training_dataset.repeat().batch(batch_size)
   training_iterator = training_batches.make_one_shot_iterator()
-  batch_data, batch_labels = training_iterator.get_next()
-  return batch_data, batch_labels
+  batch_features, batch_labels = training_iterator.get_next()
+  return batch_features, batch_labels
 
 
 def main(argv):
@@ -161,61 +161,61 @@ def main(argv):
 
   # Generate (and visualize) a toy classification dataset.
   w_true, b_true, x, y = toy_logistic_data(FLAGS.num_examples, 2)
+  features, labels = build_input_pipeline(x, y, FLAGS.batch_size)
 
-  with tf.Graph().as_default():
-    inputs, labels = build_input_pipeline(x, y, FLAGS.batch_size)
+  # Define a logistic regression model as a Bernoulli distribution
+  # parameterized by logits from a single linear layer. We use the Flipout
+  # Monte Carlo estimator for the layer: this enables lower variance
+  # stochastic gradients than naive reparameterization.
+  with tf.name_scope("logistic_regression", values=[features]):
+    layer = tfp.layers.DenseFlipout(
+        units=1,
+        activation=None,
+        kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(),
+        bias_posterior_fn=tfp.layers.default_mean_field_normal_fn())
+    logits = layer(features)
+    labels_distribution = tfd.Bernoulli(logits=logits)
 
-    # Define a logistic regression model as a Bernoulli distribution
-    # parameterized by logits from a single linear layer. We use the Flipout
-    # Monte Carlo estimator for the layer: this enables lower variance
-    # stochastic gradients than naive reparameterization.
-    with tf.name_scope("logistic_regression", values=[inputs]):
-      layer = tfp.layers.DenseFlipout(
-          units=1,
-          activation=None,
-          kernel_posterior_fn=tfp.layers.default_mean_field_normal_fn(),
-          bias_posterior_fn=tfp.layers.default_mean_field_normal_fn())
-      logits = layer(inputs)
-      labels_distribution = tfd.Bernoulli(logits=logits)
+  # Compute the -ELBO as the loss, averaged over the batch size.
+  neg_log_likelihood = -tf.reduce_mean(labels_distribution.log_prob(labels))
+  kl = sum(layer.losses) / FLAGS.num_examples
+  elbo_loss = neg_log_likelihood + kl
 
-    # Compute the -ELBO as the loss, averaged over the batch size.
-    neg_log_likelihood = -tf.reduce_mean(labels_distribution.log_prob(labels))
-    kl = sum(layer.losses) / FLAGS.num_examples
-    elbo_loss = neg_log_likelihood + kl
+  # Build metrics for evaluation. Predictions are formed from a single forward
+  # pass of the probabilistic layers. They are cheap but noisy predictions.
+  predictions = tf.cast(logits > 0, dtype=tf.int32)
+  accuracy, accuracy_update_op = tf.metrics.accuracy(
+      labels=labels, predictions=predictions)
 
-    # Build metrics for evaluation. Predictions are formed from a single forward
-    # pass of the probabilistic layers. They are cheap but noisy predictions.
-    predictions = tf.cast(logits > 0, dtype=tf.int32)
-    accuracy, accuracy_update_op = tf.metrics.accuracy(
-        labels=labels, predictions=predictions)
+  with tf.name_scope("train"):
+    optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
+    train_op = optimizer.minimize(elbo_loss)
 
-    with tf.name_scope("train"):
-      opt = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
+  init_op = tf.group(tf.global_variables_initializer(),
+                     tf.local_variables_initializer())
 
-      train_op = opt.minimize(elbo_loss)
-      with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(tf.local_variables_initializer())
+  with tf.Session() as sess:
+    sess.run(init_op)
 
-        # Fit the model to data.
-        for step in range(FLAGS.max_steps):
-          _ = sess.run([train_op, accuracy_update_op])
-          if step % 100 == 0:
-            loss_value, accuracy_value = sess.run([elbo_loss, accuracy])
-            print("Step: {:>3d} Loss: {:.3f} Accuracy: {:.3f}".format(
-                step, loss_value, accuracy_value))
+    # Fit the model to data.
+    for step in range(FLAGS.max_steps):
+      _ = sess.run([train_op, accuracy_update_op])
+      if step % 100 == 0:
+        loss_value, accuracy_value = sess.run([elbo_loss, accuracy])
+        print("Step: {:>3d} Loss: {:.3f} Accuracy: {:.3f}".format(
+            step, loss_value, accuracy_value))
 
-        # Visualize some draws from the weights posterior.
-        w_draw = layer.kernel_posterior.sample()
-        b_draw = layer.bias_posterior.sample()
-        candidate_w_bs = []
-        for _ in range(FLAGS.num_monte_carlo):
-          w, b = sess.run((w_draw, b_draw))
-          candidate_w_bs.append((w, b))
-        visualize_decision(x, y, (w_true, b_true),
-                           candidate_w_bs,
-                           fname=os.path.join(FLAGS.model_dir,
-                                              "weights_inferred.png"))
+    # Visualize some draws from the weights posterior.
+    w_draw = layer.kernel_posterior.sample()
+    b_draw = layer.bias_posterior.sample()
+    candidate_w_bs = []
+    for _ in range(FLAGS.num_monte_carlo):
+      w, b = sess.run((w_draw, b_draw))
+      candidate_w_bs.append((w, b))
+    visualize_decision(x, y, (w_true, b_true),
+                       candidate_w_bs,
+                       fname=os.path.join(FLAGS.model_dir,
+                                          "weights_inferred.png"))
 
 if __name__ == "__main__":
   tf.app.run()
