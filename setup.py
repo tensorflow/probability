2e8600db32d2eaaaf24ac4169fe111fc08c0ee1d
Christopher Suter <cgs@google.com>
2018-10-29 16:30:45 -0400

    Bump TF version dependency up to 1.12.0-rc2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ REQUIRED_PACKAGES = [
     'numpy >= 1.13.3',
 ]
 
-REQUIRED_TENSORFLOW_VERSION = '1.12.0-rc1'
+REQUIRED_TENSORFLOW_VERSION = '1.12.0-rc2'
 
 if '--gpu' in sys.argv:
   use_gpu = True
