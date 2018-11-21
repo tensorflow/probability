7ea6cc55e5b3fed04372cd188cd0764e92fd3cf4
Dave Moore <davmre@google.com>
2018-11-21 12:57:20 -0800

    Fix name in module docstring.
    PiperOrigin-RevId: 222449089

--- tensorflow_probability/python/sts/seasonal_test.py
+++ tensorflow_probability/python/sts/seasonal_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Local Linear Trend State Space Model Tests."""
+"""Seasonal Model Tests."""
 
 from __future__ import absolute_import
 from __future__ import division
