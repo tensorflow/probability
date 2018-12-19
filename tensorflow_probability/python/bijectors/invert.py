eec232e8f2812f586c8a6cbcc645539eaa996eaf
Christopher Suter <cgs@google.com>
2018-12-19 10:48:17 -0800

    Make Invert Bijector call public methods instead of private ones.
    I am increasingly certain this should be the rule in general, for any meta-Foo
    (meta-Distribution, meta-Bijector, meta-PositiveSemidefiniteKernel). The need
    for a pylint override should always be a hint that we're doing something fishy.
    In this case, I needed to make this change to correctly address a caching issue
    in the base Bijector class. There was some whacky ping-ponging going on down
    there, where the actual functioning of Invert depended on the base Bijector's
    try/except logic for figuring out which methods to use. I'm in the process of
    eliminating that try/except logic in favor of explicit existence checks and
    this class's overriding of the private methods was causing problems for that
    change.
    
    PiperOrigin-RevId: 226197773

--- tensorflow_probability/python/bijectors/invert.py
+++ tensorflow_probability/python/bijectors/invert.py
@@ -74,30 +74,30 @@ class Invert(bijector_lib.Bijector):
         dtype=bijector.dtype,
         name=name or "_".join(["invert", bijector.name]))
 
-  def _forward_event_shape(self, input_shape):
-    return self.bijector._inverse_event_shape(input_shape)  # pylint: disable=protected-access
+  def forward_event_shape(self, input_shape):
+    return self.bijector.inverse_event_shape(input_shape)
 
-  def _forward_event_shape_tensor(self, input_shape):
-    return self.bijector._inverse_event_shape_tensor(input_shape)  # pylint: disable=protected-access
+  def forward_event_shape_tensor(self, input_shape):
+    return self.bijector.inverse_event_shape_tensor(input_shape)
 
-  def _inverse_event_shape(self, output_shape):
-    return self.bijector._forward_event_shape(output_shape)  # pylint: disable=protected-access
+  def inverse_event_shape(self, output_shape):
+    return self.bijector.forward_event_shape(output_shape)
 
-  def _inverse_event_shape_tensor(self, output_shape):
-    return self.bijector._forward_event_shape_tensor(output_shape)  # pylint: disable=protected-access
+  def inverse_event_shape_tensor(self, output_shape):
+    return self.bijector.forward_event_shape_tensor(output_shape)
 
   @property
   def bijector(self):
     return self._bijector
 
-  def _forward(self, x, **kwargs):
-    return self.bijector._inverse(x, **kwargs)  # pylint: disable=protected-access
+  def forward(self, x):
+    return self.bijector.inverse(x)
 
-  def _inverse(self, y, **kwargs):
-    return self.bijector._forward(y, **kwargs)  # pylint: disable=protected-access
+  def inverse(self, y):
+    return self.bijector.forward(y)
 
-  def _inverse_log_det_jacobian(self, y, **kwargs):
-    return self.bijector._forward_log_det_jacobian(y, **kwargs)  # pylint: disable=protected-access
+  def inverse_log_det_jacobian(self, y, event_ndims):
+    return self.bijector.forward_log_det_jacobian(y, event_ndims)
 
-  def _forward_log_det_jacobian(self, x, **kwargs):
-    return self.bijector._inverse_log_det_jacobian(x, **kwargs)  # pylint: disable=protected-access
+  def forward_log_det_jacobian(self, x, event_ndims):
+    return self.bijector.inverse_log_det_jacobian(x, event_ndims)
