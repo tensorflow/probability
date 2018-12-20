d75561a12bc00bbd39147b372aea672d1f955f6f
Googler <no-reply@google.com>
2018-12-20 10:08:39 -0800

    Adds `posterior_marginals` method to `HiddenMarkovModel` class.
    This function computes, for each time step, the marginal
    conditional probability that the hidden Markov model was in
    each possible state given the observations that were made
    at each time step.
    It uses a form of the Forward-Backward algorithm.
    
    PiperOrigin-RevId: 226353228

--- tensorflow_probability/python/distributions/hidden_markov_model_test.py
+++ tensorflow_probability/python/distributions/hidden_markov_model_test.py
@@ -145,7 +145,8 @@ class _HiddenMarkovModelTest(test_util.VectorDistributionTestHelpers,
     transition_matrix_ = tf.constant([[[0.8, 0.2],
                                        [0.3, 0.7]],
                                       [[0.9, 0.1],
-                                       [0.2, 0.8]]], dtype=self.dtype)
+                                       [0.2, 0.8]]],
+                                     dtype=self.dtype)
     observation_locs_ = tf.constant([0.0, 1.0], dtype=self.dtype)
     observation_scale_ = tf.constant(0.5, dtype=self.dtype)
 
@@ -325,6 +326,86 @@ class _HiddenMarkovModelTest(test_util.VectorDistributionTestHelpers,
     self.run_test_sample_consistent_mean_variance(self.evaluate, model,
                                                   num_samples=100000,
                                                   rtol=0.02)
+
+  def test_single_path_posterior_marginals(self):
+
+    # In this test we have a 9-vertex graph with precisely one
+    # 7-vertex path from vertex 0 to vertex 8.
+    # The hidden Markov model is a random walk on this
+    # graph where the only observations are
+    # "observed at 0", "observed in {1, 2, ..., 7}",
+    # "observed at 8".
+    # The purpose of this test is to ensure that transition
+    # and observation matrices with many log probabilities
+    # equal to -infinity, and where the result contains many
+    # -infinities, are handled correctly.
+
+    initial_prob = tf.constant(np.ones(9) / 9.0, dtype=self.dtype)
+    edges = [(0, 1), (1, 2), (2, 3), (3, 4),
+             (4, 6), (2, 5), (5, 6), (6, 7),
+             (6, 8)]
+    transition_matrix = np.zeros((9, 9))
+    for (i, j) in edges:
+      transition_matrix[i, j] = 1.
+      transition_matrix[j, i] = 1.
+    transition_matrix = tf.constant(
+        transition_matrix / np.sum(transition_matrix, axis=1, keepdims=True),
+        dtype=self.dtype)
+    observation_probs = tf.constant(
+        np.block([[1, 0, 0],
+                  [np.zeros((7, 1)), np.ones((7, 1)), np.zeros((7, 1))],
+                  [0, 0, 1]]),
+        dtype=self.dtype)
+
+    model = tfd.HiddenMarkovModel(tfd.Categorical(probs=initial_prob),
+                                  tfd.Categorical(probs=transition_matrix),
+                                  tfd.Categorical(probs=observation_probs),
+                                  num_steps=7)
+
+    observations = [0, 1, 1, 1, 1, 1, 2]
+
+    probs = model.posterior_marginals(observations).probs
+    expected_probs = np.eye(9)[[0, 1, 2, 3, 4, 6, 8]]
+
+    self.assertAllClose(probs, expected_probs, rtol=1e-4, atol=0.0)
+
+  def test_broadcast_posterior_marginals(self):
+    def test_fb(initial_prob, transition_matrix,
+                observation_probs, observations):
+      model = tfd.HiddenMarkovModel(
+          tfd.Categorical(probs=tf.constant(initial_prob,
+                                            dtype=self.dtype)),
+          tfd.Categorical(probs=tf.constant(transition_matrix,
+                                            dtype=self.dtype)),
+          tfd.Categorical(probs=tf.constant(observation_probs,
+                                            dtype=self.dtype)),
+          num_steps=5)
+
+      return model.posterior_marginals(observations).probs
+
+    initial_prob = [0.5, 0.5]
+    transition_matrix = [[0.7, 0.3],
+                         [0.3, 0.7]]
+    observation_probs = [[0.9, 0.1],
+                         [0.2, 0.8]]
+    observations = [0, 0, 1, 0, 0]
+
+    # Test broadcasting by using a scalar batch for every argument apart
+    # from one which is a batch with two elements.
+    # Test all four ways we can pick one argument to be a size two batch.
+    # They should all give the same result.
+    args = [initial_prob, transition_matrix, observation_probs, observations]
+    results = [test_fb(*[[arg, arg] if i == j else arg
+                         for j, arg in enumerate(args)])
+               for i in range(len(args))]
+
+    expected_probs = [[0.8673, 0.8204, 0.3075, 0.8204, 0.8673],
+                      [0.1327, 0.1796, 0.6925, 0.1796, 0.1327]]
+
+    probs = tf.identity(results)
+    expected_probs = tf.broadcast_to(tf.transpose(expected_probs), [4, 2, 5, 2])
+
+    self.assertAllClose(probs, expected_probs, rtol=1e-3, atol=0.0)
 
 
 class HiddenMarkovModelTestFloat32(tf.test.TestCase, _HiddenMarkovModelTest):
