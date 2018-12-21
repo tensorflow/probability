22f8dbde2963daaa754170aaf791ecc9594f287b
Kristian Hartikainen <hartikainen@berkeley.edu>
2018-12-20 19:39:12 -0800

    Add conditional chain test

--- tensorflow_probability/python/bijectors/conditional_bijector_test.py
+++ tensorflow_probability/python/bijectors/conditional_bijector_test.py
@@ -48,6 +48,32 @@ class _TestBijector(tfb.ConditionalBijector):
     raise ValueError("forward_log_det_jacobian", arg1, arg2)
 
 
+class _TestPassthroughBijector(_TestBijector):
+  def __init__(self, *args, **kwargs):
+      super(_TestPassthroughBijector, self).__init__(*args, **kwargs)
+      self._called = {
+          name: False for name in
+          ('forward', 'inverse',
+           'inverse_log_det_jacobian', 'forward_log_det_jacobian')
+      }
+
+  def _forward(self, _, arg1, arg2):
+    self._called['forward'] = True
+    return _
+
+  def _inverse(self, _, arg1, arg2):
+    self._called['inverse'] = True
+    return _
+
+  def _inverse_log_det_jacobian(self, _, arg1, arg2):
+    self._called['inverse_log_det_jacobian'] = True
+    return _
+
+  def _forward_log_det_jacobian(self, _, arg1, arg2):
+    self._called['forward_log_det_jacobian'] = True
+    return _
+
+
 @tfe.run_all_tests_in_graph_and_eager_modes
 class ConditionalBijectorTest(tf.test.TestCase):
 
@@ -62,6 +88,60 @@ class ConditionalBijectorTest(tf.test.TestCase):
       method = getattr(b, name)
       with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
         method(1., event_ndims=0, arg1="b1", arg2="b2")
+
+  def testChainedConditionalBijector(self):
+    class ConditionalChain(tfb.ConditionalBijector, tfb.Chain):
+      pass
+
+    test_bijector = _TestBijector()
+    passthrough_bijector = _TestPassthroughBijector()
+    chain = ConditionalChain((test_bijector, passthrough_bijector))
+
+    name = "forward"
+    assert not passthrough_bijector._called[name]
+    method = getattr(chain, name)
+    with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
+      method(
+          1.,
+          test_bijector={"arg1": "b1", "arg2": "b2"},
+          test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
+    assert passthrough_bijector._called[name], name
+
+    name = "forward_log_det_jacobian"
+    assert not passthrough_bijector._called[name]
+    method = getattr(chain, name)
+    with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
+      method(
+          1.,
+          event_ndims=0,
+          test_bijector={"arg1": "b1", "arg2": "b2"},
+          test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
+    assert passthrough_bijector._called[name], name
+
+    test_bijector = _TestBijector()
+    passthrough_bijector = _TestPassthroughBijector()
+    chain = ConditionalChain((passthrough_bijector, test_bijector))
+
+    name = "inverse"
+    assert not passthrough_bijector._called[name]
+    method = getattr(chain, name)
+    with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
+      method(
+          1.,
+          test_bijector={"arg1": "b1", "arg2": "b2"},
+          test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
+    assert passthrough_bijector._called[name], name
+
+    name = "inverse_log_det_jacobian"
+    assert not passthrough_bijector._called[name]
+    method = getattr(chain, name)
+    with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
+      method(
+          1.,
+          event_ndims=0,
+          test_bijector={"arg1": "b1", "arg2": "b2"},
+          test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
+    assert passthrough_bijector._called[name], name
 
 
 if __name__ == "__main__":
