c4e84809208d4421502672e2c838b75fa97faee9
Kristian Hartikainen <hartikainen@berkeley.edu>
2018-12-21 16:21:15 -0800

    Revert "Add conditional chain test"
    This reverts commit 22f8dbde2963daaa754170aaf791ecc9594f287b.

--- tensorflow_probability/python/bijectors/conditional_bijector_test.py
+++ tensorflow_probability/python/bijectors/conditional_bijector_test.py
@@ -50,28 +50,6 @@ class _TestBijector(tfb.ConditionalBijector):
     raise ValueError("forward_log_det_jacobian", arg1, arg2)
 
 
-class _TestPassthroughBijector(_TestBijector):
-  def __init__(self, *args, **kwargs):
-    super(_TestPassthroughBijector, self).__init__(*args, **kwargs)
-    self._called = collections.defaultdict(bool)
-
-  def _forward(self, _, arg1, arg2):
-    self._called["forward"] = True
-    return _
-
-  def _inverse(self, _, arg1, arg2):
-    self._called["inverse"] = True
-    return _
-
-  def _inverse_log_det_jacobian(self, _, arg1, arg2):
-    self._called["inverse_log_det_jacobian"] = True
-    return _
-
-  def _forward_log_det_jacobian(self, _, arg1, arg2):
-    self._called["forward_log_det_jacobian"] = True
-    return _
-
-
 @tfe.run_all_tests_in_graph_and_eager_modes
 class ConditionalBijectorTest(tf.test.TestCase):
 
@@ -100,38 +78,6 @@ class ConditionalBijectorTest(tf.test.TestCase):
       with self.assertRaisesRegexp(
           ValueError, name + ".*{'b1': 'c1'}, {'b2': 'c2'}"):
         method(1., event_ndims=0, arg1={"b1": "c1"}, arg2={"b2": "c2"})
-
-  def testChainedConditionalBijector(self):
-    class ConditionalChain(tfb.ConditionalBijector, tfb.Chain):
-      pass
-
-    for name in ["forward", "inverse"]:
-      test_bijector = _TestBijector()
-      passthrough_bijector = _TestPassthroughBijector()
-      chain_components = (test_bijector, passthrough_bijector)
-      if name == "inverse":
-          chain_components = chain_components[::-1]
-      chain = ConditionalChain(chain_components)
-
-      self.assertFalse(passthrough_bijector._called[name])
-      method = getattr(chain, name)
-      with self.assertRaisesRegexp(ValueError, name + ".*b1.*b2"):
-        method(
-            1.,
-            test_bijector={"arg1": "b1", "arg2": "b2"},
-            test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
-      self.assertTrue(passthrough_bijector._called[name])
-
-      ldj_name = name + "_log_det_jacobian"
-      self.assertFalse(passthrough_bijector._called[ldj_name])
-      method = getattr(chain, ldj_name)
-      with self.assertRaisesRegexp(ValueError, ldj_name + ".*b1.*b2"):
-        method(
-            1.,
-            event_ndims=0,
-            test_bijector={"arg1": "b1", "arg2": "b2"},
-            test_passthrough_bijector={"arg1": "b1", "arg2": "b2"})
-      self.assertTrue(passthrough_bijector._called[ldj_name])
 
 
 if __name__ == "__main__":
