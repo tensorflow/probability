ddbb4079960425b4f9492734cd3998b11dacf5cf
Googler <no-reply@google.com>
2019-01-04 07:37:40 -0800

    Automated rollback of a previous commit. END_PUBLIC
    *** Reason for rollback ***
    
    Manually rolled back on behalf of: bjp.
    Reason Given: The broken test is due to a CNS dependency in a test, not caused by the CL being rolled back
    
    *** Original change description ***
    
    BEGIN_PUBLIC
    Automated rollback of a previous commit.
    
    PiperOrigin-RevId: 227854383

--- tensorflow_probability/python/mcmc/util.py
+++ tensorflow_probability/python/mcmc/util.py
@@ -22,6 +22,8 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
+from tensorflow.python.ops import control_flow_util
+
 tfe = tf.contrib.eager
 
 __all__ = [
@@ -202,6 +204,7 @@ def _value_and_gradients(fn, fn_arg_list, result=None, grads=None, name=None):
           """Needed to prevent `cell-var-from-loop` pylint warning."""
           return lambda *args: fn(*args)[i]
         grads = [
+            # TODO(b/122349743): Should this use value_and_gradients_fn?
             tfe.gradients_function(make_fn_slice(i))(*fn_arg_list)[i]
             for i in range(len(result))
         ]
@@ -275,7 +278,8 @@ def smart_for_loop(loop_num_iter, body_fn, initial_loop_vars,
       name, 'smart_for_loop', [loop_num_iter, initial_loop_vars]):
     loop_num_iter_ = tf.contrib.util.constant_value(tf.convert_to_tensor(
         loop_num_iter, dtype=tf.int64, name='loop_num_iter'))
-    if loop_num_iter_ is None or tf.contrib.eager.executing_eagerly():
+    if (loop_num_iter_ is None or tf.contrib.eager.executing_eagerly() or
+        control_flow_util.GraphOrParentsInXlaContext(tf.get_default_graph())):
       return tf.while_loop(
           cond=lambda i, *args: i < loop_num_iter,
           body=lambda i, *args: [i + 1] + list(body_fn(*args)),
