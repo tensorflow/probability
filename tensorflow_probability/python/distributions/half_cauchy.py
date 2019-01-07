Fix broadcasting bug in the half-Cauchy distribution's log_prob and log_cdf functions.
tf.where(condition, x, y) expects x and y to have the same shape if they are both non-None. The caller must ensure this is the case by broadcasting if required. This CL adds a missing broadcast in the _extend_support_with_default_value function and unit tests to cover the broadcasting case.

PiperOrigin-RevId: 228205852

tensorflow_probability/python/distributions/half_cauchy.py
@@ -242,7 +242,7 @@ class HalfCauchy(distribution.Distribution):
       x = x + tf.zeros_like(loc)
       # Substitute out-of-support values in x with values that are in the
       # support of the distribution before applying f.
-      y = f(tf.where(x < loc, self._inv_z(0.5), x))
+      y = f(tf.where(x < loc, self._inv_z(0.5) + tf.zeros_like(x), x))
       if default_value == 0.:
         default_value = tf.zeros_like(y)
       elif default_value == 1.: