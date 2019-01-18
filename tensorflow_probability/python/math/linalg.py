BUGFIX: Correctly broadcast tfp.math.lusolve permutation.
PiperOrigin-RevId: 229989007

tensorflow_probability/python/math/linalg.py
@@ -181,10 +181,12 @@ def lu_solve(lower_upper, perm, rhs,
 
   Args:
     lower_upper: `lu` as returned by `tf.linalg.lu`, i.e., if
-      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U`.
+      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U - eye`.
     perm: `p` as returned by `tf.linag.lu`, i.e., if
       `matmul(P, matmul(L, U)) = X` then `perm = argmax(P)`.
-    rhs: `Tensor` representing targets for which to solve; `A X = RHS`.
+    rhs: Matrix-shaped float `Tensor` representing targets for which to solve;
+      `A X = RHS`. To handle vector cases, use:
+      `lu_solve(..., rhs[..., tf.newaxis])[..., 0]`.
     validate_args: Python `bool` indicating whether arguments should be checked
       for correctness. Note: this function does not verify the implied matrix is
       actually invertible, even when `validate_args=True`.
@@ -193,7 +195,7 @@ def lu_solve(lower_upper, perm, rhs,
       Default value: `None` (i.e., "lu_solve").
 
   Returns:
-    x: The x in `A X = RHS`.
+    x: The `X` in `A @ X = RHS`.
 
   #### Examples
 
@@ -202,10 +204,11 @@ def lu_solve(lower_upper, perm, rhs,
   import tensorflow as tf
   import tensorflow_probability as tfp
 
-  x = [[[3., 4], [1, 2]],
-       [[7., 8], [3, 4]]]
-  inv_x = tfp.math.lu_solve(
-    *tf.linalg.lu(x), rhs=tf.eye(2, batch_shape=[2]))
+  x = [[[1., 2],
+        [3, 4]],
+       [[7, 8],
+        [3, 4]]]
+  inv_x = tfp.math.lu_solve(*tf.linalg.lu(x), rhs=tf.eye(2))
   tf.assert_near(tf.matrix_inverse(x), inv_x)
   # ==> True
   ```
@@ -220,40 +223,50 @@ def lu_solve(lower_upper, perm, rhs,
     rhs = tf.convert_to_tensor(
         rhs, preferred_dtype=lower_upper.dtype, name='rhs')
 
-    assertions = _lu_reconstruct_assertions(lower_upper, perm, validate_args)
+    assertions = _lu_solve_assertions(lower_upper, perm, rhs, validate_args)
     if assertions:
       with tf.control_dependencies(assertions):
         lower_upper = tf.identity(lower_upper)
         perm = tf.identity(perm)
         rhs = tf.identity(rhs)
 
-    shape = tf.shape(lower_upper)
+    if rhs.shape.ndims == 2 and perm.shape.ndims == 1:
+      # Both rhs and perm have scalar batch_shape.
+      permuted_rhs = tf.gather(rhs, perm, axis=-2)
+    else:
+      # Either rhs or perm have non-scalar batch_shape or we can't determine
+      # this information statically.
+      rhs_shape = tf.shape(rhs)
+      broadcast_batch_shape = tf.broadcast_dynamic_shape(
+          rhs_shape[:-2], tf.shape(perm)[:-1])
+      d, m = rhs_shape[-2], rhs_shape[-1]
+      rhs_broadcast_shape = tf.concat([broadcast_batch_shape, [d, m]], axis=0)
 
-    d = shape[-1]
+      # Tile out rhs.
+      broadcast_rhs = tf.broadcast_to(rhs, rhs_broadcast_shape)
+      broadcast_rhs = tf.reshape(broadcast_rhs, [-1, d, m])
+
+      # Tile out perm and add batch indices.
+      broadcast_perm = tf.broadcast_to(perm, rhs_broadcast_shape[:-1])
+      broadcast_perm = tf.reshape(broadcast_perm, [-1, d])
+      broadcast_batch_size = tf.reduce_prod(broadcast_batch_shape)
+      broadcast_batch_indices = tf.broadcast_to(
+          tf.range(broadcast_batch_size)[:, tf.newaxis],
+          [broadcast_batch_size, d])
+      broadcast_perm = tf.stack([broadcast_batch_indices, broadcast_perm],
+                                axis=-1)
+
+      permuted_rhs = tf.gather_nd(broadcast_rhs, broadcast_perm)
+      permuted_rhs = tf.reshape(permuted_rhs, rhs_broadcast_shape)
+
     lower = tf.linalg.set_diag(
         tf.matrix_band_part(lower_upper, num_lower=-1, num_upper=0),
-        tf.ones(shape[:-1], dtype=lower_upper.dtype))
-    x = linear_operator_util.matrix_triangular_solve_with_broadcast(
+        tf.ones(tf.shape(lower_upper)[:-1], dtype=lower_upper.dtype))
+    return linear_operator_util.matrix_triangular_solve_with_broadcast(
         lower_upper,  # Only upper is accessed.
         linear_operator_util.matrix_triangular_solve_with_broadcast(
-            lower, rhs),
+            lower, permuted_rhs),
         lower=False)
-
-    if lower_upper.shape.ndims is None or lower_upper.shape.ndims != 2:
-      # We either don't know the batch rank or there are >0 batch dims.
-      batch_size = tf.reduce_prod(shape[:-2])
-      x = tf.reshape(x, [batch_size, d, d])
-      perm = tf.reshape(perm, [batch_size, d])
-      batch_indices = tf.broadcast_to(
-          tf.range(batch_size)[:, tf.newaxis],
-          [batch_size, d])
-      x = tf.gather_nd(x, tf.stack([batch_indices, perm], axis=-1))
-      x = tf.reshape(x, shape)
-    else:
-      x = tf.gather(x, perm, axis=-1)
-
-    x.set_shape(lower_upper.shape)
-    return x
 
 
 def lu_matrix_inverse(lower_upper, perm, validate_args=False, name=None):
@@ -272,7 +285,7 @@ def lu_matrix_inverse(lower_upper, perm, validate_args=False, name=None):
 
   Args:
     lower_upper: `lu` as returned by `tf.linalg.lu`, i.e., if
-      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U`.
+      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U - eye`.
     perm: `p` as returned by `tf.linag.lu`, i.e., if
       `matmul(P, matmul(L, U)) = X` then `perm = argmax(P)`.
     validate_args: Python `bool` indicating whether arguments should be checked
@@ -324,7 +337,7 @@ def lu_reconstruct(lower_upper, perm, validate_args=False, name=None):
 
   Args:
     lower_upper: `lu` as returned by `tf.linalg.lu`, i.e., if
-      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U`.
+      `matmul(P, matmul(L, U)) = X` then `lower_upper = L + U - eye`.
     perm: `p` as returned by `tf.linag.lu`, i.e., if
       `matmul(P, matmul(L, U)) = X` then `perm = argmax(P)`.
     validate_args: Python `bool` indicating whether arguments should be checked
@@ -418,5 +431,31 @@ def _lu_reconstruct_assertions(lower_upper, perm, validate_args):
   elif validate_args:
     m, n = tf.split(tf.shape(lower_upper)[-2:], num_or_size_splits=2)
     assertions.append(tf.assert_equal(m, n, message=message))
+
+  return assertions
+
+
+def _lu_solve_assertions(lower_upper, perm, rhs, validate_args):
+  """Returns list of assertions related to `lu_solve` assumptions."""
+  assertions = _lu_reconstruct_assertions(lower_upper, perm, validate_args)
+
+  message = 'Input `rhs` must have at least 2 dimensions.'
+  if rhs.shape.ndims is not None:
+    if rhs.shape.ndims < 2:
+      raise ValueError(message)
+  elif validate_args:
+    assertions.append(
+        tf.assert_rank_at_least(rhs, rank=2, message=message))
+
+  message = '`lower_upper.shape[-1]` must equal `rhs.shape[-1]`.'
+  if (tf.dimension_value(lower_upper.shape[-1]) is not None and
+      tf.dimension_value(rhs.shape[-2]) is not None):
+    if lower_upper.shape[-1] != rhs.shape[-2]:
+      raise ValueError(message)
+  elif validate_args:
+    assertions.append(
+        tf.assert_equal(tf.shape(lower_upper)[-1],
+                        tf.shape(rhs)[-2],
+                        message=message))
 
   return assertions

tensorflow_probability/python/math/linalg_test.py
@@ -123,7 +123,7 @@ def make_tensor_hiding_attributes(value, hide_shape, hide_value=True):
   return tf.placeholder_with_default(input=value, shape=shape)
 
 
-class _LUInverse(object):
+class _LUReconstruct(object):
   dtype = np.float32
   use_static_shape = True
 
@@ -160,23 +160,21 @@ class _LUInverse(object):
 
 
 @tfe.run_all_tests_in_graph_and_eager_modes
-class LUInverseStatic(tf.test.TestCase, _LUInverse):
+class LUReconstructStatic(tf.test.TestCase, _LUReconstruct):
   use_static_shape = True
 
 
 @tfe.run_all_tests_in_graph_and_eager_modes
-class LUInverseDynamic(tf.test.TestCase, _LUInverse):
+class LUReconstructDynamic(tf.test.TestCase, _LUReconstruct):
   use_static_shape = False
 
 
-class _MatrixInverseLU(object):
+class _LUMatrixInverse(object):
   dtype = np.float32
   use_static_shape = True
 
   def test_non_batch(self):
-    x_ = np.array(
-        [[3, 4], [1, 2]],
-        dtype=self.dtype)
+    x_ = np.array([[1, 2], [3, 4]], dtype=self.dtype)
     x = tf.placeholder_with_default(
         x_, shape=x_.shape if self.use_static_shape else None)
 
@@ -190,8 +188,12 @@ class _MatrixInverseLU(object):
   def test_batch(self):
     x_ = np.array(
         [
-            [[3, 4], [1, 2]],
-            [[7, 8], [3, 4]],
+            [[1, 2],
+             [3, 4]],
+            [[7, 8],
+             [3, 4]],
+            [[0.25, 0.5],
+             [0.75, -2.]],
         ],
         dtype=self.dtype)
     x = tf.placeholder_with_default(
@@ -206,12 +208,77 @@ class _MatrixInverseLU(object):
 
 
 @tfe.run_all_tests_in_graph_and_eager_modes
-class MatrixInverseLUStatic(tf.test.TestCase, _MatrixInverseLU):
+class LUMatrixInverseStatic(tf.test.TestCase, _LUMatrixInverse):
   use_static_shape = True
 
 
 @tfe.run_all_tests_in_graph_and_eager_modes
-class MatrixInverseLUDynamic(tf.test.TestCase, _MatrixInverseLU):
+class LUMatrixInverseDynamic(tf.test.TestCase, _LUMatrixInverse):
+  use_static_shape = False
+
+
+class _LUSolve(object):
+  dtype = np.float32
+  use_static_shape = True
+
+  def test_non_batch(self):
+    x_ = np.array(
+        [[1, 2],
+         [3, 4]],
+        dtype=self.dtype)
+    x = tf.placeholder_with_default(
+        x_, shape=x_.shape if self.use_static_shape else None)
+    rhs_ = np.array([[1, 1]], dtype=self.dtype).T
+    rhs = tf.placeholder_with_default(
+        rhs_, shape=rhs_.shape if self.use_static_shape else None)
+
+    lower_upper, perm = tf.linalg.lu(x)
+    y = tfp.math.lu_solve(lower_upper, perm, rhs, validate_args=True)
+    y_, perm_ = self.evaluate([y, perm])
+
+    self.assertAllEqual([1, 0], perm_)
+    expected_ = np.linalg.solve(x_, rhs_)
+    if self.use_static_shape:
+      self.assertAllEqual(expected_.shape, y.shape)
+    self.assertAllClose(expected_, y_, atol=0., rtol=1e-3)
+
+  def test_batch_broadcast(self):
+    x_ = np.array(
+        [
+            [[1, 2],
+             [3, 4]],
+            [[7, 8],
+             [3, 4]],
+            [[0.25, 0.5],
+             [0.75, -2.]],
+        ],
+        dtype=self.dtype)
+    x = tf.placeholder_with_default(
+        x_, shape=x_.shape if self.use_static_shape else None)
+    rhs_ = np.array([[1, 1]], dtype=self.dtype).T
+    rhs = tf.placeholder_with_default(
+        rhs_, shape=rhs_.shape if self.use_static_shape else None)
+
+    lower_upper, perm = tf.linalg.lu(x)
+    y = tfp.math.lu_solve(lower_upper, perm, rhs, validate_args=True)
+    y_, perm_ = self.evaluate([y, perm])
+
+    self.assertAllEqual([[1, 0],
+                         [0, 1],
+                         [1, 0]], perm_)
+    expected_ = np.linalg.solve(x_, rhs_[np.newaxis])
+    if self.use_static_shape:
+      self.assertAllEqual(expected_.shape, y.shape)
+    self.assertAllClose(expected_, y_, atol=0., rtol=1e-3)
+
+
+@tfe.run_all_tests_in_graph_and_eager_modes
+class LUSolveStatic(tf.test.TestCase, _LUSolve):
+  use_static_shape = True
+
+
+@tfe.run_all_tests_in_graph_and_eager_modes
+class LUSolveDynamic(tf.test.TestCase, _LUSolve):
   use_static_shape = False
 
 