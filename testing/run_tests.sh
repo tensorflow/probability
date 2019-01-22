c29cee92e8ed11f6824cdf9162cf506614e568f2
Christopher Suter <cgs@google.com>
2019-01-22 15:09:48 -0500

    Overhaul Travis configuration.
      1. Create separate "build stages" for lint and test. This lets lint
         failures short circuit the build. It also prevents them being run
         twice, once for each python version.
      2. Move away from using 'install' entries in .travis.yml. This
         prevents, e.g., installing bazel before running lints. Instead, now
         test scripts do their own installation of things. Travis's install
         mechanism is kinda fancy (supports language-aware concepts like
         looking around for requirements.txt, for python) but we weren't
         really using it the "right way" and anyway our build configuration
         is pretty unusual for an OSS python project. This change also makes
         the installation steps "quiet" to reduce log cruft.
    
    Here's an example of the tests running on my clone of the TFP repo: Note
    that there are 2 stages, "Lint" and "Test".
    
    Example of lint failing and stopping build:
      https://travis-ci.org/csuter/probability/builds/483136955
    
    Example of everything running to completion:
      https://travis-ci.org/csuter/probability/builds/483138401

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -28,6 +28,39 @@ if [ -z "${NUM_SHARDS}" ]; then
   exit -1
 fi
 
+install_bazel() {
+  # Install Bazel for tests. Based on instructions at
+  # https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
+  # (We skip the openjdk8 install step, since travis lets us have that by
+  # default).
+
+  # Add Bazel distribution URI as a package source
+  echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" \
+    | sudo tee /etc/apt/sources.list.d/bazel.list
+  curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
+  # Update apt and install bazel (use -qq to minimize log cruft)
+  sudo apt-get update -qq
+  sudo apt-get install bazel -qq
+}
+
+install_python_packages() {
+  # Some tests use methods from these libraries so we install them although
+  # they're not official dependencies of the TFP library. pylint is used for
+  # linting.
+  # NB: tensorflow pulls in our other deps, like numpy and six, transitively.
+  pip install --quiet tf-nightly scipy hypothesis matplotlib
+
+  # Upgrade numpy to the latest to address issues that happen when testing with
+  # Python 3 (https://github.com/tensorflow/tensorflow/issues/16488).
+  pip install -U numpy
+}
+
+# Do these in parallel
+install_bazel &
+install_python_packages &
+wait
+
 # Get a shard of tests.
 shard_tests=$(bazel query 'tests(//tensorflow_probability/...)' |
   awk -v n=${NUM_SHARDS} -v s=${SHARD} 'NR%n == s' )
@@ -43,8 +76,14 @@ shard_tests=$(bazel query 'tests(//tensorflow_probability/...)' |
 #   --action_env -- specify environment vars to pass through to action
 #     environment. (We need these in order to run inside a virtualenv.)
 #     See https://github.com/bazelbuild/bazel/issues/6648 and b/121259040.
-echo "${shard_tests}" | xargs bazel test --copt=-O3 --copt=-march=native \
-  --test_tag_filters=-gpu,-requires-gpu-sm35 \
-  --test_timeout 300,450,1200,3600 --build_tests_only \
-  --action_env=PATH --action_env=LD_LIBRARY_PATH \
-  --test_output=errors
+echo "${shard_tests}" \
+  | xargs bazel test \
+    --copt=-O3 \
+    --copt=-march=native \
+    --notest_keep_going \
+    --test_tag_filters=-gpu,-requires-gpu-sm35 \
+    --test_timeout 300,450,1200,3600 \
+    --build_tests_only \
+    --action_env=PATH \
+    --action_env=LD_LIBRARY_PATH \
+    --test_output=errors
