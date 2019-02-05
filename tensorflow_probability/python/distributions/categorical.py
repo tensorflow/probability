Fix parameter broadcasting in DirichletMultinomial.  The intended semantics as I understand them are:
- total_count is of batch shape
- concentration is of shape batch shape + event shape
- event shape assumed to be rank 1
- ergo, total_count[..., tf.newaxis] must broadcast with concentration
- the actual batch shape is given by the result of this broadcast
  (stripping off the last dimension)

The implementation departed from these semantics in three places:

- sample_n was untested and broken with non-trivial batch shape.  The
  underlying problem is that tf.multinomial is actually
  tf.categorical, and doesn't understand how to draw the categorical
  samples that would be appropriate for computing a batch of
  multinomials with different numbers of draws (as that would entail
  emitting a ragged result tensor).

- the batch shape computation did not take broadcasting into account.

- the mean and variance calculations, and their tests, were coded to
  expect the user to supply the extra dimension to total_count
  themselves (i.e., were API-inconsistent with sampling).

Fixed this by

- Extracting the sampling method from the (non-Dirichlet) Multinomial
  distribution into a reusable function and reusing it.  This method
  had a correct workaround for nontrivial multinomial batch shapes.

- Performing the parameter broadcasting in the DirichletMultinomial
  constructor, and referencing the result in the batch shape
  computation.

- Bashing extra dimensions in the covariance computation and tests
  until they passed.

Fixes #275.

PiperOrigin-RevId: 232505857

tensorflow_probability/python/distributions/categorical.py
@@ -259,8 +259,8 @@ class Categorical(distribution.Distribution):
     else:
       logits_2d = tf.reshape(self.logits, [-1, self.event_size])
     sample_dtype = tf.int64 if self.dtype.size > 4 else tf.int32
-    draws = tf.multinomial(
-        logits_2d, n, seed=seed, output_dtype=sample_dtype)
+    draws = tf.random.categorical(
+        logits_2d, n, dtype=sample_dtype, seed=seed)
     draws = tf.reshape(
         tf.transpose(draws),
         tf.concat([[n], self.batch_shape_tensor()], 0))

tensorflow_probability/python/distributions/dirichlet_multinomial.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow_probability.python.distributions import distribution
+from tensorflow_probability.python.distributions import multinomial
 from tensorflow_probability.python.distributions import seed_stream
 from tensorflow_probability.python.internal import distribution_util
 from tensorflow_probability.python.internal import dtype_util
@@ -185,17 +186,19 @@ class DirichletMultinomial(distribution.Distribution):
         more of the statistic's batch members are undefined.
       name: Python `str` name prefixed to Ops created by this class.
     """
+    # Broadcasting works because:
+    # * The broadcasting convention is to prepend dimensions of size [1], and
+    #   we use the last dimension for the distribution, whereas
+    #   the batch dimensions are the leading dimensions, which forces the
+    #   distribution dimension to be defined explicitly (i.e. it cannot be
+    #   created automatically by prepending). This forces enough explicitness.
+    # * All calls involving `counts` eventually require a broadcast between
+    #  `counts` and concentration.
+    # * We broadcast explicitly to include the effect of `counts` on
+    #   `concentration` for calls that do not involve `counts`.
     parameters = dict(locals())
     with tf.name_scope(name, values=[total_count, concentration]) as name:
       dtype = dtype_util.common_dtype([total_count, concentration], tf.float32)
-      # Broadcasting works because:
-      # * The broadcasting convention is to prepend dimensions of size [1], and
-      #   we use the last dimension for the distribution, whereas
-      #   the batch dimensions are the leading dimensions, which forces the
-      #   distribution dimension to be defined explicitly (i.e. it cannot be
-      #   created automatically by prepending). This forces enough explicitness.
-      # * All calls involving `counts` eventually require a broadcast between
-      #  `counts` and concentration.
       self._total_count = tf.convert_to_tensor(
           total_count, name="total_count", dtype=dtype)
       if validate_args:
@@ -206,6 +209,8 @@ class DirichletMultinomial(distribution.Distribution):
           tf.convert_to_tensor(
               concentration, name="concentration", dtype=dtype), validate_args)
       self._total_concentration = tf.reduce_sum(self._concentration, -1)
+      self._broadcasted_concentration = tf.ones_like(
+          self._total_count[..., tf.newaxis]) * self._concentration
     super(DirichletMultinomial, self).__init__(
         dtype=dtype,
         validate_args=validate_args,
@@ -231,37 +236,32 @@ class DirichletMultinomial(distribution.Distribution):
     return self._total_concentration
 
   def _batch_shape_tensor(self):
-    return tf.shape(self.total_concentration)
+    return tf.shape(self._broadcasted_concentration)[:-1]
 
   def _batch_shape(self):
-    return self.total_concentration.shape
+    return self._broadcasted_concentration.shape.with_rank_at_least(1)[:-1]
 
   def _event_shape_tensor(self):
+    # Event shape depends only on concentration, not total_count.
     return tf.shape(self.concentration)[-1:]
 
   def _event_shape(self):
-    # Event shape depends only on total_concentration, not "n".
+    # Event shape depends only on concentration, not total_count.
     return self.concentration.shape.with_rank_at_least(1)[-1:]
 
   def _sample_n(self, n, seed=None):
     seed = seed_stream.SeedStream(seed, "dirichlet_multinomial")
     n_draws = tf.cast(self.total_count, dtype=tf.int32)
     k = self.event_shape_tensor()[0]
-    unnormalized_logits = tf.reshape(
-        tf.log(tf.random_gamma(
-            shape=[n],
-            alpha=self.concentration,
-            dtype=self.dtype,
-            seed=seed())),
-        shape=[-1, k])
-    draws = tf.multinomial(
-        logits=unnormalized_logits,
-        num_samples=n_draws,
-        seed=seed())
-    x = tf.reduce_sum(tf.one_hot(draws, depth=k), -2)
+    unnormalized_logits = tf.log(tf.random_gamma(
+        shape=[n],
+        alpha=self._broadcasted_concentration,
+        dtype=self.dtype,
+        seed=seed()))
+    x = multinomial.draw_sample(
+        1, k, unnormalized_logits, n_draws, self.dtype, seed())
     final_shape = tf.concat([[n], self.batch_shape_tensor(), [k]], 0)
-    x = tf.reshape(x, final_shape)
-    return tf.cast(x, self.dtype)
+    return tf.reshape(x, final_shape)
 
   @distribution_util.AppendDocstring(_dirichlet_multinomial_sample_note)
   def _log_prob(self, counts):
@@ -277,8 +277,9 @@ class DirichletMultinomial(distribution.Distribution):
     return tf.exp(self._log_prob(counts))
 
   def _mean(self):
-    return self.total_count * (self.concentration /
-                               self.total_concentration[..., tf.newaxis])
+    scaled_concentration = (self.concentration /
+                            self.total_concentration[..., tf.newaxis])
+    return self.total_count[..., tf.newaxis] * scaled_concentration
 
   @distribution_util.AppendDocstring(
       """The covariance for each batch member is defined as the following:
@@ -308,14 +309,14 @@ class DirichletMultinomial(distribution.Distribution):
   def _variance(self):
     scale = self._variance_scale_term()
     x = scale * self._mean()
-    return x * (self.total_count * scale - x)
+    return x * (self.total_count[..., tf.newaxis] * scale - x)
 
   def _variance_scale_term(self):
     """Helper to `_covariance` and `_variance` which computes a shared scale."""
-    # We must take care to expand back the last dim whenever we use the
-    # total_concentration.
+    # Expand back the last dim so the shape of _variance_scale_term matches the
+    # shape of self.concentration.
     c0 = self.total_concentration[..., tf.newaxis]
-    return tf.sqrt((1. + c0 / self.total_count) / (1. + c0))
+    return tf.sqrt((1. + c0 / self.total_count[..., tf.newaxis]) / (1. + c0))
 
   def _maybe_assert_valid_concentration(self, concentration, validate_args):
     """Checks the validity of the concentration parameter."""

tensorflow_probability/python/distributions/dirichlet_multinomial_test.py
@@ -49,6 +49,7 @@ class DirichletMultinomialTest(tf.test.TestCase):
       self.assertAllEqual([3, 2], dist.batch_shape_tensor().eval())
       self.assertEqual(tf.TensorShape([2]), dist.event_shape)
       self.assertEqual(tf.TensorShape([3, 2]), dist.batch_shape)
+      self.assertEqual(tf.TensorShape([3, 2, 2]), dist.sample().shape)
 
   def testNproperty(self):
     alpha = [[1., 2, 3]]
@@ -292,8 +293,8 @@ class DirichletMultinomialTest(tf.test.TestCase):
 
     # Shape [4, 3]
     alpha = np.array(4 * [alpha_v], dtype=np.float32)
-    # Shape [4, 1]
-    ns = np.array([[2.], [3.], [4.], [5.]], dtype=np.float32)
+    # Shape [4]
+    ns = np.array([2., 3., 4., 5.], dtype=np.float32)
 
     variance_entry = lambda a, a_sum: a / a_sum * (1 - a / a_sum)
     covariance_entry = lambda a, b, a_sum: -a * b / a_sum**2
@@ -315,21 +316,21 @@ class DirichletMultinomialTest(tf.test.TestCase):
         dtype=np.float32)
 
     with self.cached_session():
-      # ns is shape [4, 1], and alpha is shape [4, 3].
+      # ns is shape [4], and alpha is shape [4, 3].
       dist = ds.DirichletMultinomial(ns, alpha)
       covariance = dist.covariance()
       expected_covariance = shared_matrix * (
-          ns * (ns + alpha_0) / (1 + alpha_0))[..., tf.newaxis]
+          ns * (ns + alpha_0) / (1 + alpha_0))[..., tf.newaxis, tf.newaxis]
 
       self.assertEqual([4, 3, 3], covariance.shape)
       self.assertAllClose(expected_covariance, covariance.eval())
 
   def testCovarianceMultidimensional(self):
-    alpha = np.random.rand(3, 5, 4).astype(np.float32)
+    alpha = np.random.rand(3, 1, 4).astype(np.float32)
     alpha2 = np.random.rand(6, 3, 3).astype(np.float32)
 
-    ns = np.random.randint(low=1, high=11, size=[3, 5, 1]).astype(np.float32)
-    ns2 = np.random.randint(low=1, high=11, size=[6, 1, 1]).astype(np.float32)
+    ns = np.random.randint(low=1, high=11, size=[3, 5]).astype(np.float32)
+    ns2 = np.random.randint(low=1, high=11, size=[6, 1]).astype(np.float32)
 
     with self.cached_session():
       dist = ds.DirichletMultinomial(ns, alpha)

tensorflow_probability/python/distributions/multinomial.py
@@ -232,34 +232,7 @@ class Multinomial(distribution.Distribution):
     n_draws = tf.cast(self.total_count, dtype=tf.int32)
     k = self.event_shape_tensor()[0]
 
-    # broadcast the total_count and logits to same shape
-    n_draws = tf.ones_like(
-        self.logits[..., 0], dtype=n_draws.dtype) * n_draws
-    logits = tf.ones_like(
-        n_draws[..., tf.newaxis], dtype=self.logits.dtype) * self.logits
-
-    # flatten the total_count and logits
-    flat_logits = tf.reshape(logits, [-1, k])  # [B1B2...Bm, k]
-    flat_ndraws = n * tf.reshape(n_draws, [-1])  # [B1B2...Bm]
-
-    # computes each total_count and logits situation by map_fn
-    def _sample_single(args):
-      logits, n_draw = args[0], args[1]  # [K], []
-      x = tf.multinomial(logits[tf.newaxis, ...], n_draw,
-                         seed)  # [1, n*n_draw]
-      x = tf.reshape(x, shape=[n, -1])  # [n, n_draw]
-      x = tf.reduce_sum(tf.one_hot(x, depth=k), axis=-2)  # [n, k]
-      return x
-
-    x = tf.map_fn(
-        _sample_single, [flat_logits, flat_ndraws],
-        dtype=self.dtype)  # [B1B2...Bm, n, k]
-
-    # reshape the results to proper shape
-    x = tf.transpose(x, perm=[1, 0, 2])
-    final_shape = tf.concat([[n], self.batch_shape_tensor(), [k]], 0)
-    x = tf.reshape(x, final_shape)  # [n, B1, B2,..., Bm, k]
-    return x
+    return draw_sample(n, k, self.logits, n_draws, self.dtype, seed)
 
   @distribution_util.AppendDocstring(_multinomial_sample_note)
   def _log_prob(self, counts):
@@ -299,3 +272,77 @@ class Multinomial(distribution.Distribution):
             self.total_count, tf.reduce_sum(counts, -1),
             message="counts must sum to `self.total_count`"),
     ], counts)
+
+
+def draw_sample(num_samples, num_classes, logits, num_trials, dtype, seed):
+  """Sample a multinomial.
+
+  The batch shape is given by broadcasting num_trials with
+  remove_last_dimension(logits).
+
+  Args:
+    num_samples: Python int or singleton integer Tensor: number of multinomial
+      samples to draw.
+    num_classes: Python int or singleton integer Tensor: number of classes.
+    logits: Floating Tensor with last dimension k, of (unnormalized) logit
+      probabilities per class.
+    num_trials: Tensor of number of categorical trials each multinomial consists
+      of.  num_trials[..., tf.newaxis] must broadcast with logits.
+    dtype: dtype at which to emit samples.
+    seed: Random seed.
+
+  Returns:
+    samples: Tensor of given dtype and shape [n] + batch_shape + [k].
+  """
+  with tf.name_scope(
+      None, "multinomial.draw_sample",
+      [num_samples, num_classes, logits, num_trials]):
+    # broadcast the num_trials and logits to same shape
+    num_trials = tf.ones_like(
+        logits[..., 0], dtype=num_trials.dtype) * num_trials
+    logits = tf.ones_like(
+        num_trials[..., tf.newaxis], dtype=logits.dtype) * logits
+
+    # flatten the total_count and logits
+    # flat_logits has shape [B1B2...Bm, num_classes]
+    flat_logits = tf.reshape(logits, [-1, num_classes])
+    flat_num_trials = num_samples * tf.reshape(num_trials, [-1])  # [B1B2...Bm]
+
+    # Computes each logits and num_trials situation by map_fn.
+
+    # Using just one batch tf.random.categorical call doesn't work because that
+    # requires num_trials to be the same across all members of the batch of
+    # logits.  This restriction makes sense for tf.random.categorical because
+    # for it, num_trials is part of the returned shape.  However, the
+    # multinomial sampler does not need that restriction, because it sums out
+    # exactly that dimension.
+
+    # One possibility would be to draw a batch categorical whose sample count is
+    # max(num_trials) and mask out the excess ones.  However, if the elements of
+    # num_trials vary widely, this can be wasteful of memory.
+
+    # TODO(b/123763054, b/112152209): Revisit the possibility of writing this
+    # with a batch categorical followed by batch unsorted_segment_sum, once both
+    # of those work and are memory-efficient enough.
+    def _sample_one_batch_member(args):
+      logits, num_cat_samples = args[0], args[1]  # [K], []
+      # x has shape [1, num_cat_samples = num_samples * num_trials]
+      x = tf.random.categorical(
+          logits[tf.newaxis, ...], num_cat_samples, seed=seed)
+      x = tf.reshape(x, shape=[num_samples, -1])  # [num_samples, num_trials]
+      x = tf.one_hot(
+          x, depth=num_classes)  # [num_samples, num_trials, num_classes]
+      x = tf.reduce_sum(x, axis=-2)  # [num_samples, num_classes]
+      return x
+
+    x = tf.map_fn(
+        _sample_one_batch_member, [flat_logits, flat_num_trials],
+        dtype=dtype)  # [B1B2...Bm, num_samples, num_classes]
+
+    # reshape the results to proper shape
+    x = tf.transpose(x, perm=[1, 0, 2])
+    final_shape = tf.concat(
+        [[num_samples], tf.shape(num_trials), [num_classes]], axis=0)
+    x = tf.reshape(x, final_shape)
+
+    return x

tensorflow_probability/python/distributions/onehot_categorical.py
@@ -169,7 +169,7 @@ class OneHotCategorical(distribution.Distribution):
       logits_2d = logits
     else:
       logits_2d = tf.reshape(logits, [-1, self.event_size])
-    samples = tf.multinomial(logits_2d, n, seed=seed)
+    samples = tf.random.categorical(logits_2d, n, seed=seed)
     samples = tf.transpose(samples)
     samples = tf.one_hot(samples, self.event_size, dtype=self.dtype)
     ret = tf.reshape(samples, sample_shape)