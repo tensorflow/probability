71ee424cf051803071725ad5db038cc04a99a01d
Christopher Suter <cgs@google.com>
2019-02-14 11:54:15 -0800

    Eliminate util.variables
    It contained only one public function, to be deprecated with tf2. Rather than
    make it compatible for 2 weeks, deleting it now.
    
    PiperOrigin-RevId: 234000692

--- tensorflow_probability/python/util/__init__.py
+++ tensorflow_probability/python/util/__init__.py
@@ -13,17 +13,3 @@
 # limitations under the License.
 # ============================================================================
 """TensorFlow Probability python utilities."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow_probability.python.util.variables import externalize_variables_as_args
-
-from tensorflow.python.util.all_util import remove_undocumented
-
-_allowed_symbols = [
-    'externalize_variables_as_args',
-]
-
-remove_undocumented(__name__, _allowed_symbols)
