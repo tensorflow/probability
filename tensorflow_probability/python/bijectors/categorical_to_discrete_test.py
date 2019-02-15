466e7ea40e32e53438085c19b89508a40f57dbce
Googler <no-reply@google.com>
2019-02-14 19:28:09 -0800

    Implement CategoricalToDiscrete bijector, which maps integer x (0<=x<K) to values[x], where values is a predefined 1D tensor with size K.
    The bijector is hidden from the public API as it only applies to categorical distribution. Next steps are to expose this only through a TransformedDistribution of a Categorical.
    
    PiperOrigin-RevId: 234072612

---
+++ tensorflow_probability/python/bijectors/categorical_to_discrete_test.py
@@ -0,0 +1,103 @@
+# Copyright 2018 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests CategoricalToDiscrete bijector."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+from tensorflow_probability.python.bijectors import bijector_test_util
+from tensorflow_probability.python.bijectors import categorical_to_discrete
+
+from tensorflow.python.framework import test_util  # pylint: disable=g-direct-tensorflow-import
+
+
+@test_util.run_all_in_graph_and_eager_modes
+class CategoricalToDiscreteTest(tf.test.TestCase):
+
+  def testUnsortedValuesRaises(self):
+    with self.assertRaisesOpError('map_values is not strictly increasing'):
+      bijector = categorical_to_discrete.CategoricalToDiscrete(
+          map_values=[1, 3, 2], validate_args=True)
+      self.evaluate(bijector.map_values)
+
+  def testMapValuesRankNotEqualToOneRaises(self):
+    with self.assertRaisesWithPredicateMatch(ValueError,
+                                             'Rank of map_values must be 1'):
+      bijector = categorical_to_discrete.CategoricalToDiscrete(
+          map_values=[[1, 2], [3, 4]], validate_args=True)
+      self.evaluate(bijector.map_values)
+
+  def testMapValuesSizeZeroRaises(self):
+    with self.assertRaisesWithPredicateMatch(
+        ValueError, 'Size of map_values must be greater than 0'):
+      bijector = categorical_to_discrete.CategoricalToDiscrete(
+          map_values=[], validate_args=True)
+      self.evaluate(bijector.map_values)
+
+  def testBijectorForward(self):
+    bijector = categorical_to_discrete.CategoricalToDiscrete(
+        map_values=[0.1, 0.2, 0.3, 0.4], validate_args=True)
+    self.assertAllClose([[0.1, 0.2, 0.3, 0.4], [0.4, 0.3, 0.2, 0.1]],
+                        self.evaluate(
+                            bijector.forward([[0, 1, 2, 3], [3, 2, 1, 0]])))
+
+  def testBijectorForwardOutOfBoundIndicesRaises(self):
+    with self.assertRaisesOpError('indices out of bound'):
+      bijector = categorical_to_discrete.CategoricalToDiscrete(
+          map_values=[0.1, 0.2, 0.3, 0.4], validate_args=True)
+      self.evaluate(bijector.forward([5]))
+
+  def testBijectorInverse(self):
+    bijector = categorical_to_discrete.CategoricalToDiscrete(
+        map_values=[0.1, 0.2, 0.3, 0.4], validate_args=True)
+    self.assertAllEqual([[3, 3, 3], [0, 1, 2]],
+                        self.evaluate(
+                            bijector.inverse([[0.400001, 0.4, 0.399999],
+                                              [0.1, 0.2, 0.3]])))
+
+  def testBijectorInverseValueNotFoundRaises(self):
+    with self.assertRaisesOpError('inverse value not found'):
+      bijector = categorical_to_discrete.CategoricalToDiscrete(
+          map_values=[0.1, 0.2, 0.3, 0.4], validate_args=True)
+      self.evaluate(bijector.inverse([0.21, 0.4]))
+
+  def testInverseLogDetJacobian(self):
+    bijector = categorical_to_discrete.CategoricalToDiscrete(
+        map_values=[0.1, 0.2], validate_args=True)
+    self.assertAllClose(
+        0,
+        self.evaluate(
+            bijector.inverse_log_det_jacobian([0.1, 0.2], event_ndims=0)))
+
+  def testBijectiveAndFinite32bit(self):
+    x = np.arange(100).astype(np.int32)
+    y = np.logspace(-10, 10, 100).astype(np.float32)
+    bijector = categorical_to_discrete.CategoricalToDiscrete(map_values=y)
+    bijector_test_util.assert_bijective_and_finite(
+        bijector, x, y, eval_func=self.evaluate, event_ndims=0)
+
+  def testBijectiveAndFinite16bit(self):
+    x = np.arange(100).astype(np.int32)
+    y = np.logspace(-5, 4, 100).astype(np.float16)
+    bijector = categorical_to_discrete.CategoricalToDiscrete(map_values=y)
+    bijector_test_util.assert_bijective_and_finite(
+        bijector, x, y, eval_func=self.evaluate, event_ndims=0)
+
+
+if __name__ == '__main__':
+  tf.test.main()
