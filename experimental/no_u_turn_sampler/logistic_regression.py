8b2c9b163808781aef1a9ff50ba3572ade1c9b45
Alexey Radul <axch@google.com>
2019-03-14 15:37:13 -0400

    Test PR.

--- experimental/no_u_turn_sampler/logistic_regression.py
+++ experimental/no_u_turn_sampler/logistic_regression.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Trains a Bayesian logistic regression model using No-U-Turn Sampler."""
+"""Trains a Bayesian logistic regression model using the No-U-Turn Sampler."""
 
 from __future__ import absolute_import
 from __future__ import division
