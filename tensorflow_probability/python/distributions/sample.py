BUGFIX: Correctly compute summary statistics in `tfd.Sample`: - incorrect getattr call for most summary stats - incorrect broadcasting in most summary stats - incorrect named arg for entropy sample_shape size casting.
PiperOrigin-RevId: 241853425

tensorflow_probability/python/distributions/sample.py
@@ -33,15 +33,18 @@ def _make_summary_statistic(attr):
   """Factory for implementing summary statistics, eg, mean, stddev, mode."""
   def _fn(self):
     """Implements summary statistic, eg, mean, stddev, mode."""
-    x = getattr(self, attr)
-    shape = tf.pad(
-        tensor=self.distribution.batch_shape_tensor(),
-        paddings=[[0, prefer_static.rank_from_shape(self.sample_shape)]],
-        constant_values=1)
+    x = getattr(self.distribution, attr)()
+    shape = prefer_static.concat([
+        self.distribution.batch_shape_tensor(),
+        prefer_static.ones(prefer_static.rank_from_shape(self.sample_shape),
+                           dtype=self.sample_shape.dtype),
+        self.distribution.event_shape_tensor(),
+    ], axis=0)
     x = tf.reshape(x, shape=shape)
     shape = prefer_static.concat([
         self.distribution.batch_shape_tensor(),
         self.sample_shape,
+        self.distribution.event_shape_tensor(),
     ], axis=0)
     return tf.broadcast_to(x, shape)
   return _fn
@@ -221,7 +224,7 @@ class Sample(distribution_lib.Distribution):
   def _entropy(self):
     h = self.distribution.entropy()
     n = prefer_static.reduce_prod(self.sample_shape)
-    return tf.cast(input_tensor=n, dtype=h.dtype) * h
+    return tf.cast(x=n, dtype=h.dtype) * h
 
   _mean = _make_summary_statistic('mean')
   _stddev = _make_summary_statistic('stddev')

tensorflow_probability/python/distributions/sample_test.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 # Dependency imports
+from absl.testing import parameterized
 import numpy as np
 
 import tensorflow as tf
@@ -33,7 +34,7 @@ tfd = tfp.distributions
 
 
 @test_util.run_all_in_graph_and_eager_modes
-class SampleDistributionTest(tf.test.TestCase):
+class SampleDistributionTest(tf.test.TestCase, parameterized.TestCase):
 
   def test_everything_scalar(self):
     s = tfd.Sample(tfd.Normal(loc=0, scale=1), 5, validate_args=True)
@@ -160,6 +161,33 @@ class SampleDistributionTest(tf.test.TestCase):
     self.assertAllClose(
         *self.evaluate([expected_lp(y), actual_lp]),
         atol=0., rtol=1e-3)
+
+  @parameterized.parameters(
+      'mean',
+      'stddev',
+      'variance',
+      'mode',
+  )
+  def test_summary_statistic(self, attr):
+    sample_shape = [5, 4]
+    mvn = tfd.Independent(tfd.Normal(loc=tf.zeros([3, 2]), scale=1), 1)
+    d = tfd.Sample(mvn, sample_shape, validate_args=True)
+    self.assertEqual((3,), d.batch_shape)
+    expected_stat = (
+        getattr(mvn, attr)()[:, tf.newaxis, tf.newaxis, :] *
+        tf.ones([3, 5, 4, 2]))
+    actual_stat = getattr(d, attr)()
+    self.assertAllEqual(*self.evaluate([expected_stat, actual_stat]))
+
+  def test_entropy(self):
+    sample_shape = [3, 4]
+    mvn = tfd.Independent(tfd.Normal(loc=0, scale=[[0.25, 0.5]]), 1)
+    d = tfd.Sample(mvn, sample_shape, validate_args=True)
+    expected_entropy = 12 * tf.reduce_sum(
+        input_tensor=mvn.distribution.entropy(),
+        axis=-1)
+    actual_entropy = d.entropy()
+    self.assertAllEqual(*self.evaluate([expected_entropy, actual_entropy]))
 
 
 if __name__ == '__main__':