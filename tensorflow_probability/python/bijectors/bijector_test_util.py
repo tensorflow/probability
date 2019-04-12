Fix MatrixInverseTril log det jacobian
Turns out the correct formula for a p x p tril matrix M is

-(p + 1) * log |M|

(per http://neutrino.aquaphoenix.com/ReactionDiffusion/SERC5chap7.pdf).

Removed the ldj checks from most tests, and instead consolidated them into one test that checks a bunch of random matrices of various sizes, using bijector_test_util.

I also had to reinstate cache-busting in the theoretical fldj routine. I don't know if this ever worked, but it was not working for me without it.

PiperOrigin-RevId: 243172955

tensorflow_probability/python/bijectors/bijector_test_util.py
@@ -274,7 +274,8 @@ def get_fldj_theoretical(bijector,
     output_to_unconstrained = identity_bijector.Identity()
 
   x = tf.convert_to_tensor(value=x)
-  x_unconstrained = input_to_unconstrained.forward(x)
+  x_unconstrained = 1 * input_to_unconstrained.forward(x)
+
   with tf.GradientTape(persistent=True) as tape:
     tape.watch(x_unconstrained)
     f_x = bijector.forward(input_to_unconstrained.inverse(x_unconstrained))

tensorflow_probability/python/bijectors/matrix_inverse_tril.py
@@ -78,47 +78,15 @@ class MatrixInverseTriL(bijector.Bijector):
     return self._forward(y)
 
   def _forward_log_det_jacobian(self, x):
-    # Calculation of the Jacobian:
-    #
-    # Let X = (x_{ij}), 0 <= i,j < n, be a matrix of indeterminates.  Let Z =
-    # X^{-1} where Z = (z_{ij}).  Then
-    #
-    #     dZ/dx_{ij} = (d/dt | t=0) Y(t)^{-1},
-    #
-    # where Y(t) = X + t*E_{ij} and E_{ij} is the matrix with a 1 in the (i,j)
-    # entry and zeros elsewhere.  By the product rule,
-    #
-    #     0 = d/dt [Identity matrix]
-    #       = d/dt [Y Y^{-1}]
-    #       = Y d/dt[Y^{-1}] + dY/dt Y^{-1}
-    #
-    # so
-    #
-    #     d/dt[Y^{-1}] = -Y^{-1} dY/dt Y^{-1}
-    #                  = -Y^{-1} E_{ij} Y^{-1}.
-    #
-    # Evaluating at t=0,
-    #
-    #     dZ/dx_{ij} = -Z E_{ij} Z.
-    #
-    # Taking the (r,s) entry of each side,
-    #
-    #     dz_{rs}/dx_{ij} = -z_{ri}z_{sj}.
-    #
-    # Now, let J be the Jacobian dZ/dX, arranged as the n^2-by-n^2 matrix whose
-    # (r*n + s, i*n + j) entry is dz_{rs}/dx_{ij}.  Considering J as an n-by-n
-    # block matrix with n-by-n blocks, the above expression for dz_{rs}/dx_{ij}
-    # shows that the block at position (r,i) is -z_{ri}Z.  Hence
-    #
-    #          J = -KroneckerProduct(Z, Z),
-    #     det(J) = (-1)^(n^2) (det Z)^(2n)
-    #            = (-1)^n (det X)^(-2n).
+    # For a discussion of this (non-obvious) result, see Note 7.2.2 (and the
+    # sections leading up to it, for context) in
+    # http://neutrino.aquaphoenix.com/ReactionDiffusion/SERC5chap7.pdf
     with tf.control_dependencies(self._assertions(x)):
-      return (-2. *
-              tf.cast(tf.shape(input=x)[-1], dtype_util.base_dtype(x.dtype)) *
-              tf.reduce_sum(
-                  input_tensor=tf.math.log(tf.abs(tf.linalg.diag_part(x))),
-                  axis=-1))
+      matrix_dim = tf.cast(tf.shape(input=x)[-1],
+                           dtype_util.base_dtype(x.dtype))
+      return -(matrix_dim + 1) * tf.reduce_sum(
+          input_tensor=tf.math.log(tf.abs(tf.linalg.diag_part(x))),
+          axis=-1)
 
   def _assertions(self, x):
     if not self.validate_args: