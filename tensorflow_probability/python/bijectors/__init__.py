dea4aa398707b5dbfa8755edff94051b27fc6dd1
Brian Patton <bjp@google.com>
2019-05-06 09:24:19 -0700

    Nominal tweaks to module docstrings.
    PiperOrigin-RevId: 246838715

--- tensorflow_probability/python/bijectors/__init__.py
+++ tensorflow_probability/python/bijectors/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Bijector Ops."""
+"""Bijective transformations."""
 
 from __future__ import absolute_import
 from __future__ import division
--- tensorflow_probability/python/distributions/__init__.py
+++ tensorflow_probability/python/distributions/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Statistical distributions and bijective transformations."""
+"""Statistical distributions."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
