Fix gradients flowing through variables in the old style variational layers.
Fixes https://github.com/tensorflow/probability/issues/409.

PiperOrigin-RevId: 253821240

tensorflow_probability/python/layers/conv_variational_test.py
@@ -24,6 +24,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
+from tensorflow_probability.python.internal import test_case
 from tensorflow.python.framework import test_util  # pylint: disable=g-direct-tensorflow-import
 from tensorflow.python.keras import testing_utils
 from tensorflow.python.layers import utils as tf_layers_util
@@ -710,14 +711,26 @@ class ConvVariational(object):
   def testSequentialConvolution3DFlipout(self):
     self._testLayerInSequential(tfp.layers.Convolution3DFlipout)
 
+  def testGradients(self):
+    net = tf.keras.Sequential([
+        tfp.layers.Convolution1DFlipout(1, 1, data_format=self.data_format),
+        tfp.layers.Convolution1DReparameterization(
+            1, 1, data_format=self.data_format),
+    ])
+    with tf.GradientTape() as tape:
+      y = net(tf.zeros([1, 1, 1]))
+    grads = tape.gradient(y, net.trainable_variables)
+    self.assertLen(grads, 6)
+    self.assertAllNotNone(grads)
+
 
 @test_util.run_all_in_graph_and_eager_modes
-class ConvVariationalTestChannelsFirst(tf.test.TestCase, ConvVariational):
+class ConvVariationalTestChannelsFirst(test_case.TestCase, ConvVariational):
   data_format = 'channels_first'
 
 
 @test_util.run_all_in_graph_and_eager_modes
-class ConvVariationalTestChannelsLast(tf.test.TestCase, ConvVariational):
+class ConvVariationalTestChannelsLast(test_case.TestCase, ConvVariational):
   data_format = 'channels_last'
 
 if __name__ == '__main__':

tensorflow_probability/python/layers/dense_variational_test.py
@@ -24,6 +24,7 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 
+from tensorflow_probability.python.internal import test_case
 from tensorflow.python.framework import test_util  # pylint: disable=g-direct-tensorflow-import
 from tensorflow.python.keras import testing_utils
 
@@ -100,7 +101,7 @@ class MockKLDivergence(object):
 
 
 @test_util.run_all_in_graph_and_eager_modes
-class DenseVariational(tf.test.TestCase):
+class DenseVariational(test_case.TestCase):
 
   def _testKerasLayer(self, layer_class):
     def kernel_posterior_fn(dtype, shape, name, trainable, add_variable_fn):
@@ -515,6 +516,18 @@ class DenseVariational(tf.test.TestCase):
       outputs_ = sess.run(outputs)
 
     self.assertAllEqual(outputs_.shape, [batch_size, out_size])
+
+  def testGradients(self):
+    net = tf.keras.Sequential([
+        tfp.layers.DenseReparameterization(1),
+        tfp.layers.DenseFlipout(1),
+        tfp.layers.DenseLocalReparameterization(1)
+    ])
+    with tf.GradientTape() as tape:
+      y = net(tf.zeros([1, 1]))
+    grads = tape.gradient(y, net.trainable_variables)
+    self.assertLen(grads, 9)
+    self.assertAllNotNone(grads)
 
 if __name__ == '__main__':
   tf.test.main()

tensorflow_probability/python/layers/util.py
@@ -24,6 +24,7 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow_probability.python import distributions as tfd
+from tensorflow_probability.python import util as util
 from tensorflow.python.keras.utils import generic_utils
 
 
@@ -110,8 +111,9 @@ def default_loc_scale_fn(
         constraint=untransformed_scale_constraint,
         dtype=dtype,
         trainable=trainable)
-    scale = (np.finfo(dtype.as_numpy_dtype).eps +
-             tf.nn.softplus(untransformed_scale))
+    scale = util.DeferredTensor(
+        lambda x: (np.finfo(dtype.as_numpy_dtype).eps + tf.nn.softplus(x)),
+        untransformed_scale)
     return loc, scale
   return _fn
 