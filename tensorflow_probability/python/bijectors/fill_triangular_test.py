bcff2ba2d508e36d0131f4024643212fda64550b
Brian Patton <22173987+brianwa84@users.noreply.github.com>
2019-06-19 17:11:13 -0400

    Remove extraneous line

--- tensorflow_probability/python/bijectors/fill_triangular_test.py
+++ tensorflow_probability/python/bijectors/fill_triangular_test.py
@@ -18,6 +18,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+print(sys.version)
+
 # Dependency imports
 import numpy as np
 
