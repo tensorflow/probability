39525ce394d97ce46869f327be3a64c8dccafaaa
Googler <no-reply@google.com>
2019-07-09 07:15:01 -0700

    Improve numerical stability of kld evaluation for normal distributions.
    PiperOrigin-RevId: 257185632

--- tensorflow_probability/python/distributions/normal.py
+++ tensorflow_probability/python/distributions/normal.py
@@ -256,8 +256,7 @@ def _kl_normal_normal(n_a, n_b, name=None):
     one = tf.constant(1, dtype=n_a.dtype)
     two = tf.constant(2, dtype=n_a.dtype)
     half = tf.constant(0.5, dtype=n_a.dtype)
-    s_a_squared = tf.square(n_a.scale)
-    s_b_squared = tf.square(n_b.scale)
-    ratio = s_a_squared / s_b_squared
-    return (tf.square(n_a.loc - n_b.loc) / (two * s_b_squared) + half *
-            (ratio - one - tf.math.log(ratio)))
+    n_b_scale = tf.identity(n_b.scale)
+    log_ratio_squared = 2.0 * (tf.math.log(n_a.scale) - tf.math.log(n_b_scale))
+    return (tf.square(n_a.loc - n_b.loc) / (two * tf.square(n_b_scale)) + half *
+            (tf.math.exp(log_ratio_squared) - one - log_ratio_squared))
