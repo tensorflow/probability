Fixes broken gradient taping caused by is_constant_jacobian caching, simply by removing the constant-Jacobian cache.
Adds bijector_properties_test coverage for MatvecLU, and enables it for AffineScalar.

PiperOrigin-RevId: 257826033

tensorflow_probability/python/bijectors/affine_scalar.py
@@ -143,7 +143,7 @@ class AffineScalar(bijector.Bijector):
     if self.scale is not None:
       x = x / self.scale
     if self.log_scale is not None:
-      x = x / tf.exp(self.log_scale)
+      x = x * tf.exp(-self.log_scale)
     return x
 
   def _forward_log_det_jacobian(self, x):
@@ -152,10 +152,10 @@ class AffineScalar(bijector.Bijector):
     elif self.scale is not None:
       return tf.math.log(tf.abs(self.scale))
     else:
-    # is_constant_jacobian = True for this bijector, hence the
-    # `log_det_jacobian` need only be specified for a single input, as this will
-    # be tiled to match `event_ndims`.
-      return tf.constant(0., dtype=dtype_util.base_dtype(x.dtype))
+      # is_constant_jacobian = True for this bijector, hence the
+      # `log_det_jacobian` need only be specified for a single input, as this
+      # will be tiled to match `event_ndims`.
+      return tf.zeros([], dtype=x.dtype)
 
   def _parameter_control_dependencies(self, is_init):
     if not self.validate_args:

tensorflow_probability/python/bijectors/bijector_properties_test.py
@@ -39,7 +39,7 @@ flags.DEFINE_enum('tf_mode', 'graph', ['eager', 'graph'],
 FLAGS = flags.FLAGS
 
 TF2_FRIENDLY_BIJECTORS = (
-    # 'AffineScalar',  # TODO(b/136265958): Cached jacobian breaks grad to scale
+    'AffineScalar',
     'BatchNormalization',
     'CholeskyOuterProduct',
     'Cumsum',
@@ -50,6 +50,7 @@ TF2_FRIENDLY_BIJECTORS = (
     'IteratedSigmoidCentered',
     'Invert',
     'Kumaraswamy',
+    'MatvecLU',
     'NormalCDF',
     'Ordered',
     'RationalQuadraticSpline',
@@ -65,20 +66,24 @@ BIJECTOR_PARAMS_NDIMS = {
     'AffineScalar': dict(shift=0, scale=0, log_scale=0),
     'Gumbel': dict(loc=0, scale=0),
     'Kumaraswamy': dict(concentration1=0, concentration0=0),
+    'MatvecLU': dict(lower_upper=2, permutation=1),
     'SinhArcsinh': dict(skewness=0, tailweight=0),
-    'RationalQuadraticSpline': dict(bin_widths=2, bin_heights=2, knot_slopes=2),
+    'RationalQuadraticSpline': dict(bin_widths=1, bin_heights=1, knot_slopes=1),
 }
 
 MUTEX_PARAMS = (
     set(['scale', 'log_scale']),
 )
 
+FLDJ = 'forward_log_det_jacobian'
+ILDJ = 'inverse_log_det_jacobian'
+
+INVERT_LDJ = {FLDJ: ILDJ, ILDJ: FLDJ}
+
 NO_LDJ_GRADS_EXPECTED = {
-    'AffineScalar': {'[arg]', 'shift'},
-    'BatchNormalization': {'beta'},
-    'Cumsum': {'[arg]'},
-    'Gumbel': {'loc'},
-    'Identity': {'[arg]'},
+    'AffineScalar': dict(shift={FLDJ, ILDJ}),
+    'BatchNormalization': dict(beta={FLDJ, ILDJ}),
+    'Gumbel': dict(loc={ILDJ}),
 }
 
 
@@ -286,18 +291,24 @@ def codomain_tensors(draw, bijector, shape=None):
 
 def assert_no_none_grad(bijector, method, wrt_vars, grads):
   for var, grad in zip(wrt_vars, grads):
+    expect_grad = var.dtype not in (tf.int32, tf.int64)
     if 'log_det_jacobian' in method:
       if tensor_util.is_ref(var):
-        # We check tensor_util.is_ref to accounts for xs/ys being in vars.
+        # We check tensor_util.is_ref to account for xs/ys being in vars.
         var_name = var.name.rstrip('_0123456789:').split('/')[-1]
       else:
         var_name = '[arg]'
       to_check = bijector.bijector if is_invert(bijector) else bijector
-      if var_name in NO_LDJ_GRADS_EXPECTED.get(type(to_check).__name__, ()):
-        continue
-    if grad is None:
-      raise AssertionError('Missing `{}` -> {} grad for bijector {}'.format(
-          method, var, bijector))
+      to_check_method = INVERT_LDJ[method] if is_invert(bijector) else method
+      if var_name == '[arg]' and bijector.is_constant_jacobian:
+        expect_grad = False
+      exempt_var_method = NO_LDJ_GRADS_EXPECTED.get(type(to_check).__name__, {})
+      if to_check_method in exempt_var_method.get(var_name, ()):
+        expect_grad = False
+
+    if expect_grad != (grad is not None):
+      raise AssertionError('{} `{}` -> {} grad for bijector {}'.format(
+          'Missing' if expect_grad else 'Unexpected', method, var, bijector))
 
 
 @test_util.run_all_in_graph_and_eager_modes
@@ -400,15 +411,31 @@ class BijectorPropertiesTest(tf.test.TestCase, parameterized.TestCase):
     assert_no_none_grad(bijector, 'inverse_log_det_jacobian', wrt_vars, grads)
 
 
+def ensure_nonzero(x):
+  return tf.where(x < 1e-6, tf.constant(1e-3, x.dtype), x)
+
+
 CONSTRAINTS = {
-    'concentration0': tfp_hps.softplus_plus_eps(),
-    'concentration1': tfp_hps.softplus_plus_eps(),
-    'scale': tfp_hps.softplus_plus_eps(),
-    'tailweight': tfp_hps.softplus_plus_eps(),
-    'AffineScalar.scale': tfp_hps.softplus_plus_eps(),
-    'bin_widths': bijector_hps.spline_bin_size_constraint,
-    'bin_heights': bijector_hps.spline_bin_size_constraint,
-    'knot_slopes': bijector_hps.spline_slope_constraint,
+    'concentration0':
+        tfp_hps.softplus_plus_eps(),
+    'concentration1':
+        tfp_hps.softplus_plus_eps(),
+    'scale':
+        tfp_hps.softplus_plus_eps(),
+    'tailweight':
+        tfp_hps.softplus_plus_eps(),
+    'AffineScalar.scale':
+        tfp_hps.softplus_plus_eps(),
+    'bin_widths':
+        bijector_hps.spline_bin_size_constraint,
+    'bin_heights':
+        bijector_hps.spline_bin_size_constraint,
+    'knot_slopes':
+        bijector_hps.spline_slope_constraint,
+    'lower_upper':
+        lambda x: tf.linalg.set_diag(x, ensure_nonzero(tf.linalg.diag_part(x))),
+    'permutation':
+        lambda x: tf.math.top_k(x, k=x.shape[-1]).indices,
 }
 
 

tensorflow_probability/python/internal/hypothesis_testlib.py
@@ -30,6 +30,7 @@ import numpy as np
 import tensorflow.compat.v2 as tf
 from tensorflow_probability.python.internal import assert_util
 from tensorflow_probability.python.internal import distribution_util
+from tensorflow_probability.python.internal import dtype_util
 from tensorflow_probability.python.internal import tensorshape_util
 from tensorflow_probability.python.util.deferred_tensor import DeferredTensor
 
@@ -117,8 +118,10 @@ def constrained_tensors(constraint_fn, shape):
   float32s = hps.floats(-200, 200, allow_nan=False, allow_infinity=False)
 
   def mapper(x):
-    return assert_util.assert_finite(
-        constraint_fn(tf.convert_to_tensor(x)), message='param non-finite')
+    x = constraint_fn(tf.convert_to_tensor(x))
+    if dtype_util.is_floating(x.dtype):
+      x = assert_util.assert_finite(x, message='param non-finite')
+    return x
 
   return hpnp.arrays(
       dtype=np.float32, shape=shape, elements=float32s).map(mapper)