ee70fe3a7da4b4a9ab47e55500bd962e93b1043e
Jeff Pollock <jeffpollock9@gmail.com>
2019-07-17 17:21:41 +0100

    fix copy paste error

--- tensorflow_probability/python/sts/seasonal.py
+++ tensorflow_probability/python/sts/seasonal.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Local Linear Trend model."""
+"""Seasonal model."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
