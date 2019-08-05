Fix numerical stability in OneHotCategorical.   - Also ensure ExpRelaxedOneHotCategorical throws an appropriate error even when shapes are scalar.
PiperOrigin-RevId: 261760680

tensorflow_probability/python/distributions/onehot_categorical.py
@@ -247,8 +247,8 @@ class OneHotCategorical(distribution.Distribution):
     logits = tf.convert_to_tensor(self._logits)
     m = tf.reduce_max(logits, axis=-1, keepdims=True)
     x = logits - m
+    lse_logits = m[..., 0] + tf.reduce_logsumexp(x, axis=-1)
     sum_exp_x = tf.reduce_sum(tf.math.exp(x), axis=-1)
-    lse_logits = m[..., 0] + tf.math.log(sum_exp_x)
     return lse_logits - tf.reduce_sum(
         tf.math.multiply_no_nan(logits, tf.math.exp(x)), axis=-1) / sum_exp_x
 

tensorflow_probability/python/distributions/relaxed_onehot_categorical.py
@@ -371,7 +371,7 @@ class ExpRelaxedOneHotCategorical(distribution.Distribution):
       elif self.validate_args:
         param = tf.convert_to_tensor(param)
         assertions.append(assert_util.assert_greater_equal(
-            tf.shape(param)[-1], 1, message=msg1))
+            tf.shape(param)[-1:], 1, message=msg1))
         # NOTE: For now, we leave out a runtime assertion that
         # `tf.shape(param)[-1] <= tf.int32.max`.  An earlier `tf.shape` call
         # will fail before we get to this point.

tensorflow_probability/python/layers/distribution_layer_test.py
@@ -597,7 +597,7 @@ class OneHotCategoricalTest(tf.test.TestCase):
               steps_per_epoch=n // batch_size,
               shuffle=True)
     self.assertAllClose([[1.6180], [-2.7183]], model.get_weights()[0],
-                        atol=0, rtol=0.1)
+                        atol=0, rtol=0.15)
 
 
 @test_util.run_all_in_graph_and_eager_modes