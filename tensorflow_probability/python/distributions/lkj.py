bd177a89c092a342cc8fcb825662de34702d1537
Brian Patton <bjp@google.com>
2019-08-15 10:22:58 -0700

    Small change to LKJ to make the distribution more numpy-friendly (failure exposed by correlation_cholesky_test).
    PiperOrigin-RevId: 263589774

--- tensorflow_probability/python/distributions/lkj.py
+++ tensorflow_probability/python/distributions/lkj.py
@@ -61,8 +61,9 @@ def _uniform_unit_norm(dimension, shape, dtype, seed):
 def _replicate(n, tensor):
   """Replicate the input tensor n times along a new (major) dimension."""
   # TODO(axch) Does this already exist somewhere?  Should it get contributed?
-  multiples = tf.concat([[n], tf.ones_like(tensor.shape)], axis=0)
-  return tf.tile(tf.expand_dims(tensor, axis=0), multiples)
+  multiples = tf.concat([[n], tf.ones([tf.rank(tensor)], dtype=n.dtype)],
+                        axis=0)
+  return tf.tile(tensor[tf.newaxis], multiples)
 
 
 class LKJ(distribution.Distribution):
