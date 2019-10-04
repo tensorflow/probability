da547991672f81669f257b29cd7004bf16dc25e0
Brian Patton <bjp@google.com>
2019-10-04 10:49:30 -0700

    Append a suffix to distributions/__init__.py to allow for jax-specific wrapping / transforms of the distributions package.
    PiperOrigin-RevId: 272909420

---
+++ tensorflow_probability/python/experimental/substrates/jax/distributions/_init_suffix.py
@@ -0,0 +1,19 @@
+# Copyright 2019 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+# The generated _init_prefix.py (a rewrite of distributions/__init__.py) and
+# this file (_init_suffix.py) are concatenated to produce
+# jax/distributions/__init__.py at build time, allowing for JAX-specific
+# wrapping / modification of the distributions package.
