a2ab03a57adea8cb0111ea75e8c62fc8256b8257
Jacob Burnim <jburnim@google.com>
2019-10-17 10:38:15 -0700

    Unpin gast for tests in Travis

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -74,8 +74,7 @@ install_python_packages() {
 
   # NB: tf-nightly pulls in other deps, like numpy, absl, and six, transitively.
   TF_VERSION_STR=$(find_version_str tf-nightly)
-  pip install tf-nightly==$TF_VERSION_STR \
-    gast==0.2.2
+  pip install tf-nightly==$TF_VERSION_STR
 
   # The following unofficial dependencies are used only by tests.
   # TODO(b/141170087): Unpin Hypothesis version.
