c6d1f0cf0066af2c00124e086338458af2280d24
Christopher Suter <cgs@google.com>
2019-10-21 13:17:39 -0700

    Eliminate hard-coded magic seed in ChiTest; increase tolerance
    PiperOrigin-RevId: 275908446

--- tensorflow_probability/python/distributions/chi_test.py
+++ tensorflow_probability/python/distributions/chi_test.py
@@ -116,12 +116,12 @@ class ChiTest(tfp_test_util.TestCase):
 
     x = a.sample(
         int(8e5),
-        seed=tfp_test_util.test_seed(hardcoded_seed=0, set_eager_seed=False))
+        seed=tfp_test_util.test_seed())
     kl_sample = tf.reduce_mean(a.log_prob(x) - b.log_prob(x), axis=0)
 
     kl_, kl_sample_ = self.evaluate([kl, kl_sample])
     self.assertAllClose(true_kl, kl_, atol=0., rtol=1e-14)
-    self.assertAllClose(true_kl, kl_sample_, atol=0., rtol=5e-3)
+    self.assertAllClose(true_kl, kl_sample_, atol=0., rtol=5e-2)
 
     zero_kl = tfd.kl_divergence(a, a)
     true_zero_kl_, zero_kl_ = self.evaluate([tf.zeros_like(zero_kl), zero_kl])
