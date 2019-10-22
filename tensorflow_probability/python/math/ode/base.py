44d3b417b5ed3b04d78ebdc746a1e04f3f65ace1
Brian Patton <bjp@google.com>
2019-10-22 11:13:01 -0700

    Fix documentation on Solver.solve. Fixes #617
    PiperOrigin-RevId: 276103995

--- tensorflow_probability/python/math/ode/base.py
+++ tensorflow_probability/python/math/ode/base.py
@@ -77,13 +77,13 @@ class Solver(object):
     ```python
     t_init, t0, t1 = 0., 0.5, 1.
     y_init = tf.constant([1., 1.], dtype=tf.float64)
-    A = [[-1., -2.], [-3., -4.]]
+    A = tf.constant([[-1., -2.], [-3., -4.]], dtype=tf.float64)
 
     def ode_fn(t, y):
       return tf.linalg.matvec(A, y)
 
-    results = tfp.math.ode.BDF.solve(ode_fn, t_init, y_init,
-                                     solution_times=[t0, t1])
+    results = tfp.math.ode.BDF().solve(ode_fn, t_init, y_init,
+                                       solution_times=[t0, t1])
     y0 = results.states[0]  # == dot(matrix_exp(A * t0), y_init)
     y1 = results.states[1]  # == dot(matrix_exp(A * t1), y_init)
     ```
