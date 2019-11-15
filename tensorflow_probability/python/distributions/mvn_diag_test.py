Fix broadcasting bug in MVN*.{stddev,variance}
PiperOrigin-RevId: 280690330

tensorflow_probability/python/distributions/mvn_diag_test.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 # Dependency imports
+from absl.testing import parameterized
 import numpy as np
 from scipy import stats
 
@@ -42,7 +43,7 @@ class MultivariateNormalDiagTest(test_util.TestCase):
   def testScalarParams(self):
     mu = -1.
     diag = -5.
-    with self.assertRaisesRegexp(ValueError, "at least 1 dimension"):
+    with self.assertRaisesRegexp(ValueError, 'at least 1 dimension'):
       tfd.MultivariateNormalDiag(mu, diag)
 
   def testVectorParams(self):
@@ -99,7 +100,7 @@ class MultivariateNormalDiagTest(test_util.TestCase):
   def testSingularScaleRaises(self):
     mu = [-1., 1]
     diag = [1., 0]
-    with self.assertRaisesOpError("Singular"):
+    with self.assertRaisesOpError('Singular'):
       dist = tfd.MultivariateNormalDiag(mu, diag, validate_args=True)
       self.evaluate(dist.sample())
 
@@ -154,11 +155,29 @@ class MultivariateNormalDiagTest(test_util.TestCase):
                                                        [0, 0, 6]]])**2.,
         self.evaluate(mvn.covariance()))
 
-  def testVariance(self):
+  @parameterized.named_parameters(
+      dict(testcase_name='static', is_static=True),
+      dict(testcase_name='dynamic', is_static=False))
+  def testVariance(self, is_static):
     mvn = tfd.MultivariateNormalDiag(
-        loc=tf.zeros([2, 3], dtype=tf.float32), validate_args=True)
+        loc=self.maybe_static(tf.zeros([2, 3], dtype=tf.float32), is_static),
+        validate_args=True)
     self.assertAllClose(
-        np.ones([3], dtype=np.float32), self.evaluate(mvn.variance()))
+        np.ones([2, 3], dtype=np.float32), self.evaluate(mvn.variance()))
+
+    mvn = tfd.MultivariateNormalDiag(
+        loc=self.maybe_static(tf.zeros([100, 3], dtype=tf.float32), is_static),
+        scale_identity_multiplier=self.maybe_static(3., is_static),
+        validate_args=True)
+    self.assertAllClose(
+        np.array(9. * np.ones([100, 3])), self.evaluate(mvn.variance()))
+
+    mvn = tfd.MultivariateNormalDiag(
+        loc=self.maybe_static(tf.zeros([100, 3], dtype=tf.float32), is_static),
+        scale_diag=self.maybe_static([3., 3., 3.], is_static),
+        validate_args=True)
+    self.assertAllClose(
+        np.array(9. * np.ones([100, 3])), self.evaluate(mvn.variance()))
 
     mvn = tfd.MultivariateNormalDiag(
         loc=tf.zeros([3], dtype=tf.float32),
@@ -174,11 +193,29 @@ class MultivariateNormalDiagTest(test_util.TestCase):
     self.assertAllClose(
         np.array([[3., 2, 1], [4, 5, 6]])**2., self.evaluate(mvn.variance()))
 
-  def testStddev(self):
+  @parameterized.named_parameters(
+      dict(testcase_name='static', is_static=True),
+      dict(testcase_name='dynamic', is_static=False))
+  def testStddev(self, is_static):
     mvn = tfd.MultivariateNormalDiag(
-        loc=tf.zeros([2, 3], dtype=tf.float32), validate_args=True)
+        loc=self.maybe_static(tf.zeros([2, 3], dtype=tf.float32), is_static),
+        validate_args=True)
     self.assertAllClose(
-        np.ones([3], dtype=np.float32), self.evaluate(mvn.stddev()))
+        np.ones([2, 3], dtype=np.float32), self.evaluate(mvn.stddev()))
+
+    mvn = tfd.MultivariateNormalDiag(
+        loc=self.maybe_static(tf.zeros([100, 3], dtype=tf.float32), is_static),
+        scale_identity_multiplier=self.maybe_static(3., is_static),
+        validate_args=True)
+    self.assertAllClose(
+        np.array(3. * np.ones([100, 3])), self.evaluate(mvn.stddev()))
+
+    mvn = tfd.MultivariateNormalDiag(
+        loc=self.maybe_static(tf.zeros([100, 3], dtype=tf.float32), is_static),
+        scale_diag=self.maybe_static([3., 3., 3.], is_static),
+        validate_args=True)
+    self.assertAllClose(
+        np.array(3. * np.ones([100, 3])), self.evaluate(mvn.stddev()))
 
     mvn = tfd.MultivariateNormalDiag(
         loc=tf.zeros([3], dtype=tf.float32),
@@ -198,9 +235,9 @@ class MultivariateNormalDiagTest(test_util.TestCase):
     num_draws = 50
     dims = 3
     x = np.zeros([num_draws, dims], dtype=np.float32)
-    x_pl = tf1.placeholder_with_default(input=x, shape=[None, dims], name="x")
+    x_pl = tf1.placeholder_with_default(input=x, shape=[None, dims], name='x')
     mu_var = tf1.get_variable(
-        name="mu",
+        name='mu',
         shape=[dims],
         dtype=tf.float32,
         initializer=tf1.initializers.constant(1.))
@@ -345,5 +382,5 @@ class MultivariateNormalDiagTest(test_util.TestCase):
         self.evaluate(d.sample())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   tf.test.main()

tensorflow_probability/python/distributions/mvn_linear_operator.py
@@ -27,6 +27,7 @@ from tensorflow_probability.python.distributions import normal
 from tensorflow_probability.python.distributions import transformed_distribution
 from tensorflow_probability.python.internal import distribution_util
 from tensorflow_probability.python.internal import dtype_util
+from tensorflow_probability.python.internal import prefer_static
 from tensorflow_probability.python.internal import tensor_util
 from tensorflow_probability.python.internal import tensorshape_util
 
@@ -249,29 +250,51 @@ class MultivariateNormalLinearOperator(
 
   def _variance(self):
     if distribution_util.is_diagonal_scale(self.scale):
-      return tf.square(self.scale.diag_part())
+      variance = tf.square(self.scale.diag_part())
     elif (isinstance(self.scale, tf.linalg.LinearOperatorLowRankUpdate) and
           self.scale.is_self_adjoint):
-      return self.scale.matmul(self.scale.adjoint()).diag_part()
+      variance = self.scale.matmul(self.scale.adjoint()).diag_part()
     elif isinstance(self.scale, tf.linalg.LinearOperatorKronecker):
       factors_sq_operators = [
           factor.matmul(factor.adjoint()) for factor in self.scale.operators
       ]
-      return tf.linalg.LinearOperatorKronecker(factors_sq_operators).diag_part()
+      variance = (tf.linalg.LinearOperatorKronecker(factors_sq_operators)
+                  .diag_part())
     else:
-      return self.scale.matmul(self.scale.adjoint()).diag_part()
+      variance = self.scale.matmul(self.scale.adjoint()).diag_part()
+
+    return tf.broadcast_to(
+        variance,
+        prefer_static.broadcast_shape(
+            prefer_static.shape(variance),
+            prefer_static.shape(self.loc)))
 
   def _stddev(self):
     if distribution_util.is_diagonal_scale(self.scale):
-      return tf.abs(self.scale.diag_part())
+      stddev = tf.abs(self.scale.diag_part())
     elif (isinstance(self.scale, tf.linalg.LinearOperatorLowRankUpdate) and
           self.scale.is_self_adjoint):
-      return tf.sqrt(
+      stddev = tf.sqrt(
           tf.linalg.diag_part(self.scale.matmul(self.scale.to_dense())))
     else:
-      return tf.sqrt(
+      stddev = tf.sqrt(
           tf.linalg.diag_part(
               self.scale.matmul(self.scale.to_dense(), adjoint_arg=True)))
+
+    shape = tensorshape_util.concatenate(self.batch_shape, self.event_shape)
+    has_static_shape = tensorshape_util.is_fully_defined(shape)
+    if not has_static_shape:
+      shape = tf.concat([
+          self.batch_shape_tensor(),
+          self.event_shape_tensor(),
+      ], 0)
+
+    if has_static_shape and shape == stddev.shape:
+      return stddev
+
+    # Add dummy tensor of zeros to broadcast.  This is only necessary if shape
+    # != stddev.shape, but we could not determine if this is the case.
+    return stddev + tf.zeros(shape, self.dtype)
 
   def _mode(self):
     return self._mean()