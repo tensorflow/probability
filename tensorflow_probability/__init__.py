2164cf6c317cba2ad3ce2f3779a83c3c1af865b1
Jacob Burnim <jburnim@google.com>
2020-01-15 11:32:05 -0800

    Raise minimum required TF version to 2.1.
    PiperOrigin-RevId: 289901406

--- tensorflow_probability/__init__.py
+++ tensorflow_probability/__init__.py
@@ -53,7 +53,8 @@ def _ensure_tf_install():  # pylint: disable=g-statement-before-imports
   #
   # Update this whenever we need to depend on a newer TensorFlow release.
   #
-  required_tensorflow_version = "1.14"
+  required_tensorflow_version = "2.1"
+#   required_tensorflow_version = "1.15"  # Needed internally -- DisableOnExport
 
   if (distutils.version.LooseVersion(tf.__version__) <
       distutils.version.LooseVersion(required_tensorflow_version)):
