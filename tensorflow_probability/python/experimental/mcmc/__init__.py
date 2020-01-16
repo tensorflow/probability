ab35aa84a4287a846b2ce53db80a745ae941645e
Brian Patton <bjp@google.com>
2020-01-16 15:05:17 -0800

    Move the EllipticalSliceSampler from discussion into tfp.experimental.mcmc
    PiperOrigin-RevId: 290153826

--- tensorflow_probability/python/experimental/mcmc/__init__.py
+++ tensorflow_probability/python/experimental/mcmc/__init__.py
@@ -18,11 +18,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from tensorflow_probability.python.experimental.mcmc.elliptical_slice_sampler import EllipticalSliceSampler
 from tensorflow_probability.python.experimental.mcmc.nuts import NoUTurnSampler
 
 from tensorflow.python.util.all_util import remove_undocumented  # pylint: disable=g-direct-tensorflow-import
 
 _allowed_symbols = [
+    'EllipticalSliceSampler',
     'NoUTurnSampler',
 ]
 
