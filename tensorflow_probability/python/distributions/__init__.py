2e7dfebafb0d1d91ad9ea5fc06c5b9d0daa7ff8e
Jacob Burnim <jburnim@google.com>
2020-01-23 07:51:59 -0800

    Remove deprecated distributions VectorSinhArcsinhDiag and VectorLaplaceDiag.
    PiperOrigin-RevId: 291166159

--- tensorflow_probability/python/distributions/__init__.py
+++ tensorflow_probability/python/distributions/__init__.py
@@ -99,8 +99,6 @@ from tensorflow_probability.python.distributions.uniform import Uniform
 from tensorflow_probability.python.distributions.variational_gaussian_process import VariationalGaussianProcess
 from tensorflow_probability.python.distributions.vector_diffeomixture import VectorDiffeomixture
 from tensorflow_probability.python.distributions.vector_exponential_diag import VectorExponentialDiag
-from tensorflow_probability.python.distributions.vector_laplace_diag import VectorLaplaceDiag
-from tensorflow_probability.python.distributions.vector_sinh_arcsinh_diag import VectorSinhArcsinhDiag
 from tensorflow_probability.python.distributions.von_mises import VonMises
 from tensorflow_probability.python.distributions.von_mises_fisher import VonMisesFisher
 from tensorflow_probability.python.distributions.wishart import WishartLinearOperator
@@ -204,8 +202,6 @@ __all__ = [
     'DirichletMultinomial',
     'Multinomial',
     'VectorDiffeomixture',
-    'VectorLaplaceDiag',
-    'VectorSinhArcsinhDiag',
     'VonMises',
     'VonMisesFisher',
     'WishartLinearOperator',
