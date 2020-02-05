Use jax.nn.softplus for tf.{math,nn}.softplus.     - This fixes overflow issues with hypothesis.     - Also fix up the Independent tests.
PiperOrigin-RevId: 293291541

tensorflow_probability/python/distributions/generalized_pareto_test.py
@@ -222,7 +222,7 @@ class GeneralizedParetoTest(test_util.TestCase):
     conc = tf.constant(2.0)
     _, grads = tfp.math.value_and_gradient(
         lambda *args: tfd.GeneralizedPareto(*args, validate_args=True).sample(  # pylint: disable=g-long-lambda
-            100), [loc, scale, conc])
+            100, seed=test_util.test_seed()), [loc, scale, conc])
     self.assertLen(grads, 3)
     self.assertAllNotNone(grads)
 

tensorflow_probability/python/distributions/independent_test.py
@@ -419,9 +419,12 @@ class IndependentDistributionTest(test_util.TestCase):
     # `dist.distribution.event_shape_tensor()`.
 
     for method in ('batch_shape_tensor', 'event_shape_tensor',
-                   'mode', 'stddev', 'sample', 'entropy'):
+                   'mode', 'stddev', 'entropy'):
       with tfp_hps.assert_no_excessive_var_usage(method, max_permissible=4):
         getattr(dist, method)()
+
+    with tfp_hps.assert_no_excessive_var_usage('sample', max_permissible=4):
+      dist.sample(seed=test_util.test_seed())
 
     for method in ('log_prob', 'log_cdf', 'prob', 'cdf'):
       with tfp_hps.assert_no_excessive_var_usage(method, max_permissible=4):
@@ -446,9 +449,12 @@ class IndependentDistributionTest(test_util.TestCase):
         reinterpreted_batch_ndims=None, validate_args=True)
 
     for method in ('batch_shape_tensor', 'event_shape_tensor',
-                   'mean', 'variance', 'sample'):
+                   'mean', 'variance'):
       with tfp_hps.assert_no_excessive_var_usage(method, max_permissible=4):
         getattr(dist, method)()
+
+    with tfp_hps.assert_no_excessive_var_usage('sample', max_permissible=4):
+      dist.sample(seed=test_util.test_seed())
 
     # In addition to the four reads of `loc`, `scale` described above in
     # `testExcessiveConcretizationOfParams`, the methods below have two more
@@ -472,6 +478,8 @@ class IndependentDistributionTest(test_util.TestCase):
       with tfp_hps.assert_no_excessive_var_usage(method, max_permissible=8):
         getattr(dist, method)(np.zeros((4, 5, 2, 3)))
 
+  @test_util.jax_disable_test_missing_functionality(
+      'Shape sizes are statically known in JAX.')
   def testChangingVariableShapes(self):
     if not tf.executing_eagerly():
       return

tensorflow_probability/python/internal/backend/numpy/numpy_math.py
@@ -778,12 +778,17 @@ softmax = utils.copy_docstring(
     _softmax)
 
 
+# TODO(srvasude): Remove this once
+# https://github.com/google/jax/issues/2107 is fixed.
 @_custom_gradient
 def _softplus(x, name=None):  # pylint: disable=unused-argument
+  # TODO(b/146563881): Investigate improving numerical accuracy here.
   def grad(dy):
     return dy * scipy_special.expit(x)
-  # TODO(b/146563881): Investigate improving numerical accuracy here.
-  return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0.), grad
+  if not JAX_MODE:
+    return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0.), grad
+  return jax.nn.softplus(x), grad
+
 
 softplus = utils.copy_docstring(
     tf.math.softplus,