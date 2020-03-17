5a37beb4dfe102215153ad17a1b48f16b8442aa6
Tom Charnock <tom.charnock@iap.fr>
2020-03-17 13:48:51 +0100

    Still tinkering with setup.py

--- setup.py
+++ setup.py
@@ -1,54 +1,11 @@
-# Copyright 2018 The TensorFlow Probability Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
-"""Install tensorflow_probability."""
-import os
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
 import sys
 
-from setuptools import find_packages
-from setuptools import setup
-from setuptools.command.install import install as InstallCommandBase
-from setuptools.dist import Distribution
-
-# To enable importing version.py directly, we add its path to sys.path.
-version_path = os.path.join(
-    os.path.dirname(__file__), 'tensorflow_probability', 'python')
-sys.path.append(version_path)
-from version import __version__  # pylint: disable=g-import-not-at-top
-
-REQUIRED_PACKAGES = [
-    'six >= 1.10.0',
-    'numpy >= 1.13.3',
-    'decorator',
-    'cloudpickle == 1.1.1',
-    'gast >= 0.2, < 0.3'  # For autobatching
-]
-
-project_name = 'tensorflow-probability'
-
-class BinaryDistribution(Distribution):
-  """This class is needed in order to create OS specific wheels."""
-
-  def has_ext_modules(self):
-    return False
-
-with open('README.md', 'r') as fh:
-  long_description = fh.read()
-
 setup(
-    name=project_name,
-    version=__version__,
+    name="tensorflow-probability",
+    version="conditionalmaf",
     description='Probabilistic modeling and statistical '
                 'inference in TensorFlow',
     long_description=long_description,
@@ -58,16 +15,16 @@ setup(
     url='http://github.com/tomcharnock/probability',
     license='Apache 2.0',
     packages=find_packages(),
-    install_requires=REQUIRED_PACKAGES,
-    # Add in any packaged data.
+    install_requires=[
+      'six >= 1.10.0',
+      'numpy >= 1.13.3',
+      'decorator',
+      'cloudpickle == 1.1.1',
+      'gast >= 0.2, < 0.3'], 
     include_package_data=True,
     package_data={'': ['*.so']},
     exclude_package_data={'': ['BUILD', '*.h', '*.cc']},
     zip_safe=False,
-    distclass=BinaryDistribution,
-    cmdclass={
-        'pip_pkg': InstallCommandBase,
-    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
