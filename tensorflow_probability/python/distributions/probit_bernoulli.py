3af49f526ebbafa411851ded5261d4411ed15780
Brian Patton <bjp@google.com>
2020-03-19 10:42:17 -0700

    Update ProbitBernoulli to use internally-stateless sampling.
    PiperOrigin-RevId: 301847297

--- tensorflow_probability/python/distributions/probit_bernoulli.py
+++ tensorflow_probability/python/distributions/probit_bernoulli.py
@@ -26,6 +26,7 @@ from tensorflow_probability.python.internal import assert_util
 from tensorflow_probability.python.internal import distribution_util
 from tensorflow_probability.python.internal import dtype_util
 from tensorflow_probability.python.internal import reparameterization
+from tensorflow_probability.python.internal import samplers
 from tensorflow_probability.python.internal import special_math
 from tensorflow_probability.python.internal import tensor_util
 
@@ -133,7 +134,7 @@ class ProbitBernoulli(distribution.Distribution):
   def _sample_n(self, n, seed=None):
     probs = self._probs_parameter_no_checks()
     new_shape = tf.concat([[n], tf.shape(probs)], 0)
-    uniform = tf.random.uniform(new_shape, seed=seed, dtype=probs.dtype)
+    uniform = samplers.uniform(new_shape, seed=seed, dtype=probs.dtype)
     sample = tf.less(uniform, probs)
     return tf.cast(sample, self.dtype)
 
