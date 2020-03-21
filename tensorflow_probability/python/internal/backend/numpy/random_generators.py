2ad88b59a3cb38a019532ee20a2f27aa018b462c
Brian Patton <bjp@google.com>
2020-03-20 18:04:16 -0700

    Update Poisson to use internally-stateless sampling.
    PiperOrigin-RevId: 302137333

--- tensorflow_probability/python/internal/backend/numpy/random_generators.py
+++ tensorflow_probability/python/internal/backend/numpy/random_generators.py
@@ -138,7 +138,7 @@ def _poisson(shape, lam, dtype=np.float32, seed=None,
              name=None):  # pylint: disable=unused-argument
   rng = np.random if seed is None else np.random.RandomState(seed & 0xffffffff)
   dtype = utils.common_dtype([lam], dtype_hint=dtype)
-  shape = _ensure_tuple(shape) + np.shape(lam)
+  shape = _ensure_tuple(shape)
   return rng.poisson(lam=lam, size=shape).astype(dtype)
 
 
@@ -164,7 +164,7 @@ if JAX_MODE:
     # lam > 10. A reference implementation can be found here:
     # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/kernels/random_poisson_op.cc#L159-L239
 
-    shape = _ensure_tuple(shape) + np.shape(lam)
+    shape = _ensure_tuple(shape)
     max_iters = (max_iters
                  if max_iters is not None
                  else np.iinfo(np.int32).max)
