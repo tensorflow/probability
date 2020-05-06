Fix mode computation in the c <= 1 case

tensorflow_probability/python/distributions/loglogistic.py
@@ -135,7 +135,7 @@ class LogLogistic(transformed_distribution.TransformedDistribution):
         ]):
       mode = self.scale * tf.math.exp(
         (tf.math.log(c - 1.) - tf.math.log1p(c)) / c)
-      return tf.where(c > 1., mode, np.nan)
+      return tf.where(c > 1., mode, 0.)
 
   def _entropy(self):
     return 2. + tf.math.log(self.scale) - tf.math.log(self.concentration)

tensorflow_probability/python/distributions/loglogistic_test.py
@@ -67,7 +67,7 @@ class LogLogiticTest(test_util.TestCase):
                            validate_args=True)
     mode = scale * ((concentration - 1.) / (concentration + 1.)
                     ) ** (1. / concentration)
-    mode[0] = np.nan
+    mode[0] = 0.
     self.assertAllClose(
       self.evaluate(dist.mode()),
       mode