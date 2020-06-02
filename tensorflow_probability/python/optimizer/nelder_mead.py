0ded4010f921a29f5066115e75d7d5af53806e42
Jeff Pollock <jeffpollock9@gmail.com>
2020-06-02 13:54:34 +0100

    small optimization for `batch_evaluate_objective=True` case

--- tensorflow_probability/python/optimizer/nelder_mead.py
+++ tensorflow_probability/python/optimizer/nelder_mead.py
@@ -801,21 +801,18 @@ def _prepare_args_with_initial_vertex(objective_function,
   simplex_face = initial_vertex + step_sizes * unit_vectors_along_axes
   simplex = tf.concat([tf.expand_dims(initial_vertex, axis=0),
                        simplex_face], axis=0)
-  num_evaluations = 0
   # Evaluate the objective function at the simplex vertices.
   if objective_at_initial_vertex is None:
-    objective_at_initial_vertex = objective_function(initial_vertex)
-    num_evaluations += 1
-
-  objective_at_simplex_face, num_evals = _evaluate_objective_multiple(
-      objective_function, simplex_face, batch_evaluate_objective)
-  num_evaluations += num_evals
-
-  objective_at_simplex = tf.concat(
-      [
-          tf.expand_dims(objective_at_initial_vertex, axis=0),
-          objective_at_simplex_face
-      ], axis=0)
+    objective_at_simplex, num_evaluations = _evaluate_objective_multiple(
+        objective_function, simplex, batch_evaluate_objective)
+  else:
+    objective_at_simplex_face, num_evaluations = _evaluate_objective_multiple(
+        objective_function, simplex_face, batch_evaluate_objective)
+    objective_at_simplex = tf.concat(
+        [
+            tf.expand_dims(objective_at_initial_vertex, axis=0),
+            objective_at_simplex_face
+        ], axis=0)
 
   return (dim,
           num_vertices,
