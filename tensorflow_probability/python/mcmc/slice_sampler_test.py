5793da8a0f0305711126b0f43251eb682b577cd4
Brian Patton <bjp@google.com>
2020-06-05 07:10:36 -0700

    Seed deflake slice_sample_test.
    PiperOrigin-RevId: 314922580

--- tensorflow_probability/python/mcmc/slice_sampler_test.py
+++ tensorflow_probability/python/mcmc/slice_sampler_test.py
@@ -27,38 +27,50 @@ from tensorflow_probability.python import distributions as tfd
 from tensorflow_probability.python.internal import test_util
 
 
+def _get_mode_dependent_settings():
+  if tf.executing_eagerly():
+    num_results = 100
+    tolerance = .2
+  else:
+    num_results = 400
+    tolerance = .1
+  return num_results, tolerance
+
+
 @test_util.test_graph_and_eager_modes
 class SliceSamplerTest(test_util.TestCase):
 
-  def _get_mode_dependent_settings(self):
-    if tf.executing_eagerly():
-      num_results = 100
-      tolerance = .2
-    else:
-      num_results = 400
-      tolerance = .1
-    return num_results, tolerance
-
-  def testOneDimNormal(self):
+  @parameterized.named_parameters(
+      dict(testcase_name='_slow_asserts', asserts=True),
+      dict(testcase_name='_fast_execute_only', asserts=False),
+  )
+  def testOneDimNormal(self, asserts):
     """Sampling from the Standard Normal Distribution."""
     dtype = np.float32
-    num_results, tolerance = self._get_mode_dependent_settings()
+    num_results, tolerance = _get_mode_dependent_settings()
+    # We're not using multiple chains in this test, so dial up samples.
+    num_results = int(num_results * (5 if asserts else .2))
     target = tfd.Normal(loc=dtype(0), scale=dtype(1))
 
+    kernel = tfp.mcmc.SliceSampler(
+        target.log_prob,
+        step_size=1.0,
+        max_doublings=5,
+        seed=test_util.test_seed_stream())
+    if asserts:
+      kernel.one_step = tf.function(kernel.one_step, autograph=False)
     samples, _ = tfp.mcmc.sample_chain(
         num_results=num_results,
         current_state=dtype(1),
-        kernel=tfp.mcmc.SliceSampler(
-            target.log_prob,
-            step_size=1.0,
-            max_doublings=5,
-            seed=test_util.test_seed_stream()),
+        kernel=kernel,
         num_burnin_steps=100,
         parallel_iterations=1)  # For determinism.
 
     sample_mean = tf.reduce_mean(samples, axis=0)
     sample_std = tfp.stats.stddev(samples)
 
+    if not asserts:
+      return
     self.assertAllClose(0., sample_mean, atol=tolerance, rtol=tolerance)
     self.assertAllClose(1., sample_std, atol=tolerance, rtol=tolerance)
 
@@ -72,8 +84,8 @@ class SliceSamplerTest(test_util.TestCase):
     dtype = np.float32
     true_mean = dtype([0, 0])
     true_cov = dtype([[1, 0.5], [0.5, 1]])
-    num_results, tolerance = self._get_mode_dependent_settings()
-    num_chains = 10
+    num_results, tolerance = _get_mode_dependent_settings()
+    num_chains = 16
     # Target distribution is defined through the Cholesky decomposition.
     chol = tf.linalg.cholesky(true_cov)
     target = tfd.MultivariateNormalTriL(loc=true_mean, scale_tril=chol)
@@ -115,8 +127,8 @@ class SliceSamplerTest(test_util.TestCase):
     dtype = np.float32
     true_mean = dtype([0, 4, -8, 2])
     true_cov = np.eye(4, dtype=dtype)
-    num_results, tolerance = self._get_mode_dependent_settings()
-    num_chains = 10
+    num_results, tolerance = _get_mode_dependent_settings()
+    num_chains = 16
     target = tfd.MultivariateNormalTriL(loc=true_mean, scale_tril=true_cov)
 
     # Initial state of the chain
@@ -144,7 +156,7 @@ class SliceSamplerTest(test_util.TestCase):
 
   def testTwoStateParts(self):
     dtype = np.float32
-    num_results, tolerance = self._get_mode_dependent_settings()
+    num_results, tolerance = _get_mode_dependent_settings()
 
     true_loc1 = 1.
     true_scale1 = 1.
@@ -155,7 +167,7 @@ class SliceSamplerTest(test_util.TestCase):
         tfd.Normal(true_loc2, true_scale2),
     ])
 
-    num_chains = 10
+    num_chains = 16
 
     init_state = [np.ones([num_chains], dtype=dtype),
                   np.ones([num_chains], dtype=dtype)]
