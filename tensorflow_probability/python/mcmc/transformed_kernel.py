6bd7310390f2f189fd7b62578a3280e82bdc4ada
rupei <rupei@google.com>
2020-06-10 14:06:28 -0400

    fixed target_log_prob_fn error message bug

--- tensorflow_probability/python/mcmc/transformed_kernel.py
+++ tensorflow_probability/python/mcmc/transformed_kernel.py
@@ -110,8 +110,8 @@ def target_log_prob_getter(kernel):
   kernel_stack = [kernel]
   while 'target_log_prob_fn' not in kernel.parameters:
     if 'inner_kernel' not in kernel.parameters:
-      raise ValueError("None of the nested `inner_kernel`s contains '
-                      '`target_log_prob_fn`")
+      raise ValueError('"None of the nested `inner_kernel`s contains a '
+                      '`target_log_prob_fn`."')
     kernel = kernel.inner_kernel
     kernel_stack.append(kernel)
   return kernel.parameters['target_log_prob_fn'], kernel_stack
