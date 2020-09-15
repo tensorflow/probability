73edd2dde759709438b8112733e3a2a4c9c8fa07
Dave Moore <davmre@google.com>
2020-09-15 12:19:36 -0700

    Add `DeferredModule` to defer construction of distributions and bijectors.
    PiperOrigin-RevId: 331826382

--- tensorflow_probability/python/experimental/__init__.py
+++ tensorflow_probability/python/experimental/__init__.py
@@ -40,6 +40,7 @@ from tensorflow_probability.python.experimental import nn
 from tensorflow_probability.python.experimental import sequential
 from tensorflow_probability.python.experimental import stats
 from tensorflow_probability.python.experimental import substrates
+from tensorflow_probability.python.experimental import util
 from tensorflow_probability.python.experimental import vi
 from tensorflow_probability.python.experimental.composite_tensor import as_composite
 from tensorflow_probability.python.experimental.composite_tensor import register_composite
@@ -60,6 +61,7 @@ _allowed_symbols = [
     'sequential',
     'stats',
     'substrates',
+    'util',
     'vi',
 ]
 
