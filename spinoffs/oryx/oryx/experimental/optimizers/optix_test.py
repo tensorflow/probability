5f6030b7370da145947bd909656b73a84d152e6e
sharadmv <sharadmv@google.com>
2020-11-02 10:42:42 -0800

    [Oryx] Swap optix transformation arguments and update docstring
    PiperOrigin-RevId: 340268416

--- spinoffs/oryx/oryx/experimental/optimizers/optix_test.py
+++ spinoffs/oryx/oryx/experimental/optimizers/optix_test.py
@@ -18,8 +18,8 @@ from absl.testing import absltest
 from absl.testing import parameterized
 import jax
 from jax import random
-import jax.numpy as np
-import numpy as onp
+import jax.numpy as jnp
+import numpy as np
 
 from oryx.core import state
 from oryx.experimental.optimizers import optix
@@ -29,80 +29,80 @@ from oryx.internal import test_util
 class OptixTest(test_util.TestCase):
 
   def test_clip_should_clip_updates(self):
-    self.assertEqual(optix.clip(1.)(0., .5), .5)
-    self.assertEqual(optix.clip(1.)(0., 1.5), 1.)
+    self.assertEqual(optix.clip(1.)(0.5, 0.), .5)
+    self.assertEqual(optix.clip(1.)(1.5, 0.), 1.)
 
   def test_clip_by_global_norm_should_clip_correctly(self):
-    params = [np.zeros(6), np.zeros(10)]
-    updates = [np.ones(6), np.ones(10)]
-    onp.testing.assert_array_equal(
-        optix.clip_by_global_norm(1.)(params, updates)[0], .25 * np.ones(6))
-    onp.testing.assert_array_equal(
-        optix.clip_by_global_norm(1.)(params, updates)[1], .25 * np.ones(10))
-    onp.testing.assert_array_equal(
-        optix.clip_by_global_norm(4.)(params, updates)[0], np.ones(6))
-    onp.testing.assert_array_equal(
-        optix.clip_by_global_norm(4.)(params, updates)[1], np.ones(10))
+    params = [jnp.zeros(6), jnp.zeros(10)]
+    updates = [jnp.ones(6), jnp.ones(10)]
+    np.testing.assert_array_equal(
+        optix.clip_by_global_norm(1.)(updates, params)[0], .25 * jnp.ones(6))
+    np.testing.assert_array_equal(
+        optix.clip_by_global_norm(1.)(updates, params)[1], .25 * jnp.ones(10))
+    np.testing.assert_array_equal(
+        optix.clip_by_global_norm(4.)(updates, params)[0], jnp.ones(6))
+    np.testing.assert_array_equal(
+        optix.clip_by_global_norm(4.)(updates, params)[1], jnp.ones(10))
 
   def test_trace_should_keep_track_of_momentum(self):
-    params = np.zeros(6)
-    updates = np.ones(6)
-    opt = state.init(optix.trace(0.99, False))(random.PRNGKey(0), params,
-                                               updates)
-    onp.testing.assert_array_equal(opt.trace, np.zeros(6))
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.trace, np.ones(6))
-    onp.testing.assert_array_equal(opt(params, updates), 1.99 * np.ones(6))
+    params = jnp.zeros(6)
+    updates = jnp.ones(6)
+    opt = state.init(optix.trace(0.99, False))(random.PRNGKey(0),
+                                               updates, params)
+    np.testing.assert_array_equal(opt.trace, jnp.zeros(6))
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.trace, jnp.ones(6))
+    np.testing.assert_array_equal(opt(updates, params), 1.99 * jnp.ones(6))
 
   def test_trace_should_keep_track_of_momentum_with_nesterov(self):
-    params = np.zeros(6)
-    updates = np.ones(6)
-    opt = state.init(optix.trace(0.99, True))(random.PRNGKey(0), params,
-                                              updates)
-    onp.testing.assert_array_equal(opt.trace, np.zeros(6))
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.trace, np.ones(6))
-    onp.testing.assert_array_equal(
-        opt(params, updates), (1.99 + 0.99**2) * np.ones(6))
+    params = jnp.zeros(6)
+    updates = jnp.ones(6)
+    opt = state.init(optix.trace(0.99, True))(random.PRNGKey(0),
+                                              updates, params)
+    np.testing.assert_array_equal(opt.trace, jnp.zeros(6))
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.trace, jnp.ones(6))
+    np.testing.assert_array_equal(
+        opt(updates, params), (1.99 + 0.99**2) * jnp.ones(6))
 
   def test_scale_by_rms_should_scale_by_rms(self):
-    params = np.zeros(9)
-    updates = 2 * np.ones(9)
-    opt = state.init(optix.scale_by_rms(0.5, 0.))(random.PRNGKey(0), params,
-                                                  updates)
-    onp.testing.assert_array_equal(opt.nu, np.zeros(9))
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.nu, 2 * np.ones(9))
-    onp.testing.assert_array_equal(
-        opt(params, updates), 2 * np.ones(9) / np.sqrt(3.))
+    params = jnp.zeros(9)
+    updates = 2 * jnp.ones(9)
+    opt = state.init(optix.scale_by_rms(0.5, 0.))(random.PRNGKey(0), updates,
+                                                  params)
+    np.testing.assert_array_equal(opt.nu, jnp.zeros(9))
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.nu, 2 * jnp.ones(9))
+    np.testing.assert_array_equal(
+        opt(updates, params), 2 * jnp.ones(9) / jnp.sqrt(3.))
 
   def test_scale_by_stddev_should_scale_by_stddev(self):
-    params = np.zeros(9)
-    updates = 2 * np.ones(9)
-    opt = state.init(optix.scale_by_stddev(0.5, 0.))(random.PRNGKey(0), params,
-                                                     updates)
-    onp.testing.assert_array_equal(opt.mu, np.zeros(9))
-    onp.testing.assert_array_equal(opt.nu, np.zeros(9))
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.mu, np.ones(9))
-    onp.testing.assert_array_equal(opt.nu, 2 * np.ones(9))
-    onp.testing.assert_array_equal(
-        opt(params, updates), 2 * np.ones(9) / np.sqrt(0.75))
+    params = jnp.zeros(9)
+    updates = 2 * jnp.ones(9)
+    opt = state.init(optix.scale_by_stddev(0.5, 0.))(random.PRNGKey(0), updates,
+                                                     params)
+    np.testing.assert_array_equal(opt.mu, jnp.zeros(9))
+    np.testing.assert_array_equal(opt.nu, jnp.zeros(9))
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.mu, jnp.ones(9))
+    np.testing.assert_array_equal(opt.nu, 2 * jnp.ones(9))
+    np.testing.assert_array_equal(
+        opt(updates, params), 2 * jnp.ones(9) / jnp.sqrt(0.75))
 
   def test_scale_by_adam_should_scale_by_adam(self):
-    params = np.zeros(9)
-    updates = 2 * np.ones(9)
+    params = jnp.zeros(9)
+    updates = 2 * jnp.ones(9)
     opt = state.init(optix.scale_by_adam(0.5, 0.5, 0.))(random.PRNGKey(0),
-                                                        params, updates)
-    onp.testing.assert_array_equal(opt.count, 0.)
-    onp.testing.assert_array_equal(opt.mu, np.zeros(9))
-    onp.testing.assert_array_equal(opt.nu, np.zeros(9))
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.count, 1.)
-    onp.testing.assert_array_equal(opt.mu, np.ones(9))
-    onp.testing.assert_array_equal(opt.nu, 2 * np.ones(9))
-    onp.testing.assert_array_equal(
-        opt(params, updates), 1.5 / 0.75 * np.ones(9) / np.sqrt(3 / 0.75))
+                                                        updates, params)
+    np.testing.assert_array_equal(opt.count, 0.)
+    np.testing.assert_array_equal(opt.mu, jnp.zeros(9))
+    np.testing.assert_array_equal(opt.nu, jnp.zeros(9))
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.count, 1.)
+    np.testing.assert_array_equal(opt.mu, jnp.ones(9))
+    np.testing.assert_array_equal(opt.nu, 2 * jnp.ones(9))
+    np.testing.assert_array_equal(
+        opt(updates, params), 1.5 / 0.75 * jnp.ones(9) / jnp.sqrt(3 / 0.75))
 
   def test_scale_by_schedule_should_update_scale(self):
     params = 0.
@@ -112,34 +112,34 @@ class OptixTest(test_util.TestCase):
       return 1. / ((t + 1)**2)
 
     opt = state.init(optix.scale_by_schedule(schedule))(random.PRNGKey(0),
-                                                        params, updates)
-    onp.testing.assert_array_equal(opt.count, 0.)
-    opt = opt.update(params, updates)
-    onp.testing.assert_array_equal(opt.count, 1.)
-    onp.testing.assert_array_equal(opt(params, updates), 0.25)
+                                                        updates, params)
+    np.testing.assert_array_equal(opt.count, 0.)
+    opt = opt.update(updates, params)
+    np.testing.assert_array_equal(opt.count, 1.)
+    np.testing.assert_array_equal(opt(updates, params), 0.25)
 
   def test_add_noise_should_add_noise(self):
     params = 0.
     updates = 1.
-    opt = state.init(optix.add_noise(1., 0., 0))(
-        random.PRNGKey(0), params, updates)
-    onp.testing.assert_array_equal(opt.count, 0.)
-    onp.testing.assert_array_equal(opt.rng_key, random.PRNGKey(0))
-    value, opt = opt.call_and_update(params, updates)
-    onp.testing.assert_array_equal(
+    opt = state.init(optix.add_noise(1., 0., 0))(random.PRNGKey(0), updates,
+                                                 params)
+    np.testing.assert_array_equal(opt.count, 0.)
+    np.testing.assert_array_equal(opt.rng_key, random.PRNGKey(0))
+    value, opt = opt.call_and_update(updates, params)
+    np.testing.assert_array_equal(
         value, 1. + random.normal(random.split(random.PRNGKey(0))[1], ()))
 
   def test_apply_every_should_delay_updates(self):
     params = 0.
     updates = 1.
-    opt = state.init(optix.apply_every(5))(random.PRNGKey(0), params, updates)
-    onp.testing.assert_array_equal(opt.count, 0.)
-    onp.testing.assert_array_equal(opt.grad_acc, 0.)
+    opt = state.init(optix.apply_every(5))(random.PRNGKey(0), updates, params)
+    np.testing.assert_array_equal(opt.count, 0.)
+    np.testing.assert_array_equal(opt.grad_acc, 0.)
     for _ in range(4):
-      value, opt = opt.call_and_update(params, updates)
-      onp.testing.assert_array_equal(value, 0.)
-    value = opt(params, updates)
-    onp.testing.assert_array_equal(value, 5.)
+      value, opt = opt.call_and_update(updates, params)
+      np.testing.assert_array_equal(value, 0.)
+    value = opt(updates, params)
+    np.testing.assert_array_equal(value, 5.)
 
 
 UPDATES = [
@@ -158,13 +158,12 @@ class OptimizerTest(test_util.TestCase):
   def test_optimizer(self, update):
 
     def loss(x):
-      return np.sum(x**2)
+      return jnp.sum(x**2)
 
-    x = np.array([3., 4.])
-    opt = state.init(
-        optix.optimize(loss, update, 500))(random.PRNGKey(0), x)
+    x = jnp.array([3., 4.])
+    opt = state.init(optix.optimize(loss, update, 500))(random.PRNGKey(0), x)
     x = jax.jit(opt.call)(x)
-    onp.testing.assert_allclose(np.zeros(2), x, atol=1e-1, rtol=1e-1)
+    np.testing.assert_allclose(jnp.zeros(2), x, atol=1e-1, rtol=1e-1)
 
 
 if __name__ == '__main__':
