0b05d7ddfb3f59510060717685557c307d056b35
jvdillon <jvdillon@google.com>
2020-11-12 14:37:38 -0800

    Give `tfp.math.value_and_gradient` more natural calling convention for multiple arg functions. Also support variables and variable tracking via tf.Module.
    PiperOrigin-RevId: 342133664

--- tensorflow_probability/python/math/gradient_test.py
+++ tensorflow_probability/python/math/gradient_test.py
@@ -27,6 +27,11 @@ import tensorflow_probability as tfp
 
 from tensorflow_probability.python.internal import test_util
 from tensorflow_probability.python.math.gradient import batch_jacobian
+from tensorflow_probability.python.math.gradient import value_and_gradient_with_auto_expansion
+
+
+tfd = tfp.distributions
+tfm = tfp.math
 
 
 @test_util.test_all_tf_execution_regimes
@@ -37,7 +42,7 @@ class GradientTest(test_util.TestCase):
     f = lambda x: x**2 / 2
     g = lambda x: x
     x = np.concatenate([np.linspace(-100, 100, int(1e1)), [0]], axis=0)
-    y, dydx = self.evaluate(tfp.math.value_and_gradient(f, x))
+    y, dydx = self.evaluate(tfm.value_and_gradient(f, x))
     self.assertAllClose(f(x), y, atol=1e-6, rtol=1e-6)
     self.assertAllClose(g(x), dydx, atol=1e-6, rtol=1e-6)
 
@@ -47,7 +52,7 @@ class GradientTest(test_util.TestCase):
     g = lambda x, y: [y, x]
     args = [np.linspace(0, 100, int(1e1)),
             np.linspace(-100, 0, int(1e1))]
-    y, dydx = self.evaluate(tfp.math.value_and_gradient(f, args))
+    y, dydx = self.evaluate(tfm.value_and_gradient(f, args))
     self.assertAllClose(f(*args), y, atol=1e-6, rtol=1e-6)
     self.assertAllClose(g(*args), dydx, atol=1e-6, rtol=1e-6)
 
@@ -57,9 +62,129 @@ class GradientTest(test_util.TestCase):
     g = lambda x, y: [y + 1., x]
     args = [np.linspace(0, 100, int(1e1)),
             np.linspace(-100, 0, int(1e1))]
-    y, dydx = self.evaluate(tfp.math.value_and_gradient(f, args))
+    y, dydx = self.evaluate(tfm.value_and_gradient(f, args))
     self.assertAllClose(f(*args), y, atol=1e-6, rtol=1e-6)
     self.assertAllClose(g(*args), dydx, atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_multi_input_old_style(self):
+    arg0 = [2., 3., 4.]
+    arg1 = [5., 6., 7.]
+    f_actual = lambda x, y: x * np.log(y)
+    g_actual = lambda x, y: (np.log(y), x / np.array(y))
+    y, dydx = self.evaluate(
+        tfm.value_and_gradient(lambda x, y: x * tf.math.log(y), [arg0, arg1]))
+    self.assertAllClose(f_actual(arg0, arg1), y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose(g_actual(arg0, arg1), dydx, atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_multi_input_no_auto_unpack(self):
+    arg0 = [2., 3., 4.]
+    arg1 = [5., 6., 7.]
+    f_actual = lambda x, y: x * np.log(y)
+    g_actual = lambda x, y: (np.log(y), x / np.array(y))
+
+    # This is how users would typically write things.
+    y, dydx = self.evaluate(
+        tfm.value_and_gradient(lambda x, y: x * tf.math.log(y), arg0, arg1))
+    self.assertAllClose(f_actual(arg0, arg1), y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose(g_actual(arg0, arg1), dydx, atol=1e-6, rtol=1e-6)
+
+    # This is uncommon but possible and unambigous under new style.
+    y, dydx = self.evaluate(tfm.value_and_gradient(
+        lambda x: x[0] * tf.math.log(x[1]), [arg0, arg1],
+        auto_unpack_single_arg=False))
+    self.assertAllClose(f_actual(arg0, arg1), y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose(g_actual(arg0, arg1), dydx, atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_simple_input_no_auto_unpack(self):
+    x = [1., 2., 3.]
+    y, dydx = self.evaluate(tfm.value_and_gradient(
+        tf.math.log, x, auto_unpack_single_arg=False))
+    self.assertAllClose(np.log(x), y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose(1. / np.array(x), dydx, atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_variable_and_constant_identical(self):
+    expected = (2. * np.log(2.), [1. + np.log(2.), 1. + np.log(2.)])
+    x = tf.constant(2.)
+    self.assertAllClose(
+        expected,
+        self.evaluate(tfp.math.value_and_gradient(
+            lambda a, b: a * tf.math.log(x), x, x)),
+        atol=1e-6, rtol=1e-6)
+    x = tf.Variable(2.)
+    self.evaluate(x.initializer)
+    self.assertAllClose(
+        expected,
+        self.evaluate(tfp.math.value_and_gradient(
+            lambda a, b: a*tf.math.log(x), x, x)),
+        atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_docstring_examples(self):
+    # Case 1: argless `f`.
+    x = tf.constant(2.)
+    self.assertAllClose(
+        [np.log(2.), 0.5],
+        self.evaluate(tfm.value_and_gradient(lambda: tf.math.log(x), x)),
+        atol=1e-6, rtol=1e-6)
+
+    # Case 2: packed arguments.
+    self.assertAllClose(
+        [2. * np.log(3.), (np.log(3.), 2. / 3)],
+        self.evaluate(tfm.value_and_gradient(
+            lambda x, y: x * tf.math.log(y), [2., 3.])),
+        atol=1e-6, rtol=1e-6)
+
+    # Case 3: default.
+    x = np.array([1., 2, 3])
+    self.assertAllClose(
+        (np.log(x), 1. / x),
+        self.evaluate(tfm.value_and_gradient(
+            tf.math.log, [1., 2., 3.], auto_unpack_single_arg=False)),
+        atol=1e-6, rtol=1e-6)
+
+  @test_util.numpy_disable_gradient_test
+  @test_util.jax_disable_test_missing_functionality('value_and_gradient')
+  def test_variable_tracking(self):
+    value_and_gradient = value_and_gradient_with_auto_expansion
+    q = tfd.Normal(tf.Variable(1.), tf.Variable(1., trainable=False))
+    r = tfd.Normal(0., tf.Variable(1.))
+    self.evaluate([v.initializer for v in q.variables + r.variables])
+
+    y, dydx = self.evaluate(value_and_gradient(tfd.kl_divergence, q, r))
+    self.assertAllClose(0.5, y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose([[1.], [-1.]], dydx, atol=1e-6, rtol=1e-6)
+
+    y, dydx = self.evaluate(value_and_gradient(
+        lambda: tfd.kl_divergence(q, r), q, r))
+    self.assertAllClose(0.5, y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose([[1.], [-1.]], dydx, atol=1e-6, rtol=1e-6)
+
+    y, dydx = self.evaluate(value_and_gradient(
+        lambda: tfd.kl_divergence(q, r), *q.trainable_variables, r))
+    self.assertAllClose(0.5, y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose([1., [-1.]], dydx, atol=1e-6, rtol=1e-6)
+
+    y, dydx = self.evaluate(value_and_gradient(
+        lambda *_: tfd.kl_divergence(q, r), q, r))
+    self.assertAllClose(0.5, y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose([[1.], [-1.]], dydx, atol=1e-6, rtol=1e-6)
+
+    y, dydx = self.evaluate(value_and_gradient(
+        lambda **kw: tfd.kl_divergence(tfd.Normal(kw['loc_q'], 1),  # pylint: disable=g-long-lambda
+                                       tfd.Normal(0, kw['scale_r'])),
+        loc_q=1., scale_r=1.))
+    self.assertAllClose(0.5, y, atol=1e-6, rtol=1e-6)
+    self.assertAllClose({'loc_q': 1., 'scale_r': -1.}, dydx,
+                        atol=1e-6, rtol=1e-6)
 
   @test_util.numpy_disable_gradient_test
   def test_output_gradients(self):
@@ -68,7 +193,7 @@ class GradientTest(test_util.TestCase):
     x = np.ones([3], dtype=np.float32)
     output_gradients = np.float32([1., 2., 3.])
     y, dydx = self.evaluate(
-        tfp.math.value_and_gradient(f, x, output_gradients=output_gradients))
+        tfm.value_and_gradient(f, x, output_gradients=output_gradients))
     self.assertAllClose(f(x), y, atol=1e-6, rtol=1e-6)
     self.assertAllClose(
         np.dot(output_gradients, jacobian), dydx, atol=1e-6, rtol=1e-6)
@@ -113,7 +238,7 @@ class GradientTest(test_util.TestCase):
     for i in range(np.prod(out_shape)):
       idx = (slice(None),) + np.unravel_index(i, out_shape)
       # pylint: disable=cell-var-from-loop
-      _, grad = tfp.math.value_and_gradient(lambda x, y: f(x, y)[idx], [x, y])
+      _, grad = tfm.value_and_gradient(lambda x, y: f(x, y)[idx], [x, y])
       print(grad[0].shape, jac[0].shape, jac[0][idx].shape)
       self.assertAllClose(grad[0], jac[0][idx])
       self.assertAllClose(grad[1], jac[1][idx])
