76820950787c0641a08f081ece13983d98a1534e
Jeff Pollock <jeffpollock9@gmail.com>
2020-11-24 10:31:38 +0000

    fix doc to be consistent with code

--- tensorflow_probability/examples/statistical_rethinking/rethinking/quap.py
+++ tensorflow_probability/examples/statistical_rethinking/rethinking/quap.py
@@ -58,7 +58,7 @@ def quap(joint_dist, data=None, max_tries=20, initial_position=None, name=None):
       value will be conditioned o this data. If this is `None`, the return
       value will be a quadratic approximation to the distribution itself.
     max_tries: Optional `int` of number of times to run the optimizer internally
-      before raising a `RuntimeError`. Default is 10.
+      before raising a `RuntimeError`. Default is 20.
     initial_position: Optional `dict` to initialize the optimizer. Keys should
       correspond to names in the JointDistribution. Defaults to random draws
       from `joint_dist`.
