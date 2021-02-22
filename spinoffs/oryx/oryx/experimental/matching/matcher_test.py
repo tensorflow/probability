ba936411b4faba0e0b785887bd7e2b2fd4e79d54
sharadmv <sharadmv@google.com>
2021-02-22 12:30:04 -0800

    [Oryx] Add Star, Segment, and Plus matching to pattern matching system
    PiperOrigin-RevId: 358879137

--- spinoffs/oryx/oryx/experimental/matching/matcher_test.py
+++ spinoffs/oryx/oryx/experimental/matching/matcher_test.py
@@ -36,6 +36,22 @@ class MatcherTest(test_util.TestCase):
     self.assertDictEqual(matcher.match(matcher.Not(0.), 1.), {})
     self.assertDictEqual(matcher.match(matcher.Not('a'), 1.), {})
 
+  def test_is_match_returns_the_correct_bool_values_according_to_match(self):
+    self.assertTrue(matcher.is_match(matcher.Not(0.), 1.))
+    self.assertFalse(matcher.is_match(matcher.Not('a'), 'a'))
+
+  def test_is_match_errors_with_star_pattern(self):
+    with self.assertRaises(ValueError):
+      matcher.is_match(matcher.Star(1), 1.)
+
+  def test_match_errors_with_star_pattern(self):
+    with self.assertRaises(ValueError):
+      matcher.match(matcher.Star(1), 1.)
+
+  def test_match_all_errors_with_star_pattern(self):
+    with self.assertRaises(ValueError):
+      list(matcher.match_all(matcher.Star(1), 1.))
+
   def test_tuple_patterns_match_equal_tuples(self):
     self.assertDictEqual(matcher.match((1, 2, 3), (1, 2, 3)), {})
     self.assertDictEqual(matcher.match(((1, 2), 2, 3), ((1, 2), 2, 3)), {})
@@ -99,6 +115,115 @@ class MatcherTest(test_util.TestCase):
     pattern = matcher.Choice((1, 2), (2, 1), name='x')
     self.assertDictEqual(matcher.match(pattern, (1, 2)), {'x': (1, 2)})
     self.assertDictEqual(matcher.match(pattern, (2, 1)), {'x': (2, 1)})
+
+  def test_star_match_correctly_matches_sequence_of_patterns(self):
+    pattern = (matcher.Star(1),)
+    self.assertDictEqual(matcher.match(pattern, ()), {})
+    self.assertDictEqual(matcher.match(pattern, (1,)), {})
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), {})
+    self.assertDictEqual(matcher.match(pattern, (1, 1, 1)), {})
+
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, (1, 2))
+
+  def test_star_match_produces_multiple_matches(self):
+    pattern = (matcher.Star(1), matcher.Star(1))
+    self.assertLen(list(matcher.match_all(pattern, ())), 1)
+    self.assertLen(list(matcher.match_all(pattern, (1,))), 2)
+    self.assertLen(list(matcher.match_all(pattern, (1, 1))), 3)
+
+    pattern = (matcher.Star(1), matcher.Star(1), matcher.Star(1))
+    self.assertLen(list(matcher.match_all(pattern, ())), 1)
+    self.assertLen(list(matcher.match_all(pattern, (1,))), 3)
+    self.assertLen(list(matcher.match_all(pattern, (1, 1))), 6)
+    self.assertLen(list(matcher.match_all(pattern, (1, 1, 1))), 10)
+
+  def test_star_nongreedily_matches_by_default(self):
+    pattern = (matcher.Star(1, name='x'), matcher.Star(1, name='y'))
+    # x will be the smallest possible match
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(x=(), y=(1, 1)))
+
+  def test_star_greedily_matches_when_flag_is_set(self):
+    pattern = (matcher.Star(1, name='x', greedy=True),
+               matcher.Star(1, name='y', greedy=True))
+    # x will be the largset possible match
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(x=(1, 1), y=()))
+
+    pattern = (matcher.Star(1, name='x', greedy=True),
+               matcher.Star(1, name='y', greedy=False))
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(x=(1, 1), y=()))
+
+  def test_star_match_binds_name_to_environment(self):
+    pattern = (matcher.Star(matcher.Var('x')),)
+    self.assertDictEqual(matcher.match(pattern, ()), {})
+    self.assertDictEqual(matcher.match(pattern, (1,)), dict(x=1))
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(x=1))
+
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, (1, 2))
+
+  def test_star_can_nest_to_match_nested_patterns(self):
+    pattern = (matcher.Star((matcher.Star(1),)),)
+    self.assertDictEqual(matcher.match(pattern, ()), {})
+    self.assertDictEqual(matcher.match(pattern, ((),)), {})
+    self.assertDictEqual(matcher.match(pattern, ((1,), (1, 1, 1))), {})
+
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, (1,))
+
+  def test_star_with_accumulate_collects_values(self):
+    pattern = (matcher.Star((matcher.Var('x'), matcher.Var('y')),
+                            accumulate=['y']),)
+    self.assertDictEqual(
+        matcher.match(pattern, ((1, 2), (1, 3))), dict(x=1, y=(2, 3)))
+
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, ((1, 2), (2, 3)))
+
+  def test_star_with_name_binds_result(self):
+    pattern = (matcher.Star(1, name='x'),)
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(x=(1, 1)))
+    pattern = (matcher.Star(matcher.Var('y'), name='x'),)
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), dict(y=1, x=(1, 1)))
+
+  def test_plus_errors_on_empty_tuple(self):
+    pattern = (matcher.Plus(1),)
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, ())
+
+  def test_star_with_plus_matches_nonempty_tuple(self):
+    pattern = (matcher.Plus(1),)
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), {})
+
+  def test_segment_matches_tuple_slices(self):
+    pattern = (matcher.Segment('a'),)
+    self.assertDictEqual(matcher.match(pattern, (1, 2, 3)), {'a': (1, 2, 3)})
+
+    pattern = (matcher.Segment('a'), 2, 3)
+    self.assertDictEqual(matcher.match(pattern, (1, 2, 3)), {'a': (1,)})
+
+    pattern = (matcher.Segment('a'), 2, 3)
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, (1, 2))
+
+  def test_segment_matches_multiple_tuple_slices(self):
+    pattern = (matcher.Segment('a'), matcher.Segment('b'))
+    self.assertLen(list(matcher.match_all(pattern, ())), 1)
+    self.assertLen(list(matcher.match_all(pattern, (1,))), 2)
+    self.assertLen(list(matcher.match_all(pattern, (1, 1))), 3)
+
+  def test_segment_must_be_the_same_when_given_same_name(self):
+    pattern = (matcher.Segment('a'), matcher.Segment('a'))
+    self.assertDictEqual(matcher.match(pattern, ()), {'a': ()})
+    self.assertDictEqual(matcher.match(pattern, (1, 1)), {'a': (1,)})
+    with self.assertRaises(matcher.MatchError):
+      matcher.match(pattern, (1, 1, 1))
+
+    pattern = (matcher.Segment('x'), matcher.Segment('y'), matcher.Segment('x'))
+    matches = list(matcher.match_all(pattern, (1,) * 10))
+    self.assertLen(matches, 6)
+    for i in range(len(matches)):
+      self.assertDictEqual(matches[i], dict(x=(1,) * i, y=(1,) * (10 - 2 * i)))
 
 
 if __name__ == '__main__':
