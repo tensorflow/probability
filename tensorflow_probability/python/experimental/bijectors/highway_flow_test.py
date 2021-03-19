70cc3001f2c527d2332f89df82966919dfefc7ca
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-03-19 18:17:01 +0100

    added test input=inverse

--- tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
+++ tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow.compat.v2 as tf
 import tensorflow_probability as tfp
 from tensorflow_probability.python.internal import test_util
@@ -7,7 +8,13 @@ tfd = tfp.distributions
 
 @test_util.test_all_tf_execution_regimes
 class HighwayFlowTests(test_util.TestCase):
-    pass
+
+    def testBijector(self):
+        width = 5
+        bijector = tfp.experimental.bijectors.build_highway_flow_layer(width)
+        self.assertStartsWith(bijector.name, 'highway_flow')
+        x = np.random.uniform(0., 10., (width,))
+        self.assertAllClose(x, self.evaluate(bijector.inverse(tf.identity(bijector.forward(x)))))
 
 if __name__ == '__main__':
   tf.test.main()
