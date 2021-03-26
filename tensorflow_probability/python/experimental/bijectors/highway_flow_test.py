ab3c882916b89514ff685e9034556c3822a6a916
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-03-26 10:53:59 +0100

    added _cache BijectorCacheWithGreedyAttrs

--- tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
+++ tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
@@ -6,6 +6,20 @@ from tensorflow_probability.python.internal import test_util
 tfb = tfp.bijectors
 tfd = tfp.distributions
 
+
+def inv_f(self, y, N=20):
+    # inverse with Newton iteration
+    x = tf.ones(y.shape)
+    for _ in range(N):
+        x = x - (self.residual_fraction * x + (1. - self.residual_fraction) * tf.math.softplus(x) - y) / (
+            self.df(x))
+    return x
+
+def df(self, x):
+    # derivative of activation
+    return self.residual_fraction + (1. - self.residual_fraction) * tf.math.sigmoid(x) * (1. - tf.math.sigmoid(x))
+
+
 @test_util.test_all_tf_execution_regimes
 class HighwayFlowTests(test_util.TestCase):
 
@@ -13,8 +27,11 @@ class HighwayFlowTests(test_util.TestCase):
         width = 5
         bijector = tfp.experimental.bijectors.build_highway_flow_layer(width)
         self.assertStartsWith(bijector.name, 'highway_flow')
-        x = np.random.uniform(0., 10., (width,))
+        x = tf.random.uniform(0., 10., (width,))
         self.assertAllClose(x, self.evaluate(bijector.inverse(tf.identity(bijector.forward(x)))))
 
+    #def testThatInverseActivationGradientIsCorrect(self):
+
+
 if __name__ == '__main__':
-  tf.test.main()
+    tf.test.main()
