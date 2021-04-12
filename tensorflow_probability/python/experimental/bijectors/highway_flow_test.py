bf3bdd1f8bcd6555e1ec2d1f03c28bfb25bca6eb
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-04-12 15:13:01 +0200

    changed line length to 80 and minor changes in test

--- tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
+++ tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
@@ -6,7 +6,9 @@ from tensorflow_probability.python.internal import test_util
 
 tfb = tfp.bijectors
 tfd = tfp.distributions
-# seed = test_util.test_seed(sampler_type='stateless')
+
+seed = 1  # test_util.test_seed(sampler_type='stateless')
+
 
 def _dx(x, activation):
     if activation == 'sigmoid':
@@ -21,7 +23,9 @@ def _activation_log_det_jacobian(x, residual_fraction, activation):
     if activation == 'none':
         return tf.zeros(x.shape[0])
     else:
-        return tf.reduce_sum(tf.math.log(residual_fraction + (1 - residual_fraction) * _dx(x, activation)), -1)
+        return tf.reduce_sum(tf.math.log(
+            residual_fraction + (1 - residual_fraction) * _dx(x, activation)),
+            -1)
 
 
 @test_util.test_all_tf_execution_regimes
@@ -31,16 +35,26 @@ class HighwayFlowTests(test_util.TestCase):
         width = 2
         for dim in range(2):
             if dim == 0:
-                x = tf.ones((5, width)) # * samplers.uniform((5, width), -1., 1., seed=seed)
+                x = tf.ones((5,
+                             width)) * samplers.uniform((5, width), minval=-1.,
+                                                        maxval=1.,
+                                                        seed=seed)
             elif dim == 1:
-                x = tf.ones((5, width, width)) # * samplers.uniform((5, width, width), -1., 1., seed=seed)
+                x = tf.ones((5, width,
+                             width)) * samplers.uniform((5, width, width),
+                                                        minval=-1.,
+                                                        maxval=1., seed=seed)
 
-            bijector = tfp.experimental.bijectors.build_highway_flow_layer(width, activation_fn=tf.nn.softplus)
+            bijector = tfp.experimental.bijectors.build_highway_flow_layer(
+                width, activation_fn=tf.nn.softplus)
             self.evaluate([v.initializer for v in bijector.trainable_variables])
             self.assertStartsWith(bijector.name, 'highway_flow')
-            self.assertAllClose(x, bijector.inverse(tf.identity(bijector.forward(x))))
-            self.assertAllClose(bijector.forward_log_det_jacobian(x, event_ndims=dim+1),
-                                -bijector.inverse_log_det_jacobian(tf.identity(bijector.forward(x)), event_ndims=dim+1))
+            self.assertAllClose(x, bijector.inverse(
+                tf.identity(bijector.forward(x))))
+            self.assertAllClose(
+                bijector.forward_log_det_jacobian(x, event_ndims=dim + 1),
+                -bijector.inverse_log_det_jacobian(
+                    tf.identity(bijector.forward(x)), event_ndims=dim + 1))
 
     def testJacobianWithActivation(self):
         activations = ['softplus']
@@ -69,13 +83,20 @@ class HighwayFlowTests(test_util.TestCase):
             )
 
             self.evaluate([v.initializer for v in bijector.trainable_variables])
-            x = tf.ones((batch_size, width)) # * samplers.uniform((batch_size, width), -10., 10., seed=seed)
+            x = tf.ones((batch_size,
+                         width)) * samplers.uniform((batch_size, width), -10.,
+                                                    10., seed=seed)
             if activation == 'none':
                 y = x
             else:
-                y = residual_fraction * x + (1 - residual_fraction) * activation_fn(x)
-            expected_forward_log_det_jacobian = _activation_log_det_jacobian(x, residual_fraction, activation)
-            expected_inverse_log_det_jacobian = -expected_forward_log_det_jacobian
+                y = residual_fraction * x + (
+                        1 - residual_fraction) * activation_fn(x)
+            expected_forward_log_det_jacobian = \
+                _activation_log_det_jacobian(x,
+                                             residual_fraction,
+                                             activation)
+            expected_inverse_log_det_jacobian = \
+                -expected_forward_log_det_jacobian
             self.assertAllClose(y, bijector.forward(x))
             self.assertAllClose(x, bijector.inverse(y))
             self.assertAllClose(
@@ -86,7 +107,6 @@ class HighwayFlowTests(test_util.TestCase):
                 expected_forward_log_det_jacobian,
                 bijector.forward_log_det_jacobian(x, event_ndims=1),
             )
-
 
     def testResidualFractionGradientsWithCenteredDifference(self):
         width = 4
@@ -101,7 +121,8 @@ class HighwayFlowTests(test_util.TestCase):
             upper_diagonal_weights_matrix=tf.Variable(tf.eye(width)),
             lower_diagonal_weights_matrix=tf.Variable(tf.eye(width))
         )
-        target = tfd.MultivariateNormalDiag(loc=tf.zeros(width), scale_diag=tf.ones(width))
+        target = tfd.MultivariateNormalDiag(loc=tf.zeros(width),
+                                            scale_diag=tf.ones(width))
         x = tf.ones((batch_size, width))
         with tf.GradientTape() as g:
             g.watch(bijector.residual_fraction)
@@ -111,14 +132,15 @@ class HighwayFlowTests(test_util.TestCase):
         h = 1e-4
         eps = 1e-6
 
-        bijector.residual_fraction = residual_fraction + h
+        bijector._residual_fraction = residual_fraction + h
         y1 = tf.reduce_mean(target.log_prob(bijector.forward(x)))
-        bijector.residual_fraction = residual_fraction - h
+        bijector._residual_fraction = residual_fraction - h
         y2 = tf.reduce_mean(target.log_prob(bijector.forward(x)))
 
         manual_grad = (y1 - y2) / (2 * h)
 
-        diff = tf.math.abs(tf_grad - manual_grad) / tf.reduce_max((eps, tf.math.abs(tf_grad) + tf.math.abs(manual_grad)))
+        diff = tf.math.abs(tf_grad - manual_grad) / tf.reduce_max(
+            (eps, tf.math.abs(tf_grad) + tf.math.abs(manual_grad)))
         self.assertGreater(1e-4, diff)
 
 
