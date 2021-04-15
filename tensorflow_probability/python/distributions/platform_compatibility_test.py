9cf9f42f7a599e3e922e55c36ff15d6e9238e93d
Srinivas Vasudevan <srvasude@google.com>
2021-04-15 07:21:55 -0700

    Replace TF2_FRIENDLY_DISTS with a blocklist TF2_UNFRIENDLY_DISTS.     - Catches some distributions that were not being tested.     - Update tolerances on some tests.
    PiperOrigin-RevId: 368634503

--- tensorflow_probability/python/distributions/platform_compatibility_test.py
+++ tensorflow_probability/python/distributions/platform_compatibility_test.py
@@ -48,11 +48,13 @@ XLA_UNFRIENDLY_DISTS = frozenset([
     'Bates',
     # TODO(b/159996966)
     'Gamma',
+    'ExponentiallyModifiedGaussian',  # Fails in TF1
     # TODO(b/173546024)
     'GeneralizedExtremeValue',
     # TODO(b/163118820)
     'LogLogistic',
     'LogNormal',
+    'LogitNormal',
     # TODO(b/162935914): Needs to use XLA friendly Poisson sampler.
     'NegativeBinomial',
     'NormalInverseGaussian',  # Fails in TF1.
@@ -72,11 +74,17 @@ XLA_UNFRIENDLY_DISTS = frozenset([
 
 NO_SAMPLE_PARAM_GRADS = {
     'Deterministic': ('atol', 'rtol'),
+    'VectorDeterministic': ('atol', 'rtol'),
 }
 
-NO_LOG_PROB_PARAM_GRADS = ('Deterministic', 'Empirical')
+NO_LOG_PROB_PARAM_GRADS = (
+    'Deterministic',
+    'Empirical|event_ndims=0',
+    'Empirical|event_ndims=1',
+    'Empirical|event_ndims=2',
+    'VectorDeterministic')
 
-NO_KL_PARAM_GRADS = ('Deterministic',)
+NO_KL_PARAM_GRADS = ('Deterministic', 'VectorDeterministic')
 
 EXTRA_TENSOR_CONVERSION_DISTS = {
     'RelaxedBernoulli': 1,
@@ -137,7 +145,7 @@ XLA_LOGPROB_ATOL.update({
     'BetaQuotient': 1e-4,
     'Binomial': 5e-6,
     'Categorical': 7e-6,  # sparse_softmax_cross_entropy_with_logits
-    'DeterminantalPointProcess': 1e-5,
+    'DeterminantalPointProcess': 2e-5,
     'DirichletMultinomial': 1e-4,
     'ExpGamma': 2e-3,  # TODO(b/166257329)
     'ExpInverseGamma': 1.5e-3,  # TODO(b/166257329)
@@ -148,6 +156,7 @@ XLA_LOGPROB_ATOL.update({
     'Kumaraswamy': 4e-5,
     'Logistic': 3e-6,
     'Multinomial': 2e-4,
+    'OneHotCategorical': 6e-6,
     'PowerSpherical': 2e-5,
     'SigmoidBeta': 5e-4,
     'Skellam': 1e-4
@@ -176,13 +185,18 @@ XLA_LOGPROB_RTOL.update({
     'JohnsonSU': 1e-2,
     'LKJ': .07,
     'Multinomial': 3e-4,
+    'MultivariateNormalDiag': 5e-6,
+    'MultivariateNormalFullCovariance': 1e-5,
+    'MultivariateNormalTriL': 1e-5,
     'OneHotCategorical': 1e-3,  # TODO(b/163118820)
     'PERT': 5e-4,
     'Poisson': 3e-2,  # TODO(b/159999573)
     'PowerSpherical': .003,
     'RelaxedBernoulli': 3e-3,
+    'RelaxedOneHotCategorical': 2e-3,  # TODO(b/163118820)
     'SigmoidBeta': 5e-4,
     'TruncatedCauchy': 2e-5,
+    'VectorExponentialDiag': 7e-5,
     'VonMises': 2e-2,  # TODO(b/160000258):
     'VonMisesFisher': 5e-3,
     'WishartTriL': 1e-5,
@@ -217,7 +231,8 @@ class DistributionGradientTapeAndConcretizationTest(test_util.TestCase):
 
   @parameterized.named_parameters(
       {'testcase_name': dname, 'dist_name': dname}
-      for dname in dhps.TF2_FRIENDLY_DISTS)
+      for dname in sorted(list(dhps.INSTANTIABLE_BASE_DISTS.keys()))
+      if dname not in dhps.TF2_UNFRIENDLY_DISTS)
   @hp.given(hps.data())
   @tfp_hps.tfp_hp_settings()
   def testDistribution(self, dist_name, data):
@@ -394,7 +409,8 @@ class DistributionCompositeTensorTest(test_util.TestCase):
   # distributions.
   @parameterized.named_parameters(
       {'testcase_name': dname, 'dist_name': dname}
-      for dname in dhps.TF2_FRIENDLY_DISTS)
+      for dname in sorted(list(dhps.INSTANTIABLE_BASE_DISTS.keys()))
+      if dname not in dhps.TF2_UNFRIENDLY_DISTS)
   @hp.given(hps.data())
   @tfp_hps.tfp_hp_settings()
   def testCompositeTensor(self, dist_name, data):
@@ -429,8 +445,10 @@ class DistributionXLATest(test_util.TestCase):
                         rtol=XLA_LOGPROB_RTOL[dist_name])
 
   @parameterized.named_parameters(
-      {'testcase_name': dname, 'dist_name': dname}
-      for dname in dhps.TF2_FRIENDLY_DISTS if dname not in XLA_UNFRIENDLY_DISTS)
+      {'testcase_name': dname, 'dist_name': dname}  # pylint: disable=g-complex-comprehension
+      for dname in sorted(list(dhps.INSTANTIABLE_BASE_DISTS.keys())) if
+      (dname not in dhps.TF2_UNFRIENDLY_DISTS) and
+      (dname not in XLA_UNFRIENDLY_DISTS))
   @hp.given(hps.data())
   @tfp_hps.tfp_hp_settings()
   def testXLACompile(self, dist_name, data):
