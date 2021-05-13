e194bd48e3f8ba379d3be6200207736e0857e1cb
sharadmv <sharadmv@google.com>
2021-05-12 23:03:14 -0700

    Implement rwb_psum/rwb_pbroadcast in distribute_lib and use them for make_psum_function/make_sharded_log_prob_parts
    PiperOrigin-RevId: 373521564

--- tensorflow_probability/python/internal/distribute_lib.py
+++ tensorflow_probability/python/internal/distribute_lib.py
@@ -19,10 +19,10 @@ from __future__ import division
 from __future__ import print_function
 
 import collections
+import functools
 
 import tensorflow.compat.v2 as tf
 from tensorflow_probability.python.internal import custom_gradient as tfp_custom_gradient
-from tensorflow_probability.python.math import gradient as math_gradient
 
 from tensorflow.python.util import nest  # pylint: disable=g-direct-tensorflow-import
 
@@ -43,13 +43,17 @@ def canonicalize_axis_name(axis_name):
 
 
 def psum(x, axis_name=None):
-  if JAX_MODE:
-    axis_name = canonicalize_axis_name(axis_name)
-    for name in axis_name:
-      x = lax.psum(x, name)
-    return x
-  ctx = tf.distribute.get_replica_context()
-  return ctx.all_reduce('sum', x)
+  axis_name = canonicalize_axis_name(axis_name)
+  for name in axis_name:
+    x = rwb_psum(x, name)
+  return x
+
+
+def pbroadcast(x, axis_name=None):
+  axis_name = canonicalize_axis_name(axis_name)
+  for name in axis_name:
+    x = rwb_pbroadcast(x, name)
+  return x
 
 
 def pmean(x, axis_name=None):
@@ -76,32 +80,119 @@ def get_axis_size(axis_name=None):
   return ctx.num_replicas_in_sync
 
 
-class _DummyGrads(object):
-  """Wraps gradients to preserve structure when computing a custom gradient."""
-
-  def __init__(self, grads):
-    self.grads = grads
-
-  def tree_flatten(self):
-    return (self.grads,), ()
-
-  @classmethod
-  def tree_unflatten(cls, _, xs):
-    return cls(*xs)
-
-  def __repr__(self):
-    return f'_DummyGrads({self.grads})'
+def _rwb_psum_fwd(x, axis_name):
+  if JAX_MODE:
+    axis_name = canonicalize_axis_name(axis_name)
+    out = lax.psum(x, axis_name)
+  else:
+    ctx = tf.distribute.get_replica_context()
+    out = ctx.all_reduce('sum', x)
+  return out, None
 
 
-if JAX_MODE:
-  from jax import tree_util  # pylint: disable=g-import-not-at-top
-  tree_util.register_pytree_node_class(_DummyGrads)
+def _rwb_psum_bwd(axis_name, _, cts):
+  return (rwb_pbroadcast(cts, axis_name),)
 
 
-def make_psum_function(fn, in_axes, out_axes):
-  """Constructs a function that psums over outputs and corrects input gradients.
+@tfp_custom_gradient.custom_gradient(
+    vjp_fwd=_rwb_psum_fwd, vjp_bwd=_rwb_psum_bwd, nondiff_argnums=(1,))
+def rwb_psum(x, axis_name):
+  """Applies a psum with reduce-without-broadcast (RWB) semantics.
 
-  Given a function `fn`, this `make_psum_function` returns a new one that
+  RWB semantics allow taking gradients w.r.t. unmapped variables of functions
+  with psums in them.
+
+  Args:
+    x: a `Tensor` target for the psum.
+    axis_name: A string axis name for the psum.
+  Returns:
+    A `Tensor` that is the result of applying a psum to an input `Tensor`.
+  """
+  return _rwb_psum_fwd(x, axis_name)[0]
+
+
+def _rwb_pbroadcast_fwd(x, _):
+  return x, None
+
+
+def _rwb_pbroadcast_bwd(axis_name, _, cts):
+  return (rwb_psum(cts, axis_name),)
+
+
+@tfp_custom_gradient.custom_gradient(
+    vjp_fwd=_rwb_pbroadcast_fwd,
+    vjp_bwd=_rwb_pbroadcast_bwd,
+    nondiff_argnums=(1,))
+def rwb_pbroadcast(x, axis_name):
+  """Applies a pbroadcast with reduce-without-broadcast (RWB) semantics."""
+  return _rwb_pbroadcast_fwd(x, axis_name)[0]
+
+
+def make_pbroadcast_function(fn, in_axes, out_axes, out_dtype):
+  """Constructs a function that broadcasts inputs over named axes.
+
+  Given a function `fn`, `make_pbroadcast_function` returns a new one that
+  applies `pbroadcast` to input terms according to axis names provided in
+  `in_axes` and `out_axes`. For each output axis in each term out the output of
+  `fn`, inputs that do not have the output axes present are pbroadcasted before
+  that term is computed.
+
+  Args:
+    fn: a callable to be transformed to have proadcasts at its inputs.
+    in_axes: A structure of axis names that should match the structure of the
+      input to `fn`. If the set of input axes for an input value does not match
+      the output axes of a particular output value, the gradient of that output
+      value w.r.t. the input value will be psum-ed over the axes present in the
+      output but not the input.
+    out_axes: A structure of axis names that should match the structure of the
+      output of `fn`. The inputs to `fn` will be pbroadcast-ed before
+      computing output terms according to their output axes.
+    out_dtype: A structure of dtypes that matches the output of `fn`.
+
+  Returns:
+    A new function that applies pbroadcasts to the inputs of the original
+    function.
+  """
+
+  if not isinstance(in_axes, tuple):
+    in_axes = (in_axes,)
+
+  def pbroadcast_fn(*args):
+    nest.assert_shallow_structure(args, in_axes)
+    nest.assert_shallow_structure(out_dtype, out_axes)
+    map_in_axes = nest.map_structure_up_to(args, canonicalize_axis_name,
+                                           in_axes)
+    map_out_axes = nest.map_structure_up_to(out_dtype, canonicalize_axis_name,
+                                            out_axes)
+
+    def _pbroadcast_input(out_axes, x, in_axes):
+      psum_axes = [
+          axis_name for axis_name in out_axes if axis_name not in in_axes
+      ]
+      return pbroadcast(x, psum_axes)
+
+    def _flat_fn_index(i, *args):
+      out = fn(*args)
+      return tf.nest.flatten(out)[i]
+
+    def _flat_fn(*args):
+      outputs = []
+      for i, out_axis in enumerate(nest.flatten_up_to(out_dtype, map_out_axes)):
+        local_args = nest.map_structure_up_to(
+            args, functools.partial(_pbroadcast_input, out_axis), args,
+            map_in_axes)
+        outputs.append(_flat_fn_index(i, *local_args))
+      return tf.nest.pack_sequence_as(out_dtype, outputs)
+
+    return _flat_fn(*args)
+
+  return pbroadcast_fn
+
+
+def make_psum_function(fn, in_axes, out_axes, out_dtype):
+  """Constructs a function that broadcasts inputs over named axes.
+
+  Given a function `fn`, `make_psum_function` returns a new one that
   includes psums over terms according to axis names provided in `out_axes`. It
   also adds psums for the vector-Jacobian product of the outputs of `fn` w.r.t.
   its inputs according to `in_axes` if there are axes in the outputs that are
@@ -118,118 +209,23 @@ def make_psum_function(fn, in_axes, out_axes):
     out_axes: A structure of axis names that should match the structure of the
       output of `fn`. The outputs of `fn` will be psum-med according to their
       respective output axes.
+    out_dtype: A structure of dtypes that matches the output of `fn`.
 
   Returns:
     A new function that applies psums on to the output of the original
     function and corrects the gradient with respect to its inputs.
   """
 
-  if not isinstance(in_axes, tuple):
-    in_axes = (in_axes,)
+  out_axes = nest.map_structure_up_to(out_dtype, canonicalize_axis_name,
+                                      out_axes)
 
-  def _psum_fn_fwd(*args):
-    nest.assert_shallow_structure(args, in_axes)
-    out_parts = fn(*args)
-    nest.assert_shallow_structure(out_parts, out_axes)
-    map_out_axes = nest.map_structure_up_to(out_parts, canonicalize_axis_name,
-                                            out_axes)
-
-    total_out_parts = nest.map_structure_up_to(
-        out_parts,
-        lambda out_part, axis_name: (  # pylint: disable=g-long-lambda
-            psum(out_part, axis_name=axis_name) if axis_name else out_part),
-        out_parts,
-        map_out_axes)
-
-    return total_out_parts, (args, out_parts)
-
-  def _psum_fn_bwd(args_and_out_parts, gs):
-    args, out_parts = args_and_out_parts
-    map_in_axes = nest.map_structure_up_to(args, canonicalize_axis_name,
-                                           in_axes)
-    map_out_axes = nest.map_structure_up_to(out_parts, canonicalize_axis_name,
-                                            out_axes)
-
-    def flat_fn(flat_args):
-      unflat_args = tf.nest.pack_sequence_as(args, flat_args)
-      out_parts = fn(*unflat_args)
-      return tf.nest.flatten(out_parts)
-
-    # Operate with flattened lists, to make it easier to tease-out individual
-    # outputs for the local grads.
-    flat_value = tf.nest.flatten(args)
-    flat_gs = tf.nest.flatten(gs)
-    local_grads = [
-        math_gradient.value_and_gradient(  # pylint: disable=g-complex-comprehension
-            lambda *val: flat_fn(val)[out_idx],  # pylint: disable=cell-var-from-loop
-            flat_value,
-            output_gradients=out_g)[1] for out_idx, out_g in enumerate(flat_gs)
-    ]
-    # Transpose.
-    local_grads = list(zip(*local_grads))
-    # Repack.
-    local_grads = tf.nest.pack_sequence_as(args, [
-        _DummyGrads(tf.nest.pack_sequence_as(out_parts, v)) for v in local_grads
-    ])
-
-    def value_grad(v, in_axis_names, term_grads):
-      """Computes reductions of output gradients.
-
-      A `log_prob_parts` function takes in a list of values and outputs
-      a log density for each input to the function. The vector-Jacobian
-      product (VJP) of a `log_prob_parts` function thus needs to compute the
-      gradient of each output term w.r.t. each input value. This function
-      overrides the default VJP of an output term `j` w.r.t to an input
-      value `i` to include an all-reduce-sum when:
-      1) The gradient of `j` w.r.t. `i` is connected.
-      2) `j` is a sharded term and `i` is an unsharded value.
-
-      If these conditions do not hold, the gradient remains the same and
-      either corresponds to:
-      1) The gradient of a sharded term w.r.t to a sharded value
-      2) The gradient of an unsharded term w.r.t. to an unsharded value.
-      3) The gradient of an unsharded term w.r.t. to an sharded value.
-      In any of these cases, no all-reduce-sum is necessary.
-      Args:
-        v: The output term of a `log_prob_part` function.
-        in_axis_names: A list of axis names indicating whether or not the output
-          term is sharded or not, `None` if no sharding.
-        term_grads: The gradient of the output term w.r.t. to each of the input
-          values to the `log_prob_part` function.
-
-      Returns:
-        The vector Jacobian product of `v` w.r.t. the input parts of the
-        `log_prob_parts` function.
-      """
-      term_grads = term_grads.grads
-
-      def psum_grads(term_grad, out_axis_names):
-        if term_grad is not None:
-          psum_axes = [
-              axis_name for axis_name in out_axis_names
-              if axis_name not in in_axis_names
-          ]
-          if psum_axes:
-            term_grad = psum(term_grad, axis_name=psum_axes)
-        return term_grad
-
-      total_grad = nest.map_structure_up_to(term_grads, psum_grads, term_grads,
-                                            map_out_axes)
-      if all([grad is None for grad in tf.nest.flatten(total_grad)]):
-        return None
-      return tf.add_n([
-          v for v in tf.nest.flatten(total_grad)
-          if tfp_custom_gradient.is_valid_gradient(v)
-      ])
-
-    out = nest.map_structure_up_to(args, value_grad, args, map_in_axes,
-                                   local_grads)
-    return out
-
-  @tfp_custom_gradient.custom_gradient(
-      vjp_fwd=_psum_fn_fwd, vjp_bwd=_psum_fn_bwd)
   def psum_fn(*args):
-    return _psum_fn_fwd(*args)[0]
+    out = make_pbroadcast_function(fn, in_axes, out_axes, out_dtype)(*args)
+
+    def _psum_output(x, out_axis):
+      return psum(x, out_axis)
+
+    return nest.map_structure_up_to(out_dtype, _psum_output, out, out_axes)
 
   return psum_fn
 
@@ -258,4 +254,12 @@ def make_sharded_log_prob_parts(log_prob_parts_fn, axis_names):
   Returns:
     A new log prob parts function that can be run inside of a strategy.
   """
-  return make_psum_function(log_prob_parts_fn, (axis_names,), axis_names)
+
+  def sharded_log_prob_parts_fn(*args):
+    out_dtype = tf.nest.map_structure(lambda x: x.dtype, args)
+    if len(args) == 1:
+      out_dtype = out_dtype[0]
+    return make_psum_function(log_prob_parts_fn, (axis_names,), axis_names,
+                              out_dtype)(*args)
+
+  return sharded_log_prob_parts_fn
