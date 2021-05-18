6304f8a18792b957005e6ecacb0619c165afc8dc
emilyaf <emilyaf@google.com>
2021-05-18 11:06:29 -0700

    Add 20210518 to bad tf-nightly dates since there was no py3.6 wheel.
    PiperOrigin-RevId: 374460285

--- testing/install_test_dependencies.sh
+++ testing/install_test_dependencies.sh
@@ -95,7 +95,7 @@ find_good_tf_nightly_version_str() {
   # These are nightly builds we'd like to avoid for some reason; separated by
   # regex OR operator.
   BAD_NIGHTLY_DATES="20200112\|20200113\|20210119\|20210219\|20210220\|\
-  20210221\|20210222\|20210224\|20210316"
+  20210221\|20210222\|20210224\|20210316\|20210518"
   # This will fail to find version 'X" and log available version strings to
   # stderr. We then sort, remove bad versions and take the last entry. This
   # allows us to avoid hardcoding the main version number, which would then need
