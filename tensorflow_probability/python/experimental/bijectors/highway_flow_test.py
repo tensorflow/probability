6534cc9ddabed8ee592e211e5e2e638525a9c0df
GianluigiSilvestri <gianlu.silvestri@gmail.com>
2021-05-31 14:43:06 +0200

    added test to check activation layer

--- tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
+++ tensorflow_probability/python/experimental/bijectors/highway_flow_test.py
@@ -26,6 +26,20 @@ from tensorflow_probability.python.internal import test_util
 tfb = tfp.bijectors
 tfd = tfp.distributions
 
+def _dx(x, activation):
+  if activation == 'sigmoid':
+    return tf.math.sigmoid(x) * (1 - tf.math.sigmoid(x))
+  elif activation == 'softplus':
+    return tf.math.sigmoid(x)
+  elif activation == 'tanh':
+    return 1. - tf.math.tanh(x) ** 2
+def _activation_log_det_jacobian(x, residual_fraction, activation):
+  if activation == 'none':
+    return tf.zeros(x.shape[0])
+  else:
+    return tf.reduce_sum(tf.math.log(
+      residual_fraction + (1 - residual_fraction) * _dx(x, activation)),
+      -1)
 
 @test_util.test_all_tf_execution_regimes
 class HighwayFlowTests(test_util.TestCase):
@@ -180,6 +194,56 @@ class HighwayFlowTests(test_util.TestCase):
         self.evaluate(fldj),
         atol=1e-5,
         rtol=1e-5)
+
+  def testJacobianWithActivation(self):
+    activations = ['softplus']
+    batch_size = 3
+    width = 4
+    dtype = tf.float32
+    residual_fraction = tf.constant(0.5)
+    for activation in activations:
+      if activation == 'sigmoid':
+        activation_fn = tf.nn.sigmoid
+      elif activation == 'softplus':
+        activation_fn = tf.nn.softplus
+      elif activation == 'tanh':
+        activation_fn = tf.nn.tanh
+      elif activation == 'none':
+        activation_fn = None
+      bijector = tfp.experimental.bijectors.HighwayFlow(
+        residual_fraction=residual_fraction,
+        activation_fn=activation_fn,
+        bias=tf.zeros(width),
+        upper_diagonal_weights_matrix=tf.eye(width),
+        lower_diagonal_weights_matrix=tf.eye(width),
+        gate_first_n=width
+      )
+      self.evaluate(
+        [v.initializer for v in bijector.trainable_variables])
+      x = tf.ones((batch_size,
+                   width)) * samplers.uniform((batch_size, width), -10.,
+                                              10., seed=(0,0))
+      if activation == 'none':
+        y = x
+      else:
+        y = residual_fraction * x + (
+            1 - residual_fraction) * activation_fn(x)
+      expected_forward_log_det_jacobian = \
+        _activation_log_det_jacobian(x,
+                                     residual_fraction,
+                                     activation)
+      expected_inverse_log_det_jacobian = \
+        -expected_forward_log_det_jacobian
+      self.assertAllClose(y, bijector.forward(x))
+      self.assertAllClose(x, bijector.inverse(y))
+      self.assertAllClose(
+        expected_inverse_log_det_jacobian,
+        bijector.inverse_log_det_jacobian(y, event_ndims=1),
+      )
+      self.assertAllClose(
+        expected_forward_log_det_jacobian,
+        bijector.forward_log_det_jacobian(x, event_ndims=1),
+      )
 
   def testBijectorIsDeterministicGivenSeed(self):
     width = 2
