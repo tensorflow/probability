Fix *LDJ ratio computation.
The old code was broken in many ways:

- Multipart bijectors didn't work
- Scalar LDJ bijectors didn't reduce correctly in all situations
- Subclass *LDJ ratio functions didn't correctly override parent class's

There are four major issues with the solution and one small one. The resultant solution feels unsatisfactory.

1) There is no unified interpretation of an LDJ of amulti-part bijector. tfb.Bijector has one piece of code to do it, and tfb.Composition has a different one which are not compatible (tfb.Bijector's is more restrictive). A simple disagreement I ran into is that tfb.Bijector forbids len(set(map(lambda e, me: e - me, event_ndims, min_event_ndims))) > 1 from being True, while tfb.JointMap will happily broadcast component LDJs. This forces me to use two code paths when computing the *ldj ratios rather than just one (tfb.Bijector's), as well as plumb event_ndims args to the custom LDJ ratio functions.

I don't know of this is WAI or a bug, but both behaviors are enshrined in tests.

2) JD's default bijector relies on kwargs to function, which forced me to plumb them throughout the entire machinery, which looks horrid and is likely fragile.

3) I had to put ldj_ratio.py into the `bijector` lib because of circular dependencies. This makes the already complicated code even harder to understand.

4) The kahan summation really should be decided by the bijector and not the LDJ ratio function. Since kahan summation has a small performance penalty and incomplete JAX support, I turned it off by default.

The small issue is that the seemingly innocuous refactor of tfb.Composition broke the testChainDynamicToStatic test, which enshrines some incorrect code's behavior. While I managed to get the test passing, it's concerning that we're testing extremely internal aspects of the code in an end-to-end test.

PiperOrigin-RevId: 380913022

tensorflow_probability/python/bijectors/ldj_ratio_test.py
@@ -36,18 +36,29 @@ class LdjRatioTest(test_util.TestCase):
         tfeb.inverse_log_det_jacobian_ratio(
             tfb.Scale(3.), 0., tfb.Scale(-2.), 0., event_ndims=0))
 
+  def test_scale_2d(self):
+    x = tf.zeros([3, 2])
+    self.assertAllClose(
+        2 * (np.log(3.) - np.log(2.)),
+        tfeb.forward_log_det_jacobian_ratio(
+            tfb.Scale(3.), x, tfb.Scale(-2.), x, event_ndims=1))
+    self.assertAllClose(
+        2 * (np.log(2.) - np.log(3.)),
+        tfeb.inverse_log_det_jacobian_ratio(
+            tfb.Scale(3.), x, tfb.Scale(-2.), x, event_ndims=1))
+
   def test_scale_matvec_diag(self):
     z = tf.zeros([2])
     self.assertAllClose(
         (np.log([3., 2]) - np.log([2., 7])).sum(),
         tfeb.forward_log_det_jacobian_ratio(
             tfb.ScaleMatvecDiag([3., 2]), z, tfb.ScaleMatvecDiag([-2., 7]), z,
-            event_ndims=0))
+            event_ndims=1))
     self.assertAllClose(
         (np.log([2., 7]) - np.log([3., 2])).sum(),
         tfeb.inverse_log_det_jacobian_ratio(
             tfb.ScaleMatvecDiag([3., 2]), z, tfb.ScaleMatvecDiag([-2., 7]), z,
-            event_ndims=0))
+            event_ndims=1))
 
   def test_exp(self):
     b = tfb.Exp()

tensorflow_probability/python/bijectors/scale_matvec_diag.py
@@ -24,6 +24,7 @@ from tensorflow_probability.python.bijectors import ldj_ratio
 from tensorflow_probability.python.bijectors import scale_matvec_linear_operator
 from tensorflow_probability.python.internal import dtype_util
 from tensorflow_probability.python.internal import parameter_properties
+from tensorflow_probability.python.internal import prefer_static as ps
 from tensorflow_probability.python.internal import tensor_util
 
 
@@ -114,8 +115,10 @@ class ScaleMatvecDiag(scale_matvec_linear_operator.ScaleMatvecLinearOperator):
 
 
 @ldj_ratio.RegisterILDJRatio(ScaleMatvecDiag)
-def _ildj_ratio_scale_matvec_diag(p, x, q, y):
-  del x, y
-  return tf.math.reduce_sum(tf.math.log(tf.math.abs(q.scale.diag_part())) -
-                            tf.math.log(tf.math.abs(p.scale.diag_part())),
-                            axis=-1)
+def _ildj_ratio_scale_matvec_diag(p, x, q, y, event_ndims, p_kwargs, q_kwargs):
+  del x, y, p_kwargs, q_kwargs
+  return tf.math.reduce_sum(
+      tf.math.log(tf.math.abs(q.scale.diag_part())) -
+      tf.math.log(tf.math.abs(p.scale.diag_part())),
+      axis=-1 - ps.range(event_ndims),
+  )