c2a61b5080a47f222d38bb73bf52d31e93f3e9e8
Christopher Suter <cgs@google.com>
2021-06-24 08:04:25 -0700

    Avoid tf-nightly with missing py37 support, in tests.
    PiperOrigin-RevId: 381255963

--- testing/install_test_dependencies.sh
+++ testing/install_test_dependencies.sh
@@ -94,7 +94,7 @@ find_good_tf_nightly_version_str() {
   PKG_NAME=$1
   # These are nightly builds we'd like to avoid for some reason; separated by
   # regex OR operator.
-  BAD_NIGHTLY_DATES="20210519\|20210619"
+  BAD_NIGHTLY_DATES="20210519\|20210619\|20210624"
   # This will fail to find version 'X" and log available version strings to
   # stderr. We then sort, remove bad versions and take the last entry. This
   # allows us to avoid hardcoding the main version number, which would then need
