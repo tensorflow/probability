1ba509203e1207bda93008975073dfac2a5d21ad
Dave Moore <davmre@google.com>
2021-07-01 16:16:54 -0700

    Clean up toplevel STS directory by moving components to a subdirectory.
    The components are still exposed from the toplevel module (`tfp.sts.Sum` rather than `tfp.sts.components.Sum`). In the future, we could consider deprecating this access pattern to clean up the module namespace.
    
    PiperOrigin-RevId: 382626548

--- tensorflow_probability/python/sts/__init__.py
+++ tensorflow_probability/python/sts/__init__.py
@@ -19,12 +19,9 @@ from __future__ import division
 from __future__ import print_function
 
 from tensorflow_probability.python.internal import all_util
-from tensorflow_probability.python.sts.autoregressive import Autoregressive
-from tensorflow_probability.python.sts.autoregressive import AutoregressiveStateSpaceModel
+from tensorflow_probability.python.sts.components import *
 from tensorflow_probability.python.sts.decomposition import decompose_by_component
 from tensorflow_probability.python.sts.decomposition import decompose_forecast_by_component
-from tensorflow_probability.python.sts.dynamic_regression import DynamicLinearRegression
-from tensorflow_probability.python.sts.dynamic_regression import DynamicLinearRegressionStateSpaceModel
 from tensorflow_probability.python.sts.fitting import build_factored_surrogate_posterior
 from tensorflow_probability.python.sts.fitting import fit_with_hmc
 from tensorflow_probability.python.sts.fitting import sample_uniform_initial_state
@@ -32,24 +29,9 @@ from tensorflow_probability.python.sts.forecast import forecast
 from tensorflow_probability.python.sts.forecast import impute_missing_values
 from tensorflow_probability.python.sts.forecast import one_step_predictive
 from tensorflow_probability.python.sts.internal.missing_values_util import MaskedTimeSeries
-from tensorflow_probability.python.sts.local_level import LocalLevel
-from tensorflow_probability.python.sts.local_level import LocalLevelStateSpaceModel
-from tensorflow_probability.python.sts.local_linear_trend import LocalLinearTrend
-from tensorflow_probability.python.sts.local_linear_trend import LocalLinearTrendStateSpaceModel
-from tensorflow_probability.python.sts.regression import LinearRegression
-from tensorflow_probability.python.sts.regression import SparseLinearRegression
 from tensorflow_probability.python.sts.regularization import MissingValuesTolerance
 from tensorflow_probability.python.sts.regularization import regularize_series
-from tensorflow_probability.python.sts.seasonal import ConstrainedSeasonalStateSpaceModel
-from tensorflow_probability.python.sts.seasonal import Seasonal
-from tensorflow_probability.python.sts.seasonal import SeasonalStateSpaceModel
-from tensorflow_probability.python.sts.semilocal_linear_trend import SemiLocalLinearTrend
-from tensorflow_probability.python.sts.semilocal_linear_trend import SemiLocalLinearTrendStateSpaceModel
-from tensorflow_probability.python.sts.smooth_seasonal import SmoothSeasonal
-from tensorflow_probability.python.sts.smooth_seasonal import SmoothSeasonalStateSpaceModel
 from tensorflow_probability.python.sts.structural_time_series import StructuralTimeSeries
-from tensorflow_probability.python.sts.sum import AdditiveStateSpaceModel
-from tensorflow_probability.python.sts.sum import Sum
 
 
 _allowed_symbols = [
---
+++ tensorflow_probability/python/sts/components/__init__.py
@@ -0,0 +1,64 @@
+# Copyright 2021 The TensorFlow Probability Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Components for Bayesian structural time series models."""
+
+
+from tensorflow_probability.python.internal import all_util
+from tensorflow_probability.python.sts.components.autoregressive import Autoregressive
+from tensorflow_probability.python.sts.components.autoregressive import AutoregressiveStateSpaceModel
+from tensorflow_probability.python.sts.components.dynamic_regression import DynamicLinearRegression
+from tensorflow_probability.python.sts.components.dynamic_regression import DynamicLinearRegressionStateSpaceModel
+from tensorflow_probability.python.sts.components.local_level import LocalLevel
+from tensorflow_probability.python.sts.components.local_level import LocalLevelStateSpaceModel
+from tensorflow_probability.python.sts.components.local_linear_trend import LocalLinearTrend
+from tensorflow_probability.python.sts.components.local_linear_trend import LocalLinearTrendStateSpaceModel
+from tensorflow_probability.python.sts.components.regression import LinearRegression
+from tensorflow_probability.python.sts.components.regression import SparseLinearRegression
+from tensorflow_probability.python.sts.components.seasonal import ConstrainedSeasonalStateSpaceModel
+from tensorflow_probability.python.sts.components.seasonal import Seasonal
+from tensorflow_probability.python.sts.components.seasonal import SeasonalStateSpaceModel
+from tensorflow_probability.python.sts.components.semilocal_linear_trend import SemiLocalLinearTrend
+from tensorflow_probability.python.sts.components.semilocal_linear_trend import SemiLocalLinearTrendStateSpaceModel
+from tensorflow_probability.python.sts.components.smooth_seasonal import SmoothSeasonal
+from tensorflow_probability.python.sts.components.smooth_seasonal import SmoothSeasonalStateSpaceModel
+from tensorflow_probability.python.sts.components.sum import AdditiveStateSpaceModel
+from tensorflow_probability.python.sts.components.sum import Sum
+
+
+_allowed_symbols = [
+    'AdditiveStateSpaceModel',
+    'Autoregressive',
+    'AutoregressiveStateSpaceModel',
+    'ConstrainedSeasonalStateSpaceModel',
+    'DynamicLinearRegression',
+    'DynamicLinearRegressionStateSpaceModel',
+    'LinearRegression',
+    'LocalLevel',
+    'LocalLevelStateSpaceModel',
+    'LocalLinearTrend',
+    'LocalLinearTrendStateSpaceModel',
+    'MaskedTimeSeries',
+    'Seasonal',
+    'SeasonalStateSpaceModel',
+    'SemiLocalLinearTrend',
+    'SemiLocalLinearTrendStateSpaceModel',
+    'SmoothSeasonal',
+    'SmoothSeasonalStateSpaceModel',
+    'SparseLinearRegression',
+    'StructuralTimeSeries',
+    'Sum',
+]
+
+all_util.remove_undocumented(__name__, _allowed_symbols)
