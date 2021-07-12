Fix bug in particle filter where static evaluation could block the gradient signal.
PiperOrigin-RevId: 384310858

tensorflow_probability/python/experimental/mcmc/particle_filter.py
@@ -500,7 +500,7 @@ def _compute_observation_log_weights(step,
         lambda x, step=step: tf.gather(x, observation_idx), observations)
 
     log_weights = observation_fn(step, particles).log_prob(observation)
-    return ps.where(step_has_observation,
+    return tf.where(step_has_observation,
                     log_weights,
                     tf.zeros_like(log_weights))
 