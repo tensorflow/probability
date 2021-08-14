f179eb7aef62f1940bb8d70589879361f06a0b07
siege <siege@google.com>
2021-08-13 19:28:22 -0700

    Delete tfp.experimental.distributions.JointDensityCoroutine.
    Use a regular JointDistributionCoroutine instead.
    
    PiperOrigin-RevId: 390738547

--- tensorflow_probability/python/experimental/distributions/increment_log_prob_test.py
+++ tensorflow_probability/python/experimental/distributions/increment_log_prob_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Tests for JointDensityCoroutine."""
+"""Tests for IncrementLogProb."""
 
 import tensorflow.compat.v2 as tf
 import tensorflow_probability as tfp
@@ -20,42 +20,29 @@ import tensorflow_probability as tfp
 from tensorflow_probability.python.internal import test_util
 
 tfd = tfp.distributions
-tfde = tfp.experimental.distributions
+tfed = tfp.experimental.distributions
 
 
 @test_util.test_graph_and_eager_modes
 class JointDensityCoroutineTest(test_util.TestCase):
 
-  def test_constructor(self):
-    root = tfd.JointDistributionCoroutine.Root
-
-    @tfde.JointDensityCoroutine
-    def model():
-      w = yield root(tfd.Normal(0., 1.))
-      yield tfd.Normal(w, 1.)
-      yield tfde.IncrementLogProb(5.)
-
-    return model
-
   def test_dtype(self):
-    root = tfd.JointDistributionCoroutine.Root
-
-    @tfde.JointDensityCoroutine
+    @tfd.JointDistributionCoroutine
     def model():
-      w = yield root(tfd.Normal(0., 1.))
+      w = yield tfd.Normal(0., 1.)
       yield tfd.Normal(w, 1.)
-      yield root(tfde.IncrementLogProb(5.))
+      yield tfed.IncrementLogProb(5.)
 
     self.assertEqual((tf.float32, tf.float32, tf.float32), model.dtype)
 
   def test_sample_shape(self):
     root = tfd.JointDistributionCoroutine.Root
 
-    @tfde.JointDensityCoroutine
+    @tfd.JointDistributionCoroutine
     def model():
       w = yield root(tfd.Normal(0., 1.))
       yield tfd.Normal(w, 1.)
-      yield root(tfde.IncrementLogProb(5.))
+      yield root(tfed.IncrementLogProb(5.))
 
     seed = test_util.test_seed()
     sample_shape = [10]
@@ -65,44 +52,28 @@ class JointDensityCoroutineTest(test_util.TestCase):
                      tf.TensorShape(sample_shape) + tf.TensorShape([0]))
 
   def test_log_prob(self):
-    root = tfd.JointDistributionCoroutine.Root
-
-    @tfde.JointDensityCoroutine
-    def model():
-      w = yield root(tfd.Normal(0., 1.))
-      yield tfd.Normal(w, 1.)
-
-    with self.assertRaises(AttributeError):
-      self.evaluate(model.log_prob(2., 3.))
-
-  def test_joint_distribution_coroutine(self):
-    # This tests that ImplementLogProb doesn't work with
-    # Joint>>>Distribution<<<Coroutine.
-    root = tfd.JointDistributionCoroutine.Root
-    increment_amount = 5.
 
     @tfd.JointDistributionCoroutine
     def model():
-      w = yield root(tfd.Normal(0., 1.))
+      w = yield tfd.Normal(0., 1.)
       yield tfd.Normal(w, 1.)
-      yield root(tfde.IncrementLogProb(increment_amount))
+      yield tfed.IncrementLogProb(5.)
 
     with self.assertRaises(AttributeError):
       self.evaluate(model.log_prob(2., 3., ()))
 
   def test_unnormalized_log_prob(self):
-    root = tfd.JointDistributionCoroutine.Root
     increment_amount = 5.
 
-    @tfde.JointDensityCoroutine
+    @tfd.JointDistributionCoroutine
     def model_with_increment():
-      w = yield root(tfd.Normal(0., 1.))
+      w = yield tfd.Normal(0., 1.)
       yield tfd.Normal(w, 1.)
-      yield root(tfde.IncrementLogProb(increment_amount))
+      yield tfed.IncrementLogProb(increment_amount)
 
     @tfd.JointDistributionCoroutine
     def model_without_increment():
-      w = yield root(tfd.Normal(0., 1.))
+      w = yield tfd.Normal(0., 1.)
       yield tfd.Normal(w, 1.)
 
     self.assertAlmostEqual(
@@ -110,40 +81,18 @@ class JointDensityCoroutineTest(test_util.TestCase):
         self.evaluate(model_without_increment.log_prob(
             (2., 3.))) + increment_amount)
 
-  def test_unnormalized_log_prob_and_root(self):
-    root = tfd.JointDistributionCoroutine.Root
-    increment_amount = 5.
-
-    @tfde.JointDensityCoroutine
-    def model_with_increment():
-      yield root(tfde.IncrementLogProb(increment_amount))
-      w = yield root(tfd.Normal(0., 1.))
-      yield tfd.Normal(w, 1.)
-
-    @tfd.JointDistributionCoroutine
-    def model_without_increment():
-      w = yield root(tfd.Normal(0., 1.))
-      yield tfd.Normal(w, 1.)
-
-    self.assertAlmostEqual(
-        self.evaluate(model_with_increment.unnormalized_log_prob(((), 2., 3.))),
-        self.evaluate(model_without_increment.log_prob(
-            (2., 3.))) + increment_amount,
-        places=6)
-
   def test_unnormalized_log_prob_batch(self):
-    root = tfd.JointDistributionCoroutine.Root
     increment_amount = tf.convert_to_tensor([5., 6.])
 
-    @tfde.JointDensityCoroutine
+    @tfd.JointDistributionCoroutine
     def model_with_increment():
-      w = yield root(tfd.Normal([0., 1.], 2.))
+      w = yield tfd.Normal([0., 1.], 2.)
       yield tfd.Normal(w, 3.)
-      yield root(tfde.IncrementLogProb(increment_amount))
+      yield tfed.IncrementLogProb(increment_amount)
 
     @tfd.JointDistributionCoroutine
     def model_without_increment():
-      w = yield root(tfd.Normal([0., 1.], 2.))
+      w = yield tfd.Normal([0., 1.], 2.)
       yield tfd.Normal(w, 3.)
 
     self.assertAllClose(
