64ddcc4987e4362c3898c521274d7a4b9ddac371
Dave Moore <davmre@google.com>
2021-08-23 19:20:53 -0700

    Support associative scans over nonzero values of `axis`.
    PiperOrigin-RevId: 392569638

--- tensorflow_probability/python/math/scan_associative.py
+++ tensorflow_probability/python/math/scan_associative.py
@@ -14,81 +14,120 @@
 # ============================================================================
 """Utilities for parallel calculation of prefix sums."""
 
+import functools
 import numpy as np
 import tensorflow.compat.v2 as tf
 
-from tensorflow_probability.python.internal import prefer_static
+from tensorflow_probability.python.internal import prefer_static as ps
 
 
-def _interleave(a, b):
-  """Interleaves two `Tensor`s along their first axis."""
+def _slice_along_axis(x, start=0, stop=None, step=1, axis=0):
+  """Slices a Tensor along the given axis."""
+  axis_ = tf.get_static_value(axis)
+  if axis_ is None:  # Non-static axis: use `gather`.
+    axis_len = ps.shape(x)[axis]
+    start = 0 if start is None else start if start >= 0 else start + axis_len
+    stop = axis_len if stop is None else stop if stop >= 0 else stop + axis_len
+    return tf.gather(x, tf.range(start, stop, delta=step), axis=axis)
+
+  # Static axis: construct explicit slice sequence.
+  axis = int(axis_)
+  if axis >= 0:
+    slices = [slice(None)] * axis + [slice(start, stop, step)]
+  else:
+    slices = [Ellipsis, slice(start, stop, step)] + [slice(None)] * (-1 - axis)
+  return x[tuple(slices)]
+
+
+def _interleave(a, b, axis):
+  """Interleaves two `Tensor`s along the given axis."""
   # [a b c ...] [d e f ...] -> [a d b e c f ...]
-  num_elems_a = prefer_static.shape(a)[0]
-  num_elems_b = prefer_static.shape(b)[0]
+  num_elems_a = ps.shape(a)[axis]
+  num_elems_b = ps.shape(b)[axis]
+
+  # Note that interleaving implies rank(a)==rank(b).
+  axis = ps.where(axis >= 0, axis, ps.rank(a) + axis)
+  axis = (int(axis)  # Avoid ndarray values.
+          if tf.get_static_value(axis) is not None
+          else axis)
 
   def _interleave_with_b(a):
     return tf.reshape(
-        tf.stack([a, b], axis=1),
-        prefer_static.concat([[2 * num_elems_b],
-                              prefer_static.shape(a)[1:]], axis=0))
-  return prefer_static.cond(
-      prefer_static.equal(num_elems_a, num_elems_b + 1),
-      lambda: tf.concat([_interleave_with_b(a[:-1]), a[-1:]], axis=0),
+        # Work around lack of support for Tensor axes in `tf.stack` by using
+        # `concat` and `expand_dims` instead.
+        tf.concat([tf.expand_dims(a, axis=axis + 1),
+                   tf.expand_dims(b, axis=axis + 1)],
+                  axis=axis + 1),
+        ps.concat(
+            [
+                ps.shape(a)[:axis],
+                [2 * num_elems_b],
+                ps.shape(a)[axis + 1:]
+            ],
+            axis=0))
+  return ps.cond(
+      ps.equal(num_elems_a, num_elems_b + 1),
+      lambda: tf.concat([  # pylint: disable=g-long-lambda
+          _interleave_with_b(_slice_along_axis(a, None, -1, axis=axis)),
+          _slice_along_axis(a, -1, None, axis=axis)], axis=axis),
       lambda: _interleave_with_b(a))
 
 
-def _validate_elem_length(max_num_levels, elems_flat):
+def _validate_elem_length(max_num_levels, elems_flat, axis):
   """Checks that elems all have the same length, and returns that length."""
   assertions = []
 
-  elem_length = prefer_static.shape(elems_flat[0])[0]
+  elem_length = ps.shape(elems_flat[0])[axis]
 
   # The default size limit will overflow a 32-bit int, so make sure we're
   # using 64-bit.
-  size_limit = 2**(prefer_static.cast(max_num_levels, np.int64) + 1)
-  enough_levels = prefer_static.less(
-      prefer_static.cast(elem_length, np.int64), size_limit)
+  size_limit = 2**(ps.cast(max_num_levels, np.int64) + 1)
+  enough_levels = ps.less(ps.cast(elem_length, np.int64), size_limit)
   enough_levels_ = tf.get_static_value(enough_levels)
   if enough_levels_ is None:
     assertions.append(
         tf.debugging.assert_equal(
             enough_levels, True,
-            message='Input `Tensor`s must have first axis dimension less than'
-            ' `2**(max_num_levels + 1)`'
+            message='Input `Tensor`s must have dimension less than'
+            ' `2**(max_num_levels + 1)` along `axis=={}`.'
             ' (saw: {} which is not less than 2**{} == {})'.format(
+                axis,
                 elem_length,
                 max_num_levels,
                 size_limit)))
   elif not enough_levels_:
     raise ValueError(
-        'Input `Tensor`s must have first axis dimension less than'
-        ' `2**(max_num_levels + 1)`'
+        'Input `Tensor`s must have dimension less than'
+        ' `2**(max_num_levels + 1)` along `axis == {}`'
         ' (saw: {} which is not less than 2**{} == {})'.format(
+            axis,
             elem_length,
             max_num_levels,
             size_limit))
 
-  is_consistent = prefer_static.reduce_all([
-      prefer_static.equal(
-          prefer_static.shape(elem)[0], elem_length)
-      for elem in elems_flat[1:]])
+  is_consistent = ps.reduce_all([ps.equal(ps.shape(elem)[axis], elem_length)
+                                 for elem in elems_flat[1:]])
 
   is_consistent_ = tf.get_static_value(is_consistent)
   if is_consistent_ is None:
     assertions.append(
         tf.debugging.assert_equal(
             is_consistent, True,
-            message='Input `Tensor`s must have the same first dimension.'
+            message='Inputs must have the same size along the given axis.'
             ' (saw: {})'.format([elem.shape for elem in elems_flat])))
   elif not is_consistent_:
     raise ValueError(
-        'Input `Tensor`s must have the same first dimension.'
+        'Inputs must have the same size along the given axis.'
         ' (saw: {})'.format([elem.shape for elem in elems_flat]))
   return elem_length, assertions
 
 
-def scan_associative(fn, elems, max_num_levels=48,
-                     validate_args=False, name=None):
+def scan_associative(fn,
+                     elems,
+                     max_num_levels=48,
+                     axis=0,
+                     validate_args=False,
+                     name=None):
   """Perform a scan with an associative binary operation, in parallel.
 
   The associative scan operation computes the cumulative sum, or
@@ -131,20 +170,21 @@ def scan_associative(fn, elems, max_num_levels=48,
       signature `r = fn(a, b)`. This must satisfy associativity:
       `fn(a, fn(b, c)) == fn(fn(a, b), c)`. The inputs and result are
       (possibly nested structures of) `Tensor`(s), matching `elems`. Each
-      `Tensor` has a leading batch dimension in place of `elem_length`; the `fn`
+      `Tensor` has a batch dimension in place of `elem_length`; the `fn`
       is expected to map over this dimension. The result `r` has the same shape
       (and structure) as the two inputs `a` and `b`.
-    elems: A (possibly nested structure of) `Tensor`(s), each with leading
-      dimension `elem_length`. Note that `elem_length` determines the number
+    elems: A (possibly nested structure of) `Tensor`(s), each with dimension
+      `elem_length` along `axis`. Note that `elem_length` determines the number
       of recursive steps required to perform the scan: if, in graph mode,
       this is not statically available, then ops will be created to
       handle any `elem_length` up to the maximum dimension of a `Tensor`.
-    max_num_levels: Python `int`. The size
-      of the first dimension of the tensors in `elems` must be less than
-      `2**(max_num_levels + 1)`. The default value is sufficiently large
-      for most needs. Lowering this value can reduce graph-building time when
-      `scan_associative` is used with inputs of unknown shape.
+    max_num_levels: Python `int`. The `axis` of the tensors in `elems` must have
+      size less than `2**(max_num_levels + 1)`. The default value is
+      sufficiently large for most needs. Lowering this value can reduce
+      graph-building time when `scan_associative` is used with inputs of unknown
+      shape.
       Default value: `48`.
+    axis: Tensor `int` axis along which to perform the scan.
     validate_args: Python `bool`. When `True`, runtime checks
       for invalid inputs are performed. This may carry a performance cost.
       Default value: `False`.
@@ -175,6 +215,7 @@ def scan_associative(fn, elems, max_num_levels=48,
   tfp.math.scan_associative(tf.matmul, matrices)
   ```
   """
+  slice_elem = functools.partial(_slice_along_axis, axis=axis)
 
   def lowered_fn(a, b):
     # Lower `fn` to operate on flattened sequences of elems.
@@ -236,31 +277,34 @@ def scan_associative(fn, elems, max_num_levels=48,
   # statically then we don't need a `level` parameter.
   def _scan(level, elems):
     """Perform scan on `elems`."""
-    elem_length = prefer_static.shape(elems[0])[0]
+    elem_length = ps.shape(elems[0])[axis]
 
     # Apply `fn` to reduce adjacent pairs to a single entry.
-    a = [elem[0:-1:2] for elem in elems]
-    b = [elem[1::2] for elem in elems]
+    a = [slice_elem(elem, 0, -1, step=2) for elem in elems]
+    b = [slice_elem(elem, 1, None, step=2) for elem in elems]
     reduced_elems = lowered_fn(a, b)
 
     def handle_base_case_elem_length_two():
-      return [tf.concat([elem[0:1], reduced_elem], axis=0)
+      return [tf.concat([slice_elem(elem, 0, 1), reduced_elem], axis=axis)
               for (reduced_elem, elem) in zip(reduced_elems, elems)]
 
     def handle_base_case_elem_length_three():
       reduced_reduced_elems = lowered_fn(
-          reduced_elems, [elem[2:3] for elem in elems])
+          reduced_elems,
+          [slice_elem(elem, 2, 3) for elem in elems])
       return [
-          tf.concat([elem[0:1], reduced_elem, reduced_reduced_elem], axis=0)
+          tf.concat([slice_elem(elem, 0, 1),  # pylint: disable=g-complex-comprehension
+                     reduced_elem,
+                     reduced_reduced_elem], axis=axis)
           for (reduced_reduced_elem, reduced_elem, elem)
           in zip(reduced_reduced_elems, reduced_elems, elems)]
 
     # Base case of recursion: assumes `elem_length` is 2 or 3.
-    at_base_case = prefer_static.logical_or(
-        prefer_static.equal(elem_length, 2),
-        prefer_static.equal(elem_length, 3))
-    base_value = lambda: prefer_static.cond(  # pylint: disable=g-long-lambda
-        prefer_static.equal(elem_length, 2),
+    at_base_case = ps.logical_or(
+        ps.equal(elem_length, 2),
+        ps.equal(elem_length, 3))
+    base_value = lambda: ps.cond(  # pylint: disable=g-long-lambda
+        ps.equal(elem_length, 2),
         handle_base_case_elem_length_two,
         handle_base_case_elem_length_three)
 
@@ -272,31 +316,35 @@ def scan_associative(fn, elems, max_num_levels=48,
       odd_elems = _scan(level - 1, reduced_elems)
 
       def even_length_case():
-        return lowered_fn([odd_elem[:-1] for odd_elem in odd_elems],
-                          [elem[2::2] for elem in elems])
+        return lowered_fn(
+            [slice_elem(odd_elem, 0, -1) for odd_elem in odd_elems],
+            [slice_elem(elem, 2, None, 2) for elem in elems])
 
       def odd_length_case():
         return lowered_fn([odd_elem for odd_elem in odd_elems],
-                          [elem[2::2] for elem in elems])
+                          [slice_elem(elem, 2, None, 2) for elem in elems])
 
-      results = prefer_static.cond(
-          prefer_static.equal(elem_length % 2, 0),
+      results = ps.cond(
+          ps.equal(elem_length % 2, 0),
           even_length_case,
           odd_length_case)
 
       # The first element of a scan is the same as the first element
       # of the original `elems`.
-      even_elems = [tf.concat([elem[0:1], result], axis=0)
+      even_elems = [tf.concat([slice_elem(elem, 0, 1), result], axis=axis)
                     for (elem, result) in zip(elems, results)]
-      return list(map(_interleave, even_elems, odd_elems))
+      return list(map(lambda a, b: _interleave(a, b, axis=axis),
+                      even_elems,
+                      odd_elems))
 
-    return prefer_static.cond(at_base_case, base_value, recursive_case)
+    return ps.cond(at_base_case, base_value, recursive_case)
 
   with tf.name_scope(name if name else 'scan_associative'):
-    elem_length, assertions = _validate_elem_length(max_num_levels, elems_flat)
+    elem_length, assertions = _validate_elem_length(
+        max_num_levels, elems_flat, axis=axis)
 
   with tf.control_dependencies(assertions if validate_args else []):
-    return prefer_static.cond(
+    return ps.cond(
         elem_length < 2,
         lambda: elems,
         lambda: (tf.nest.pack_sequence_as(  # pylint: disable=g-long-lambda
