46ce72750712b0984c21f282ca9f3e13e5d7be45
Neel Ghoshal <72409908+NeelGhoshal@users.noreply.github.com>
2021-08-31 09:30:17 +0530

    updated backtracking_test

--- tensorflow_probability/python/optimizer/linesearch/backtracking_test.py
+++ tensorflow_probability/python/optimizer/linesearch/backtracking_test.py
@@ -1,20 +1,23 @@
 import unittest
-import Backtracking
-
+import backtracking
+from tensorflow_probability.python.internal import test_util
 
 class TestBacktracking(unittest.TestCase):
     
 
 
     def test_ndegree(self):
-         self.assertEqual(Backtracking.backtracking
+         self.assertEqual(backtracking.backtracking
                           (lambda x: x**2 +3*x, lambda x: 2*x + 3,11), 0.49984899999999993)
          
-         self.assertEqual(Backtracking.backtracking
+         self.assertEqual(backtracking.backtracking
                           (lambda x: x**10 +3*x, lambda x: 10*(x**9) + 3,2), 6.07776055631376e-05)
          
-         self.assertEqual(Backtracking.backtracking
+         self.assertEqual(backtracking.backtracking
                           (lambda x: x**5 - 3*x, lambda x: 5*(x**4) - 3,2),1)
        
 if __name__ == '__main__':
     unittest.main()
+
+if __name__ == '__main__':
+  test_util.main()
