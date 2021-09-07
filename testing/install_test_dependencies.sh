4988dc99c02289f3d833af6a32e5de908c805831
colcarroll <colcarroll@google.com>
2021-09-07 12:38:14 -0700

    Pin setuptools to allow coverage to be installed.
    PiperOrigin-RevId: 395307111

--- testing/install_test_dependencies.sh
+++ testing/install_test_dependencies.sh
@@ -200,7 +200,8 @@ install_python_packages() {
 # Ensure newer than 18.x pip version, which is necessary after tf-nightly
 # switched to manylinux2010.
 python -m pip install $PIP_FLAGS --upgrade 'pip>=19.2'
-python -m pip install --upgrade setuptools
+# TODO(b/199158964): Remove pin when setuptools works with use_2to3 again.
+python -m pip install --upgrade setuptools\<58.0;
 
 check_for_common_package_conflicts
 install_python_packages
