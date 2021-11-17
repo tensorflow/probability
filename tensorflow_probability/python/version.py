563ae8afa5b728375e05f4455f4cc518b0714555
emilyaf <emilyaf@google.com>
2021-11-16 22:39:46 -0800

    Increment minor version number; TFP 0.15 release is in progress.
    PiperOrigin-RevId: 410437202

--- tensorflow_probability/python/version.py
+++ tensorflow_probability/python/version.py
@@ -16,7 +16,7 @@
 
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = '0'
-_MINOR_VERSION = '15'
+_MINOR_VERSION = '16'
 _PATCH_VERSION = '0'
 
 # When building releases, we can update this value on the release branch to
