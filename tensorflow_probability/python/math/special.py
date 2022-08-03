Fix partial derivative of `betainc` w.r.t. `x`

tensorflow_probability/python/math/special.py
@@ -450,14 +450,14 @@ def _betainc_partials(a, b, x):
   grad_b = tf.where(use_power_series, ps_grad_b, cf_grad_b)
 
   # According to the code accompanying [1], grad_a = grad_b = 0 when x is
-  # equal to 0 or 1. Under the same condition, grad_x = 0 by its expression.
+  # equal to 0 or 1.
   # [1] R. Boik, J. Robinson-Cox,
   #     Derivatives of the Incomplete Beta Function
   #     https://www.jstatsoft.org/article/view/v003i01/beta.der.pdf
-  result_is_zero = tf.math.equal(x, zero) | tf.math.equal(x, one)
-  grad_a, grad_b, grad_x = [
-      tf.where(result_is_zero, zero, grad)
-      for grad in [grad_a, grad_b, grad_x]]
+  grads_a_and_b_should_be_zero = tf.math.equal(x, zero) | tf.math.equal(x, one)
+  grad_a, grad_b = [
+      tf.where(grads_a_and_b_should_be_zero, zero, grad)
+      for grad in [grad_a, grad_b]]
 
   # Determine if the inputs are out of range (should return NaN output).
   result_is_nan = (a <= zero) | (b <= zero) | (x < zero) | (x > one)

tensorflow_probability/python/math/special_test.py
@@ -263,15 +263,16 @@ class BetaincTest(test_util.TestCase):
     x = np.array([0.5, 0.5, 0.5, 0.5, -1., 2.], dtype=dtype)
 
     for partial in self.evaluate(betainc_partials(a, b, x)):
-      self.assertAllClose(np.full_like(a, np.nan), partial)
+      self.assertAllEqual(np.full_like(a, np.nan), partial)
 
     # Test partials when x is equal to 0 or 1.
     a = np.array([0.4, 0.4], dtype=dtype)
     b = np.array([0.6, 0.6], dtype=dtype)
     x = np.array([0., 1.], dtype=dtype)
 
-    for partial in self.evaluate(betainc_partials(a, b, x)):
-      self.assertAllClose(np.zeros_like(a), partial)
+    partial_a, partial_b, _ = self.evaluate(betainc_partials(a, b, x))
+    for partial in [partial_a, partial_b]:
+      self.assertAllEqual(np.zeros_like(a), partial)
 
   def _testBetaincDerivative(self,
                              a,