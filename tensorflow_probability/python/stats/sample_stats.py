627625bffd47536943ef88d1b4561e960cd22889
Nicolas Pinchaud <nicolas.pinchaud@gmail.com>
2022-08-05 21:43:42 +0200

    Fix doc of `windowed_variance` to match implementation

--- tensorflow_probability/python/stats/sample_stats.py
+++ tensorflow_probability/python/stats/sample_stats.py
@@ -712,7 +712,7 @@ def windowed_variance(
 
   Computes variances among data in the Tensor `x` along the given windows:
 
-    result[i] = variance(x[low_indices[i]:high_indices[i]+1])
+    result[i] = variance(x[low_indices[i]:high_indices[i]])
 
   accurately and efficiently.  To wit, if K is the size of
   `low_indices` and `high_indices`, and `N` is the size of `x` along
