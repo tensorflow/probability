Fix gradient issue in tfpk.Matern when input points are equal.
PiperOrigin-RevId: 485744124

tensorflow_probability/python/math/psd_kernels/matern.py
@@ -205,8 +205,7 @@ class GeneralizedMatern(_AmplitudeLengthScaleMixin,
 
   def _apply_with_distance(
       self, x1, x2, pairwise_square_distance, example_ndims=0):
-    # Use util.sqrt_with_finite_grads to avoid NaN gradients when `x1 == x2`.
-    norm = util.sqrt_with_finite_grads(pairwise_square_distance)
+    norm = tf.math.sqrt(pairwise_square_distance)
     inverse_length_scale = self._inverse_length_scale_parameter()
     if inverse_length_scale is not None:
       inverse_length_scale = util.pad_shape_with_ones(
@@ -319,8 +318,15 @@ class MaternOneHalf(_AmplitudeLengthScaleMixin,
 
   def _apply_with_distance(
       self, x1, x2, pairwise_square_distance, example_ndims=0):
-    # Use util.sqrt_with_finite_grads to avoid NaN gradients when `x1 == x2`.
-    norm = util.sqrt_with_finite_grads(pairwise_square_distance)
+    # Where pairwise square distance is 0, gradients with respect to each of the
+    # inputs should be 0 as well. Set square distances to be statically 0 to
+    # ensure that gradients are 0 (and not infinity/NaN when the square root is
+    # taken).
+    pairwise_sq = tf.where(
+        tf.equal(pairwise_square_distance, 0.),
+        tf.zeros([], dtype=pairwise_square_distance.dtype),
+        pairwise_square_distance)
+    norm = tf.math.sqrt(pairwise_sq)
     inverse_length_scale = self._inverse_length_scale_parameter()
     if inverse_length_scale is not None:
       inverse_length_scale = util.pad_shape_with_ones(
@@ -393,8 +399,15 @@ class MaternThreeHalves(_AmplitudeLengthScaleMixin,
 
   def _apply_with_distance(
       self, x1, x2, pairwise_square_distance, example_ndims=0):
-    # Use util.sqrt_with_finite_grads to avoid NaN gradients when `x1 == x2`.
-    norm = util.sqrt_with_finite_grads(pairwise_square_distance)
+    # Where pairwise square distance is 0, gradients with respect to each of the
+    # inputs should be 0 as well. Set square distances to be statically 0 to
+    # ensure that gradients are 0 (and not infinity/NaN when the square root is
+    # taken).
+    pairwise_sq = tf.where(
+        tf.equal(pairwise_square_distance, 0.),
+        tf.zeros([], dtype=pairwise_square_distance.dtype),
+        pairwise_square_distance)
+    norm = tf.math.sqrt(pairwise_sq)
     np_dtype = dtype_util.as_numpy_dtype(norm.dtype)
     inverse_length_scale = self._inverse_length_scale_parameter()
     if inverse_length_scale is not None:
@@ -468,8 +481,15 @@ class MaternFiveHalves(_AmplitudeLengthScaleMixin,
 
   def _apply_with_distance(
       self, x1, x2, pairwise_square_distance, example_ndims=0):
-    # Use util.sqrt_with_finite_grads to avoid NaN gradients when `x1 == x2`.
-    norm = util.sqrt_with_finite_grads(pairwise_square_distance)
+    # Where pairwise square distance is 0, gradients with respect to each of the
+    # inputs should be 0 as well. Set square distances to be statically 0 to
+    # ensure that gradients are 0 (and not infinity/NaN when the square root is
+    # taken).
+    pairwise_sq = tf.where(
+        tf.equal(pairwise_square_distance, 0.),
+        tf.zeros([], dtype=pairwise_square_distance.dtype),
+        pairwise_square_distance)
+    norm = tf.math.sqrt(pairwise_sq)
     inverse_length_scale = self._inverse_length_scale_parameter()
     if inverse_length_scale is not None:
       inverse_length_scale = util.pad_shape_with_ones(