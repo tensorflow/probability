e398aadd357376d941f70ca4fdbb7a467c45754f
aleslamitz <109731102+aleslamitz@users.noreply.github.com>
2022-11-27 17:59:08 +0100

    Update Particle Filter

--- tensorflow_probability/python/experimental/mcmc/particle_filter.py
+++ tensorflow_probability/python/experimental/mcmc/particle_filter.py
@@ -201,6 +201,7 @@ def infer_trajectories(observations,
   # Generate simulated observations.
   observed_positions = tfd.Normal(loc=tf.linspace(0.4, 0.8, 0.01),
                                   scale=0.1).sample()
+
   # Run particle filtering to sample plausible trajectories.
   (trajectories,  # {'position': [40, 1000], 'velocity': [40, 1000]}
    lps) = tfp.experimental.mcmc.infer_trajectories(
@@ -290,6 +291,7 @@ def sequential_monte_carlo(loop_seed,
         never_trace=lambda *_: False
         ):
     """Samples a series of particles representing filtered latent states.
+
       The particle filter samples from the sequence of "filtering" distributions
       `p(state[t] | observations[:t])` over latent
       states: at each point in time, this is a sample from the distribution conditioned
@@ -297,6 +299,7 @@ def sequential_monte_carlo(loop_seed,
       at time `t` may be different from the particle with the same index at time
       `t + 1`. To reconstruct trajectories by tracing back through the resampling
       process, see `tfp.mcmc.experimental.reconstruct_trajectories`.
+
       ${particle_filter_arg_str}
         trace_fn: Python `callable` defining the values to be traced at each step,
           with signature `traced_values = trace_fn(weighted_particles, results)`
@@ -328,7 +331,9 @@ def sequential_monte_carlo(loop_seed,
           `trace_criterion_fn==None`, this is computed from the final step;
           otherwise, each Tensor will have initial dimension `num_steps_traced`
           and stacks the traced results across all steps.
+
       #### References
+
       [1] Adam Scibior, Vaden Masrani, and Frank Wood. Differentiable Particle
           Filtering without Modifying the Forward Pass. _arXiv preprint
           arXiv:2106.10314_, 2021. https://arxiv.org/abs/2106.10314
@@ -563,6 +568,7 @@ def _compute_observation_log_weights(step,
                                      observation_fn,
                                      num_transitions_per_observation=1):
   """Computes particle importance weights from an observation step.
+
   Args:
     step: int `Tensor` current step.
     particles: Nested structure of `Tensor`s, each of shape
