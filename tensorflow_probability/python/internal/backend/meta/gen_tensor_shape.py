4a5348f5ba303def61f3020d60781749cbd6eed0
thomaswc <thomaswc@google.com>
2023-02-07 08:34:01 -0800

    Update gen_tensor_shape.py to track latest TensorFlow changes, specifically a new nested_structure_coder registration.  The corresponding changes to tensor_shape.py were then automatically generated by rewrite_equivalence_test --update --modules_to_check=tensor_shape
    PiperOrigin-RevId: 507790314

--- tensorflow_probability/python/internal/backend/meta/gen_tensor_shape.py
+++ tensorflow_probability/python/internal/backend/meta/gen_tensor_shape.py
@@ -27,10 +27,13 @@ FLAGS = flags.FLAGS
 COMMENT_OUT = [
     'from tensorflow.core.framework import tensor_shape_pb2',
     'from tensorflow.core.function import trace_type',
+    'from tensorflow.core.protobuf import struct_pb2',
     'from tensorflow.python import tf2',
     'from tensorflow.python.eager import monitoring',
     ('from tensorflow.python.platform '
      'import tf_logging as logging'),
+    ('from tensorflow.python.saved_model '
+     'import nested_structure_coder'),
     'from tensorflow.python.types import trace',
     'from tensorflow.python.util.tf_export import tf_export',
     'from tensorflow.tools.docs import doc_controls',
@@ -81,6 +84,21 @@ def tf_export(*args, **kwargs):
 
 class doc_controls:
   do_not_doc_inheritable = lambda x: x
+
+
+class nested_structure_coder:
+  register_codec = lambda x: None
+
+
+class struct_pb2:
+  pass
+
+
+class StructuredValue:
+  pass
+
+
+setattr(struct_pb2, "StructuredValue", StructuredValue)
 """
 
 
