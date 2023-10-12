46fab90c7d41ea0cf3c508165fce3bb32f5c66fc
Jacob Burnim <jburnim@google.com>
2023-10-12 15:27:27 -0700

    Update version for TFP 0.22.1.

--- setup.py
+++ setup.py
@@ -88,7 +88,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
